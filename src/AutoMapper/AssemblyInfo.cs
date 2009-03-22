f5cadbb800986bb6cc3f15f2a3af20a383e8a4ab
Jimmy Bogard <jimmy.bogard@gmail.com>
2009-03-22 00:19:54 +0000

    Changes to the build to do common assembly info

--- src/AutoMapper/AssemblyInfo.cs
+++ src/AutoMapper/AssemblyInfo.cs
@@ -2,7 +2,6 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("AutoMapper")]
 [assembly: AssemblyDescription("AutoMapper")]
-[assembly: AssemblyVersion("0.2.1.*")]
 
 [assembly: AssemblyDelaySign(false)]
 
