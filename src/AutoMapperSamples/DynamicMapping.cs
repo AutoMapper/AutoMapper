eab7b15fe651b43de840018abc86f4ccffa3b834
Jimmy Bogard <jimmy.bogard@gmail.com>
2009-04-17 13:02:04 +0000

    Adding sample for common base class configuration

---
+++ src/AutoMapperSamples/DynamicMapping.cs
@@ -0,0 +1,34 @@
+using AutoMapper;
+using NBehave.Spec.NUnit;
+using NUnit.Framework;
+
+namespace AutoMapperSamples
+{
+	namespace DynamicMapping
+	{
+		[TestFixture]
+		public class MappingToInterfaces
+		{
+			public interface ICreateOrderMessage
+			{
+				string CustomerName { get; set; }
+			}
+
+			[SetUp]
+			public void SetUp()
+			{
+				Mapper.Reset();
+			}
+
+			[Test]
+			public void Example()
+			{
+				var order = new {CustomerName = "Bob Smith"};
+
+				var message = Mapper.DynamicMap<ICreateOrderMessage>(order);
+
+				message.CustomerName.ShouldEqual("Bob Smith");
+			}
+		}
+	}
+}
