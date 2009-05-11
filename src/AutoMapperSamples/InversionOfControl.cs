20766d4aa28b79f6599e84f4f01e293bbe5a9d4f
Jimmy Bogard <jimmy.bogard@gmail.com>
2009-05-11 03:22:43 +0000

    Attempting an IoC sample.  It doesnt work right now.

--- src/AutoMapperSamples/InversionOfControl.cs
+++ src/AutoMapperSamples/InversionOfControl.cs
@@ -31,9 +31,23 @@ namespace AutoMapperSamples
                     init.AddRegistry<ConfigurationRegistry>();
                 });
 
-                var configuration1 = ObjectFactory.GetInstance<Configuration>();
-                var configuration2 = ObjectFactory.GetInstance<Configuration>();
+                var configuration1 = ObjectFactory.GetInstance<IConfiguration>();
+                var configuration2 = ObjectFactory.GetInstance<IConfiguration>();
                 configuration1.ShouldBeTheSameAs(configuration2);
+
+                var configurationProvider = ObjectFactory.GetInstance<IConfigurationProvider>();
+                configurationProvider.ShouldBeTheSameAs(configuration1);
+
+                var configuration = ObjectFactory.GetInstance<Configuration>();
+                configuration.ShouldBeTheSameAs(configuration1);
+                
+                configuration1.CreateMap<Source, Destination>();
+
+                var engine = ObjectFactory.GetInstance<IMappingEngine>();
+
+                var destination = engine.Map<Source, Destination>(new Source {Value = 15});
+
+                destination.Value.ShouldEqual(15);
             }
         }
 
@@ -41,12 +55,18 @@ namespace AutoMapperSamples
         {
             public ConfigurationRegistry()
             {
-                Scan(scanner => scanner.AssemblyContainingType<IMappingEngine>());
+                ForRequestedType<Configuration>()
+                    .CacheBy(InstanceScope.Singleton)
+                    .TheDefault.Is.OfConcreteType<Configuration>()
+                    .CtorDependency<IObjectMapper[]>().Is(expr => expr.ConstructedBy(MapperRegistry.AllMappers));
 
-                //ForRequestedType<IConfigurationProvider>()
-                //    .CacheBy(InstanceScope.Singleton)
-                //    .TheDefault.Is.OfConcreteType<Configuration>()
-                //    .CtorDependency<IObjectMapper[]>().Is(expr => expr.ConstructedBy(MapperRegistry.AllMappers));
+                ForRequestedType<IConfigurationProvider>()
+                    .TheDefault.Is.ConstructedBy(ctx => ctx.GetInstance<Configuration>());
+
+                ForRequestedType<IConfiguration>()
+                    .TheDefault.Is.ConstructedBy(ctx => ctx.GetInstance<Configuration>());
+
+                ForRequestedType<IMappingEngine>().TheDefaultIsConcreteType<MappingEngine>();
             }
         }
     }
