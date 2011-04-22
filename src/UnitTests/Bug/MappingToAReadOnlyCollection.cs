5f406551467388b1000abb21006781a68e3ad4a5
hazzik <hazzik@gmail.com>
2011-04-22 23:11:06 +0600

    Implemented ReadOnlyCollectionMapper

--- src/UnitTests/Bug/MappingToAReadOnlyCollection.cs
+++ src/UnitTests/Bug/MappingToAReadOnlyCollection.cs
@@ -12,11 +12,13 @@ namespace AutoMapper.UnitTests.Bug
         public class Source
         {
             public int[] Values { get; set; }
+            public int[] Values2 { get; set; }
         }
 
         public class Destination
         {
             public ReadOnlyCollection<int> Values { get; set; }
+            public ReadOnlyCollection<int> Values2 { get; set; }
         }
 
         protected override void Establish_context()
@@ -29,6 +31,7 @@ namespace AutoMapper.UnitTests.Bug
             var source = new Source
                              {
                                  Values = new[] {1, 2, 3, 4},
+                                 Values2 = new[] {5, 6},
                              };
             _destination = Mapper.Map<Source, Destination>(source);
         }
@@ -37,10 +40,16 @@ namespace AutoMapper.UnitTests.Bug
         public void Should_map_the_list_of_source_items()
         {
             _destination.Values.ShouldNotBeNull();
+            _destination.Values.ShouldBeOfLength(4);
             _destination.Values.ShouldContain(1);
             _destination.Values.ShouldContain(2);
             _destination.Values.ShouldContain(3);
             _destination.Values.ShouldContain(4);
+
+            _destination.Values2.ShouldNotBeNull();
+            _destination.Values2.ShouldBeOfLength(2);
+            _destination.Values2.ShouldContain(5);
+            _destination.Values2.ShouldContain(6);
         }
     }
 }
