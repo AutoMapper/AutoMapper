bfa2285571db723a1dc626e4c3d7cc9bfa9ff055
Pero Pejovic <peropejovic@gmail.com>
2012-03-30 22:48:35 -0700

    GetHashCode() returns a precalculated value in an unchecked context. Have put the calculation back into a checked context.
    Closes #188

--- src/AutoMapper/Internal/TypePair.cs
+++ src/AutoMapper/Internal/TypePair.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System;
 
 namespace AutoMapper.Internal
@@ -45,3 +46,52 @@ namespace AutoMapper.Internal
 		}
 	}
 }
+=======
+using System;
+
+namespace AutoMapper.Internal
+{
+	internal struct TypePair : IEquatable<TypePair>
+	{
+
+		public TypePair(Type sourceType, Type destinationType)
+			: this()
+		{
+			_sourceType = sourceType;
+			_destinationType = destinationType;
+			_hashcode = unchecked((_sourceType.GetHashCode()*397) ^ _destinationType.GetHashCode());
+		}
+
+        private readonly Type _destinationType;
+        private readonly int _hashcode;
+        private readonly Type _sourceType;
+
+	    public Type SourceType
+	    {
+	        get { return _sourceType; }
+	    }
+
+	    public Type DestinationType
+	    {
+	        get { return _destinationType; }
+	    }
+
+		public bool Equals(TypePair other)
+		{
+			return Equals(other._sourceType, _sourceType) && Equals(other._destinationType, _destinationType);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (obj.GetType() != typeof (TypePair)) return false;
+			return Equals((TypePair) obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return _hashcode;			
+		}
+	}
+}
+>>>>>>> 405eb8b... GetHashCode() returns a precalculated value in an unchecked context. Have put the calculation back into a checked context.
