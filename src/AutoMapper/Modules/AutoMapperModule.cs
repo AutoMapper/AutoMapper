eeb505ad77dbcb656726758779a788ad4e55db23
Simon Jefford <simon.jefford@gmail.com>
2012-04-16 14:51:15 +0100

    start of module support

---
+++ src/AutoMapper/Modules/AutoMapperModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoMapper.Modules
+{
+    public abstract class AutoMapperModule
+    {
+        public static Func<IConfiguration> Configuration = () => Mapper.Configuration;
+
+        public abstract void Load();
+
+        protected IMappingExpression<TSource, TDestination> CreateMap<TSource, TDestination>()
+        {
+            return Configuration().CreateMap<TSource, TDestination>();
+        }
+
+        public virtual string Name
+        {
+            get { return GetType().FullName; }
+        }
+    }
+}
---
+++ src/UnitTests/Modules/ModuleLoading.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Rhino.Mocks;
+using AutoMapper.Modules;
+
+namespace AutoMapper.UnitTests.Modules
+{
+    class Source
+    {
+        public int SomeProperty { get; set; }
+    }
+
+    class Destination
+    {
+        public int SomeProperty { get; set; }
+    }
+
+    class TestModule : AutoMapperModule
+    {
+        public override void Load()
+        {
+            CreateMap<Source, Destination>();
+        }
+    }
+
+    [TestFixture]
+    public class ModuleLoading
+    {
+        [Test]
+        public void modules_should_delegate_mapping_creation_to_an_IConfiguration_instance()
+        {
+            IConfiguration config = MockRepository.GenerateMock<IConfiguration>();
+            AutoMapperModule.Configuration = () => config;
+
+            config.Expect(c => c.CreateMap<Source, Destination>()).Return(null);
+
+            var module = new TestModule();
+            module.Load();
+
+            config.VerifyAllExpectations();
+        }
+    }
+}
