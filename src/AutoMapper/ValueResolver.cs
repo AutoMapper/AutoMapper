59934d2e70d5f10e5942b1191f6320787f41e6e7
Jimmy Bogard <jimmy.bogard@gmail.com>
2012-05-07 19:37:40 -0500

    Corrected value resolver exception message. Closes #199

--- src/AutoMapper/ValueResolver.cs
+++ src/AutoMapper/ValueResolver.cs
@@ -4,10 +4,10 @@ namespace AutoMapper
 	{
 		public ResolutionResult Resolve(ResolutionResult source)
 		{
-			if (source.Value != null && !(source.Value is TSource))
-			{
-				throw new AutoMapperMappingException(string.Format("Value supplied is of type {0} but expected {1}.\nChange the value resolver source type, or redirect the source value supplied to the value resolver using FromMember.", typeof(TSource), source.Value.GetType()));
-			}
+            if (source.Value != null && !(source.Value is TSource))
+            {
+                throw new AutoMapperMappingException(string.Format("Value supplied is of type {0} but expected {1}.\nChange the value resolver source type, or redirect the source value supplied to the value resolver using FromMember.", source.Value.GetType(), typeof(TSource)));
+            }
 
 			return source.New(ResolveCore((TSource) source.Value), typeof (TDestination));
 		}
