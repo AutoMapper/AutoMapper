34a5252241aec3b67485216bbf696a8751395967
Jimmy Bogard <jimmy.bogard@gmail.com>
2012-05-07 19:51:00 -0500

    Fixing problem of mapping to existing destination type to check the runtime type. Closes #195

--- src/AutoMapper/MappingEngine.cs
+++ src/AutoMapper/MappingEngine.cs
@@ -96,7 +96,7 @@ namespace AutoMapper
 	    public TDestination Map<TSource, TDestination>(TSource source, TDestination destination, Action<IMappingOperationOptions> opts)
 	    {
             Type modelType = typeof(TSource);
-            Type destinationType = typeof(TDestination);
+            Type destinationType = (Equals(destination, default(TDestination)) ? typeof(TDestination) : destination.GetType());
 
             return (TDestination)Map(source, destination, modelType, destinationType, opts);
         }
@@ -149,7 +149,7 @@ namespace AutoMapper
 		public void DynamicMap<TSource, TDestination>(TSource source, TDestination destination)
 		{
 			Type modelType = typeof(TSource);
-			Type destinationType = typeof(TDestination);
+            Type destinationType = (Equals(destination, default(TDestination)) ? typeof(TDestination) : destination.GetType());
 
 			DynamicMap(source, destination, modelType, destinationType);
 		}
