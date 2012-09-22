d5e82f8b9e7966fccad8ed6f4750d8509509607c
Jimmy Bogard <jimmy.bogard@gmail.com>
2012-09-21 21:05:57 -0500

    Adding a test to verify deep nested graph of filling existing destinations; closes #170

--- src/UnitTests/FillingExistingDestination.cs
+++ src/UnitTests/FillingExistingDestination.cs
@@ -54,6 +54,86 @@ namespace AutoMapper.UnitTests
 				_originalDest.ShouldBeSameAs(_dest);
 			}
 		}
+       
+        public class When_the_destination_object_is_specified_with_child_objects : AutoMapperSpecBase
+        {
+            private Source _source;
+            private Destination _originalDest;
+            private Destination _dest;
+
+            public class Source
+            {
+                public int Value { get; set; }
+                public ChildSource Child { get; set; }
+            }
+
+            public class Destination
+            {
+                public int Value { get; set; }
+                public string Name { get; set; }
+                public ChildDestination Child { get; set; }
+            }
+
+            public class ChildSource
+            {
+                public int Value { get; set; }
+            }
+
+            public class ChildDestination
+            {
+                public int Value { get; set; }
+                public string Name { get; set; }
+            }
+
+            protected override void Establish_context()
+            {
+                base.Establish_context();
+
+                Mapper.Initialize(cfg =>
+                {
+                    cfg.CreateMap<Source, Destination>(MemberList.Source);
+                    cfg.CreateMap<ChildSource, ChildDestination>(MemberList.Source);
+                });
+
+                _source = new Source
+                {
+                    Value = 10,
+                    Child = new ChildSource
+                    {
+                        Value = 20
+                    }
+                };
+            }
+
+            protected override void Because_of()
+            {
+                _originalDest = new Destination
+                {
+                    Value = 1111,
+                    Name = "foo",
+                    Child = new ChildDestination
+                    {
+                        Name = "bar"
+                    }
+                };
+                _dest = Mapper.Map<Source, Destination>(_source, _originalDest);
+            }
+
+            [Test]
+            public void Should_do_the_translation()
+            {
+                _dest.Value.ShouldEqual(10);
+                _dest.Child.Value.ShouldEqual(20);
+            }
+
+            [Test]
+            public void Should_return_the_destination_object_that_was_passed_in()
+            {
+                _dest.Name.ShouldEqual("foo");
+                _dest.Child.Name.ShouldEqual("bar");
+            }
+        }
+
 
 		public class When_the_destination_object_is_specified_and_you_are_converting_an_enum : AutoMapperSpecBase
 		{
