007b84408c45ea5c7a19d805eb45309adc93b206
Jimmy Bogard <jimmy.bogard@gmail.com>
2013-02-11 19:10:28 -0600

    Cannot reproduce #298 but hey regression tests are good. closes #298

---
+++ src/UnitTests/Bug/NullableEnums.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace AutoMapper.UnitTests.Bug
+{
+    public class NullableEnums : AutoMapperSpecBase
+    {
+        public class Src { public EnumType? A { get; set; } }
+        public class Dst { public EnumType? A { get; set; } }
+
+        public enum EnumType { One, Two }
+
+        protected override void Establish_context()
+        {
+            Mapper.CreateMap<Src, Dst>();
+        }
+
+        [Test]
+        public void TestNullableEnum()
+        {
+            var d = Mapper.Map(new Src { A = null }, new Dst { A = EnumType.One });
+
+            Assert.That(d.A, Is.Null);
+        } 
+    }
+}
