d6f98108d8ab7e4664e5b99cd4a271b2adb4e125
Jimmy Bogard <jimmy.bogard@gmail.com>
2013-02-11 18:56:23 -0600

    Destination arrays if exist are automatically rebuilt, fixes #271

--- src/AutoMapper/Mappers/ArrayMapper.cs
+++ src/AutoMapper/Mappers/ArrayMapper.cs
@@ -19,9 +19,14 @@ namespace AutoMapper.Mappers
 			destination.SetValue(mappedValue, index);
 		}
 
-		protected override Array CreateDestinationObjectBase(Type destElementType, int sourceLength)
-		{
-			return ObjectCreator.CreateArray(destElementType, sourceLength);
-		}
+	    protected override Array CreateDestinationObjectBase(Type destElementType, int sourceLength)
+	    {
+            throw new NotImplementedException();
+        }
+
+	    protected override object GetOrCreateDestinationObject(ResolutionContext context, IMappingEngineRunner mapper, Type destElementType, int sourceLength)
+        {
+            return ObjectCreator.CreateArray(destElementType, sourceLength);
+        }
 	}
 }
---
+++ src/UnitTests/Bug/ExistingArrays.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace AutoMapper.UnitTests.Bug
+{
+    public class ExistingArrays : AutoMapperSpecBase
+    {
+        protected override void Establish_context()
+        {
+            Mapper.CreateMap<Source, Dest>();
+        }
+
+        [Test]
+        public void should_map_array_inside_object()
+        {
+            var source = new Source { Values = new[] { "1", "2" } };
+            var dest = Mapper.Map<Dest>(source);
+        }
+
+        public class Source
+        {
+            public Source()
+            {
+                Values = new string[0];
+            }
+
+            public string[] Values { get; set; }
+        }
+
+        public class Dest
+        {
+            public Dest()
+            {
+                // remove this line will get it fixed. 
+                Values = new string[0];
+            }
+
+            public string[] Values { get; set; }
+        }
+    }
+}
