3834a7137c4671dd1518b19823138dd8c1e2f3bb
Jimmy Bogard <jimmy.bogard@gmail.com>
2013-02-28 14:17:12 -0600

    Trying StatLight

---
+++ src/UnitTests.SL4/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Silverlight.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SilverlightTest1
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
