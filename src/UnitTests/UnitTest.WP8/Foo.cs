a55791c9b4bd1098bd2418f26254e841c08a04a2
Pedro Reys <pedroreys@gmail.com>
2013-03-25 09:12:46 -0500

    Add test project targeting WP8 and running xunit

---
+++ src/UnitTests/UnitTest.WP8/Foo.cs
@@ -0,0 +1,36 @@
+﻿namespace UnitTest.WP8
+{
+    using AutoMapper;
+
+    using Xunit;
+
+    public class AutoMapperWpSupportTest
+    {
+        [Fact]
+        public void Should_be_able_to_create_a_map()
+        {
+            var foo = new Foo() { Bar = new Bar() { Value = "someValue" } };
+
+            Mapper.CreateMap<Foo, FooBar>();
+
+            var fooBar = Mapper.Map<FooBar>(foo);
+
+            Assert.Equal(foo.Bar.Value, fooBar.BarValue);
+        }
+    }
+
+    public class Foo
+    {
+        public Bar Bar { get; set; }
+    }
+
+    public class Bar
+    {
+        public string Value { get; set; }
+    }
+
+    public class FooBar
+    {
+        public string BarValue { get; set; }
+    }
+}
