5f02108437e90516b6da8c7c54b1f2d0775f6f6b
Jimmy Bogard <jimmy.bogard@gmail.com>
2013-11-02 18:35:07 -0500

    Verifying large array copying is fixed; closes #364, closes #390

--- src/UnitTests/Bug/AssignableCollectionBug.cs
+++ src/UnitTests/Bug/AssignableCollectionBug.cs
@@ -81,4 +81,43 @@ namespace AutoMapper.UnitTests.Bug
 		}
 
 	}
+
+    namespace ByteArrayBug
+    {
+        public class When_mapping_byte_arrays : AutoMapperSpecBase
+        {
+            private Picture _source;
+            private PictureDto _dest;
+
+            public class Picture
+            {
+                public int Id { get; set; }
+                public string Description { get; set; }
+                public byte[] ImageData { get; set; }
+            }
+
+            public class PictureDto
+            {
+                public string Description { get; set; }
+                public byte[] ImageData { get; set; }
+            }
+
+            protected override void Establish_context()
+            {
+                Mapper.Initialize(cfg => cfg.CreateMap<Picture, PictureDto>());
+            }
+
+            protected override void Because_of()
+            {
+                _source = new Picture {ImageData = new byte[1000000]};
+                _dest = Mapper.Map<Picture, PictureDto>(_source);
+            }
+
+            [Fact]
+            public void Should_copy_array()
+            {
+                _dest.ImageData.ShouldBeSameAs(_source.ImageData);
+            }
+        }
+    }
 }
