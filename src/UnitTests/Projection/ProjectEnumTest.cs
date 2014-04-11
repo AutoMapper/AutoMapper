8a83c91a157c2a584fe864d6b8bb5c6c55b0d04d
whut <rafalklys@wp.pl>
2014-04-11 08:32:00 +0200

    Unit test to show missing feature of handling enum to string mappings in queryable projections

---
+++ src/UnitTests/Projection/ProjectEnumTest.cs
@@ -0,0 +1,63 @@
+﻿namespace AutoMapper.UnitTests.Projection
+{
+    using AutoMapper.QueryableExtensions;
+    using Should;
+    using System.Linq;
+    using Xunit;
+
+    public class ProjectEnumTest
+    {
+        public ProjectEnumTest()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerType, string>().ConvertUsing(ct => ct.ToString().ToUpper());
+        }
+
+        [Fact]
+        public void RegularMappingWorks()
+        {
+            var customers = new[] { new Customer() { FirstName = "Bill", LastName = "White", CustomerType = CustomerType.Vip } }.AsQueryable();
+
+            var projected = Mapper.Map<CustomerDto[]>(customers);
+
+            projected.ShouldNotBeNull();
+            Assert.Equal(customers.Single().CustomerType.ToString().ToUpper(), projected.Single().CustomerType);
+        }
+
+        [Fact]
+        public void ProjectingEnumToString()
+        {
+            var customers = new[] { new Customer() { FirstName = "Bill", LastName = "White", CustomerType = CustomerType.Vip } }.AsQueryable();
+
+            Mapper.Map<CustomerDto[]>(customers);
+
+            var projected = customers.Project().To<CustomerDto>();
+            projected.ShouldNotBeNull();
+            Assert.Equal(customers.Single().CustomerType.ToString().ToUpper(), projected.Single().CustomerType);
+        }
+
+        public class Customer
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public CustomerType CustomerType { get; set; }
+        }
+
+        public class CustomerDto
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public string CustomerType { get; set; }
+        }
+
+        public enum CustomerType
+        {
+            Regular,
+            Vip,
+        }
+    }
+}
