e50267f73d7bf45fa8f2fb5448b2906710ca469f
Jimmy Bogard <jimmy.bogard@gmail.com>
2014-09-05 17:58:08 +0100

    Moving enumerable expressions out

--- src/AutoMapper/QueryableExtensions/Extensions.cs
+++ src/AutoMapper/QueryableExtensions/Extensions.cs
@@ -72,7 +72,7 @@ namespace AutoMapper.QueryableExtensions
             return new ProjectionExpression<TSource>(source, mappingEngine);
         }
 
-        private static LambdaExpression CreateMapExpression(IMappingEngine mappingEngine, ExpressionRequest request,
+        internal static LambdaExpression CreateMapExpression(IMappingEngine mappingEngine, ExpressionRequest request,
             Internal.IDictionary<ExpressionRequest, int> typePairCount)
         {
             // this is the input parameter of this expression with name <variableName>
@@ -144,6 +144,7 @@ namespace AutoMapper.QueryableExtensions
                 var propertyTypeMap = mappingEngine.ConfigurationProvider.FindTypeMapFor(result.Type, propertyMap.DestinationPropertyType);
                 var propertyRequest = new ExpressionRequest(result.Type, propertyMap.DestinationPropertyType, request.IncludedMembers);
 
+                var enumerableExpressionBinder = new EnumerableExpressionBinder();
                 var stringBinder = new StringExpressionBinder();
                 var customBinder = new CustomProjectionExpressionBinder();
 
@@ -158,10 +159,9 @@ namespace AutoMapper.QueryableExtensions
                 {
                     bindExpression = BindAssignableExpression(propertyMap, result);
                 }
-                else if (propertyMap.DestinationPropertyType.GetInterfaces().Any(t => t.Name == "IEnumerable") &&
-                         propertyMap.DestinationPropertyType != typeof(string))
+                else if (enumerableExpressionBinder.IsMatch(propertyMap, propertyTypeMap))
                 {
-                    bindExpression = BindEnumerableExpression(mappingEngine, propertyMap, request, result, typePairCount);
+                    bindExpression = enumerableExpressionBinder.Build(mappingEngine, propertyMap, propertyTypeMap, propertyRequest, result, typePairCount);
                 }
                 else if (propertyTypeMap != null && propertyTypeMap.CustomProjection == null)
                 {
@@ -238,82 +238,6 @@ namespace AutoMapper.QueryableExtensions
         private static MemberAssignment BindAssignableExpression(PropertyMap propertyMap, ExpressionResolutionResult result)
         {
             return Expression.Bind(propertyMap.DestinationProperty.MemberInfo, result.ResolutionExpression);
-        }
-
-        private static MemberAssignment BindEnumerableExpression(IMappingEngine mappingEngine, PropertyMap propertyMap, ExpressionRequest request, ExpressionResolutionResult result, Internal.IDictionary<ExpressionRequest, int> typePairCount)
-        {
-            MemberAssignment bindExpression;
-            Type destinationListType = GetDestinationListTypeFor(propertyMap);
-            Type sourceListType = null;
-            // is list
-
-            if (result.Type.IsArray)
-            {
-                sourceListType = result.Type.GetElementType();
-            }
-            else
-            {
-                sourceListType = result.Type.GetGenericArguments().First();
-            }
-            var listTypePair = new ExpressionRequest(sourceListType, destinationListType, request.IncludedMembers);
-
-
-            var selectExpression = result.ResolutionExpression;
-            if (sourceListType != destinationListType)
-            {
-                var transformedExpression = CreateMapExpression(mappingEngine, listTypePair, typePairCount);
-                selectExpression = Expression.Call(
-                    typeof(Enumerable),
-                    "Select",
-                    new[] { sourceListType, destinationListType },
-                    result.ResolutionExpression,
-                    transformedExpression);
-            }
-
-            if (typeof(IList<>).MakeGenericType(destinationListType).IsAssignableFrom(propertyMap.DestinationPropertyType)
-                || typeof(ICollection<>).MakeGenericType(destinationListType).IsAssignableFrom(propertyMap.DestinationPropertyType))
-            {
-                // Call .ToList() on IEnumerable
-                var toListCallExpression = GetToListCallExpression(propertyMap, destinationListType, selectExpression);
-
-                bindExpression = Expression.Bind(propertyMap.DestinationProperty.MemberInfo, toListCallExpression);
-            }
-            else if (propertyMap.DestinationPropertyType.IsArray)
-            {
-                // Call .ToArray() on IEnumerable
-                MethodCallExpression toArrayCallExpression = Expression.Call(
-                    typeof(Enumerable),
-                    "ToArray",
-                    new Type[] { destinationListType },
-                    selectExpression);
-                bindExpression = Expression.Bind(propertyMap.DestinationProperty.MemberInfo, toArrayCallExpression);
-            }
-            else
-            {
-                // destination type implements ienumerable, but is not an ilist. allow deferred enumeration
-                bindExpression = Expression.Bind(propertyMap.DestinationProperty.MemberInfo, selectExpression);
-            }
-            return bindExpression;
-        }
-
-        private static Type GetDestinationListTypeFor(PropertyMap propertyMap)
-        {
-            Type destinationListType;
-            if (propertyMap.DestinationPropertyType.IsArray)
-                destinationListType = propertyMap.DestinationPropertyType.GetElementType();
-            else
-                destinationListType = propertyMap.DestinationPropertyType.GetGenericArguments().First();
-            return destinationListType;
-        }
-
-        private static MethodCallExpression GetToListCallExpression(PropertyMap propertyMap, Type destinationListType,
-            Expression selectExpression)
-        {
-            return Expression.Call(
-                typeof(Enumerable),
-                propertyMap.DestinationPropertyType.IsArray ? "ToArray" : "ToList",
-                new[] { destinationListType },
-                selectExpression);
         }
 
         private static readonly IList<IExpressionResultConverter> ExpressionResultConverters =
