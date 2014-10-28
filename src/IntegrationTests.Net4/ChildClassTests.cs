1d456ea212968115c5208296a4cd06219c16deac
TylerCarlson1 <tycarlso@hotmail.com>
2014-10-27 23:29:38 -0500

    Remove Entity Framework reference to be moved to another repository

--- src/IntegrationTests.Net4/ChildClassTests.cs
+++ src/IntegrationTests.Net4/ChildClassTests.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using AutoMapper.EquivilencyExpression;
-using AutoMapper.Mappers;
 using AutoMapper.QueryableExtensions;
 using Xunit;
 using Should;
@@ -114,153 +107,11 @@ namespace AutoMapper.IntegrationTests.Net4
                     baseDTO.Sub.Sub1.ShouldEqual("sub1");
 
 
-                    baseDTO = Mapper.Map<BaseDTO>(context.Bases.FirstOrDefault());
+                    baseDTO = context.Bases.Project().To<BaseDTO>().FirstOrDefault();
                     baseDTO.ShouldNotBeNull();
                     baseDTO.BaseID.ShouldEqual(1);
                     baseDTO.Sub.Sub1.ShouldEqual("sub1");
                 }
-            }
-        }
-
-    }
-
-    namespace ChildClassListTests
-    {
-        public class Base
-        {
-            public int BaseID { get; set; }
-
-            [Required]
-            public string Base1 { get; set; }
-
-            [Required]
-            public virtual ICollection<Sub> Subs { get; set; }
-        }
-
-        public class BaseDTO
-        {
-            public int BaseID { get; set; }
-            public string Base1 { get; set; }
-            public virtual IList<SubDTO> Subs { get; set; }
-        }
-
-        public class Sub
-        {
-            [Key]
-            public int BaseId { get; set; }
-
-            [Required]
-            public string Sub1 { get; set; }
-        }
-
-        public class SubDTO
-        {
-            public int BaseID2 { get; set; }
-            public string Sub1 { get; set; }
-        }
-
-        public class Context : DbContext
-        {
-            public Context()
-                : base()
-            {
-                Database.SetInitializer<Context>(new DatabaseInitializer());
-            }
-
-            public DbSet<Base> Bases { get; set; }
-            public DbSet<Sub> Subs { get; set; }
-
-        }
-
-        public class DatabaseInitializer : CreateDatabaseIfNotExists<Context>
-        {
-            protected override void Seed(Context context)
-            {
-                context.Bases.Add(new Base() { BaseID = 1, Base1 = "base1", Subs = new List<Sub> { new Sub { BaseId = 1, Sub1 = "sub1" }, new Sub { BaseId = 2, Sub1 = "sub2" } }});
-
-                base.Seed(context);
-            }
-        }
-
-        public class UnitTest
-        {
-            public UnitTest()
-            {
-                Mapper.Reset();
-            }
-
-            [Fact]
-            public void EFConfiguredCorrectly()
-            {
-                using (var context = new Context())
-                {
-                    var baseEntitiy = context.Bases.FirstOrDefault();
-                    baseEntitiy.ShouldNotBeNull();
-                    baseEntitiy.BaseID.ShouldEqual(1);
-                    baseEntitiy.Subs.ElementAt(0).Sub1.ShouldEqual("sub1");
-                    baseEntitiy.Subs.ElementAt(1).Sub1.ShouldEqual("sub2");
-                }
-            }
-
-            [Fact]
-            public void AutoMapperEFRelationsTest()
-            {
-                EquivilentExpressions.GenerateEquality.Add(new GenerateEntityFrameworkPrimaryKeyEquivilentExpressions<Context>());
-                Mapper.CreateMap<Base, BaseDTO>().ReverseMap();
-                Mapper.CreateMap<Sub, SubDTO>().ForMember(dest => dest.BaseID2, opt => opt.MapFrom(src => src.BaseId))
-                    .ReverseMap().ForMember(dest => dest.BaseId, opt => opt.MapFrom(src => src.BaseID2));
-                Mapper.AssertConfigurationIsValid();
-
-                using (var context = new Context())
-                {
-                    var baseDTO = context.Bases.Project().To<BaseDTO>().FirstOrDefault();
-                    baseDTO.ShouldNotBeNull();
-                    baseDTO.Subs[1].Sub1 = "sub2 (modified)";
-                    baseDTO.Subs.Add(new SubDTO{BaseID2 = 3});
-                    var first = baseDTO.Subs[0];
-
-                    var baseObj = context.Bases.First();
-                    Mapper.Map(baseDTO, baseObj);
-                    var changes = context.ChangeTracker.Entries();
-
-                    var modified = changes.Where(c => c.State == EntityState.Modified).ToList();
-                    modified.Count().ShouldEqual(1);
-                    modified[0].Entity.ShouldBeSameAs(baseObj.Subs.ElementAt(1));
-
-                    var added = changes.Where(c => c.State == EntityState.Added).ToList();
-                    added.Count().ShouldEqual(1);
-                    added[0].Entity.ShouldBeSameAs(baseObj.Subs.ElementAt(2));
-                }
-            }
-
-            [Fact]
-            public void AutoMapperEFQuniqueExpressionTest()
-            {
-                EquivilentExpressions.GenerateEquality.Add(new GenerateEntityFrameworkPrimaryKeyEquivilentExpressions<Context>());
-                Mapper.CreateMap<Base, BaseDTO>().ReverseMap();
-                Mapper.CreateMap<Sub, SubDTO>().ForMember(dest => dest.BaseID2, opt => opt.MapFrom(src => src.BaseId))
-                    .ReverseMap().ForMember(dest => dest.BaseId, opt => opt.MapFrom(src => src.BaseID2));
-                Mapper.AssertConfigurationIsValid();
-                
-                var expression = Mapper.Map<BaseDTO,Expression<Func<Base,bool>>>(new BaseDTO{BaseID = 4});
-
-                var values = new object[5].Select((o, i) => new Base {BaseID = i}).ToList();
-                values.AsQueryable().FirstOrDefault(expression).ShouldEqual(values[4]);
-            }
-
-            [Fact]
-            public void AutoMapperEFExpressionConverterTest()
-            {
-                EquivilentExpressions.GenerateEquality.Add(new GenerateEntityFrameworkPrimaryKeyEquivilentExpressions<Context>());
-                Mapper.CreateMap<Base, BaseDTO>().ReverseMap();
-                Mapper.CreateMap<Sub, SubDTO>().ForMember(dest => dest.BaseID2, opt => opt.MapFrom(src => src.BaseId))
-                    .ReverseMap().ForMember(dest => dest.BaseId, opt => opt.MapFrom(src => src.BaseID2));
-                Mapper.AssertConfigurationIsValid();
-
-                var expression = Mapper.Map<Expression<Func<BaseDTO, bool>>, Expression<Func<Base, bool>>>(dto => dto.BaseID == 4);
-
-                var values = new object[5].Select((o, i) => new Base {BaseID = i}).ToList();
-                values.AsQueryable().FirstOrDefault(expression).ShouldEqual(values[4]);
             }
         }
 
