81fe659f810a9dc620952301ac3983a10c662218
Jimmy Bogard <jimmy.bogard@gmail.com>
2014-11-21 15:26:12 -0600

    Supporting open generics, closes #549, closes #59

---
+++ src/UnitTests/OpenGenerics.cs
@@ -0,0 +1,33 @@
+﻿namespace AutoMapper.UnitTests
+{
+    using Should;
+    using Xunit;
+
+    public class OpenGenerics
+    {
+        public class Source<T>
+        {
+            public T Value { get; set; }
+        }
+
+        public class Dest<T>
+        {
+            public T Value { get; set; }
+        }
+
+        [Fact]
+        public void Can_map_simple_generic_types()
+        {
+            Mapper.Initialize(cfg => cfg.CreateMap(typeof(Source<>), typeof(Dest<>)));
+
+            var source = new Source<int>
+            {
+                Value = 5
+            };
+
+            var dest = Mapper.Map<Source<int>, Dest<int>>(source);
+
+            dest.Value.ShouldEqual(5);
+        }
+    }
+}
