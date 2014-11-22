14ee5a8bab2236bce1b9db0fa8c423e9017ef6d9
TylerCarlson1 <tycarlso@hotmail.com>
2014-11-22 11:54:30 -0600

    Checking Recursive Generic Mappings and non-generic type mappings.

--- src/UnitTests/OpenGenerics.cs
+++ src/UnitTests/OpenGenerics.cs
@@ -7,11 +7,13 @@
     {
         public class Source<T>
         {
+            public int A { get; set; }
             public T Value { get; set; }
         }
 
         public class Dest<T>
         {
+            public int A { get; set; }
             public T Value { get; set; }
         }
 
@@ -28,6 +30,39 @@
             var dest = Mapper.Map<Source<int>, Dest<int>>(source);
 
             dest.Value.ShouldEqual(5);
+        }
+
+        [Fact]
+        public void Can_map_non_generic_members()
+        {
+            Mapper.Initialize(cfg => cfg.CreateMap(typeof(Source<>), typeof(Dest<>)));
+
+            var source = new Source<int>
+            {
+                A = 5
+            };
+
+            var dest = Mapper.Map<Source<int>, Dest<int>>(source);
+
+            dest.A.ShouldEqual(5);
+        }
+
+        [Fact]
+        public void Can_map_recursive_generic_types()
+        {
+            Mapper.Initialize(cfg => cfg.CreateMap(typeof(Source<>), typeof(Dest<>)));
+
+            var source = new Source<Source<int>>
+            {
+                Value = new Source<int>
+                {
+                    Value = 5,
+                }
+            };
+
+            var dest = Mapper.Map<Source<Source<int>>, Dest<Dest<double>>>(source);
+
+            dest.Value.Value.ShouldEqual(5);
         }
     }
 }
