69b638f30461a5d5ce3b5cf21ca526f37db3c921
TylerCarlson1 <tycarlso@hotmail.com>
2014-12-14 21:06:28 -0600

    Fix Issues with Expression Mappings when use properties which types have their own mappings that need to be converted over.
    Issue still lies with multi level same type one to many relationships.  Tools.Any(t => t.SubTools.Any(t => t.ID == id)) will fail to convert over.

--- src/AutoMapper/Mappers/ExpressionMapper.cs
+++ src/AutoMapper/Mappers/ExpressionMapper.cs
@@ -1,4 +1,9 @@
-﻿namespace AutoMapper.Mappers
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using AutoMapper.QueryableExtensions.Impl;
+
+namespace AutoMapper.Mappers
 {
     using System.Linq;
     using System.Linq.Expressions;
@@ -16,30 +21,8 @@
             if (sourceDelegateType.GetGenericTypeDefinition() != destDelegateType.GetGenericTypeDefinition())
                 throw new AutoMapperMappingException("Source and destination expressions must be of the same type.");
 
-            var parameters = expression.Parameters.ToArray();
-            var body = expression.Body;
-
-            for (int i = 0; i < expression.Parameters.Count; i++)
-            {
-                var sourceParamType = sourceDelegateType.GetGenericArguments()[i];
-                var destParamType = destDelegateType.GetGenericArguments()[i];
-
-                if (sourceParamType == destParamType)
-                    continue;
-
-                var typeMap = mapper.ConfigurationProvider.ResolveTypeMap(destParamType, sourceParamType);
-
-                if (typeMap == null)
-                    throw new AutoMapperMappingException(
-                        $"Could not find type map from destination type {destParamType} to source type {sourceParamType}. Use CreateMap to create a map from the source to destination types.");
-
-                var oldParam = expression.Parameters[i];
-                var newParam = Expression.Parameter(typeMap.SourceType, oldParam.Name);
-                parameters[i] = newParam;
-                var visitor = new MappingVisitor(typeMap, oldParam, newParam);
-                body = visitor.Visit(body);
-            }
-            return Expression.Lambda(body, parameters);
+            var mappingVisitor = new MappingVisitor(destDelegateType.GetGenericArguments());
+            return mappingVisitor.Visit(expression);
         }
 
         public bool IsMatch(ResolutionContext context)
@@ -52,11 +35,19 @@
 
         private class MappingVisitor : ExpressionVisitor
         {
-            private readonly TypeMap _typeMap;
-            private readonly ParameterExpression _oldParam;
-            private readonly ParameterExpression _newParam;
+            private IList<Type> _destSubTypes;
 
-            public MappingVisitor(TypeMap typeMap, ParameterExpression oldParam, ParameterExpression newParam)
+            private readonly TypeMap _typeMap;
+            private readonly Expression _oldParam;
+            private readonly Expression _newParam;
+
+            public MappingVisitor(IList<Type> destSubTypes)
+                : this(null, Expression.Parameter(typeof(Nullable)), Expression.Parameter(typeof(Nullable)) )
+            {
+                _destSubTypes = destSubTypes;
+            }
+
+            private MappingVisitor(TypeMap typeMap, Expression oldParam, Expression newParam)
             {
                 _typeMap = typeMap;
                 _oldParam = oldParam;
@@ -70,40 +61,144 @@
                 return node;
             }
 
+            protected override Expression VisitMethodCall(MethodCallExpression node)
+            {
+                return base.VisitMethodCall(GetConvertedMethodCall(node));
+            }
+
+            private MethodCallExpression GetConvertedMethodCall(MethodCallExpression node)
+            {
+                var convertedArguments = node.Arguments.Select(e => base.Visit(e)).ToList();
+                var convertedMethodArgumentTypes = node.Method.GetGenericArguments().Select(t => GetConvertingTypeIfExists(node.Arguments, t, convertedArguments)).ToArray();
+                var convertedMethodCall = node.Method.GetGenericMethodDefinition().MakeGenericMethod(convertedMethodArgumentTypes);
+                return Expression.Call(convertedMethodCall, convertedArguments);
+            }
+
+            private static Type GetConvertingTypeIfExists(IList<Expression> args, Type t, IList<Expression> arguments)
+            {
+                var matchingArgument = args.Where(a => !a.Type.IsGenericType).FirstOrDefault(a => a.Type == t);
+                if (matchingArgument != null)
+                {
+                    var index = args.IndexOf(matchingArgument);
+                    if (index < 0)
+                        return t;
+                    return arguments[index].Type;
+                }
+
+                var matchingEnumerableArgument = args.Where(a => a.Type.IsGenericType).FirstOrDefault(a => a.Type.GetGenericArguments()[0] == t);
+                var index2 = args.IndexOf(matchingEnumerableArgument);
+                if (index2 < 0) 
+                    return t;
+                return arguments[index2].Type.GetGenericArguments()[0];
+            }
+
+            protected override Expression VisitLambda<T>(Expression<T> expression)
+            {
+                if (expression.Parameters.Any(b => b.Type == _oldParam.Type))
+                    return VisitLambdaExpression(expression);
+                return VisitAllParametersExpression(expression);
+            }
+
+            private Expression VisitLambdaExpression<T>(Expression<T> expression)
+            {
+                var convertedBody = base.Visit(expression.Body);
+                var convertedArguments = expression.Parameters.Select(e => base.Visit(e) as ParameterExpression).ToList();
+                return Expression.Lambda(convertedBody, convertedArguments);
+            }
+
+            private Expression VisitAllParametersExpression<T>(Expression<T> expression)
+            {
+                var parameters = expression.Parameters.ToArray();
+                var visitors = new List<ExpressionVisitor>();
+                for (var i = 0; i < expression.Parameters.Count; i++)
+                {
+                    var sourceParamType = expression.Parameters[i].Type;
+                    if (_destSubTypes.Count <= i)
+                        continue;
+                    var destParamType = _destSubTypes[i];
+                    if (sourceParamType == destParamType)
+                        continue;
+
+                    var typeMap = Mapper.FindTypeMapFor(destParamType, sourceParamType);
+
+                    if (typeMap == null)
+                        throw new AutoMapperMappingException(
+                            string.Format(
+                                "Could not find type map from destination type {0} to source type {1}. Use CreateMap to create a map from the source to destination types.",
+                                destParamType, sourceParamType));
+
+                    var oldParam = expression.Parameters[i];
+                    var newParam = Expression.Parameter(typeMap.SourceType, oldParam.Name);
+                    parameters[i] = newParam;
+                    visitors.Add(new MappingVisitor(typeMap, oldParam, newParam));
+                }
+                return visitors.Aggregate(expression as Expression, (e, v) => v.Visit(e));
+            }
+
             protected override Expression VisitMember(MemberExpression node)
             {
-                var memberAccessor = node.Member.ToMemberAccessor();
-                var propertyMap = _typeMap.GetExistingPropertyMapFor(memberAccessor);
+                if (node == _oldParam)
+                    return _newParam;
+                var propertyMap = PropertyMap(node);
 
-
-                if (propertyMap.CustomExpression != null)
+                if (propertyMap == null)
                 {
-                    var replaced = new ParameterReplacementVisitor(_newParam);
-                    var newBody = replaced.Visit(propertyMap.CustomExpression.Body);
-                    return newBody;
+                    if (node.Expression is MemberExpression)
+                        return GetConvertedSubMemberCall(node);
+                    return node;
                 }
+                SetSorceSubTypes(propertyMap);
 
                 var replacedExpression = Visit(node.Expression);
 
+                if (propertyMap.CustomExpression != null)
+                    return ConvertCustomExpression(replacedExpression, propertyMap);
+                
                 return propertyMap.GetSourceValueResolvers()
                     .OfType<IMemberGetter>()
                     .Aggregate(replacedExpression,
                         (current, memberGetter) => Expression.MakeMemberAccess(current, memberGetter.MemberInfo));
             }
+
+            private Expression GetConvertedSubMemberCall(MemberExpression node)
+            {
+                var baseExpression = Visit(node.Expression);
+                var propertyMap = FindPropertyMapOfExpression(node.Expression as MemberExpression);
+
+                var typeMap = Mapper.FindTypeMapFor(propertyMap.SourceMember.GetMemberType(), propertyMap.DestinationPropertyType);
+                var memberExpression = new MappingVisitor(typeMap, node.Expression, baseExpression).Visit(node) as MemberExpression;
+
+                return Expression.MakeMemberAccess(baseExpression,memberExpression.Member);
         }
 
-        private class ParameterReplacementVisitor : ExpressionVisitor
-        {
-            private readonly ParameterExpression _newParam;
-
-            public ParameterReplacementVisitor(ParameterExpression newParam)
+            private PropertyMap FindPropertyMapOfExpression(MemberExpression expression)
             {
-                _newParam = newParam;
+                var propertyMap = PropertyMap(expression);
+                if (propertyMap == null && expression.Expression is MemberExpression)
+                    return FindPropertyMapOfExpression(expression.Expression as MemberExpression);
+                return propertyMap;
             }
 
-            protected override Expression VisitParameter(ParameterExpression node)
+            private PropertyMap PropertyMap(MemberExpression node)
+        {
+                var memberAccessor = node.Member.ToMemberAccessor();
+                var propertyMap = _typeMap.GetExistingPropertyMapFor(memberAccessor);
+                return propertyMap;
+            }
+
+            private void SetSorceSubTypes(PropertyMap propertyMap)
             {
-                return _newParam;
+                if (propertyMap.SourceMember is PropertyInfo)
+                    _destSubTypes = (propertyMap.SourceMember as PropertyInfo).PropertyType.GetGenericArguments();
+                else if (propertyMap.SourceMember is FieldInfo)
+                    _destSubTypes = (propertyMap.SourceMember as FieldInfo).FieldType.GetGenericArguments();
+            }
+
+            private Expression ConvertCustomExpression(Expression node, PropertyMap propertyMap)
+            {
+                var replaced = new ParameterReplacementVisitor(node);
+                var newBody = replaced.Visit(propertyMap.CustomExpression.Body);
+                return newBody;
             }
         }
     }
