6608fc2591a929beac3ea7346e12c1a5e56e9585
lbargaoanu <lbargaoanu@lucian.com>
2015-06-18 14:52:28 +0300

    if the destination member type is object, use the current source type, hopefully more specific

---
+++ src/UnitTests/Bug/ObjectEnumToObjectEnum.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Should;
+using AutoMapper.Mappers;
+using Xunit;
+
+namespace AutoMapper.UnitTests.Bug
+{
+    public class ObjectEnumToObjectEnum : AutoMapperSpecBase
+    {
+        MappingEngine _mapper;
+        Target _target;
+
+        public enum SourceEnumValue
+        {
+            Donkey,
+            Mule
+        }
+
+        public enum TargetEnumValue
+        {
+            Donkey,
+            Mule
+        }
+
+        public class Source
+        {
+            public object Value { get; set; }
+        }
+
+        public class Target
+        {
+            public object Value { get; set; }
+        }
+
+        protected override void Establish_context()
+        {
+            var configuration = new ConfigurationStore(new TypeMapFactory(), MapperRegistry.Mappers);
+            _mapper = new MappingEngine(configuration);
+            var parentMapping = configuration.CreateMap<Source, Target>();
+            parentMapping.ForMember(dest => dest.Value, opt => opt.MapFrom(s => (TargetEnumValue)s.Value));
+        }
+
+        protected override void Because_of()
+        {
+            _target = _mapper.Map<Target>(new Source { Value = SourceEnumValue.Mule });
+        }
+
+        [Fact]
+        public void Should_be_enum()
+        {
+            _target.Value.ShouldBeType<TargetEnumValue>();
+        }
+    }
+}
