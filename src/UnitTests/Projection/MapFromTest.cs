e606e436435affb904db4f9883582d1bde38bfb6
jkuehner <jochen.kuehner@kardex.com>
2015-07-01 00:49:48 +0200

    Unit Test for MapFrom(string)

--- src/UnitTests/Projection/MapFromTest.cs
+++ src/UnitTests/Projection/MapFromTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Should;
 using Xunit;
 
 namespace AutoMapper.UnitTests.Projection
@@ -16,6 +17,20 @@ namespace AutoMapper.UnitTests.Projection
                                 .ForMember(dto => dto.FullName, opt => opt.MapFrom(src => src.LastName + " " + src.FirstName));
 
                 typeof(NullReferenceException).ShouldNotBeThrownBy(() => Mapper.Engine.CreateMapExpression<UserModel, UserDto>()); //null reference exception here
+            }
+
+            [Fact]
+            public void Should_map_from_String()
+            {
+                Mapper.CreateMap<UserModel, UserDto>()
+                                .ForMember(dto => dto.FullName, opt => opt.MapFrom<string>("FirstName"));
+
+                var um = new UserModel();
+                um.FirstName = "Hallo";
+                var u = new UserDto();
+                Mapper.Map(um, u);
+
+                u.FullName.ShouldEqual(um.FirstName);
             }
         }
         public class UserModel
