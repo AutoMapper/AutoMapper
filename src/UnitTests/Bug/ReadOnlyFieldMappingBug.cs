baccafeb4a21927ed6c4c407565730c28f2eaabd
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2015-07-03 20:19:42 +0300

    Add test for read only field bug

---
+++ src/UnitTests/Bug/ReadOnlyFieldMappingBug.cs
@@ -0,0 +1,42 @@
+﻿using Should;
+using Xunit;
+
+namespace AutoMapper.UnitTests.Bug
+{
+    public class ReadOnlyFieldMappingBug : AutoMapperSpecBase
+    {
+        public class Source
+        {
+            public string Value { get; set; }
+        }
+
+        public class Destination
+        {
+            public readonly string Value;
+
+            public Destination(string value)
+            {
+                Value = value;
+            }
+        }
+
+        protected override void Establish_context()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                // BUG. ArgumentException : Expression must be writeable
+                cfg.CreateMap<Source, Destination>();
+            });
+        }
+
+        [Fact]
+        public void Should_map_over_constructor()
+        {
+            var source = new Source { Value = "value" };
+
+            var dest = Mapper.Map<Destination>(source);
+
+            dest.Value.ShouldEqual(source.Value);
+        }
+    }
+}
