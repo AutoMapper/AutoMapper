e144e8a3595e923265cfa90b9387bc3c1ce08947
Jimmy Bogard <jimmy.bogard@gmail.com>
2015-07-06 11:44:08 -0500

    Batch file will set up DNVM for you

--- psake.bat
+++ psake.bat
@@ -1,15 +1,36 @@
-
 @echo off
 cd %~dp0
 
 SETLOCAL
+SET CACHED_NUGET="%LocalAppData%\NuGet\NuGet.exe"
 
+IF EXIST %CACHED_NUGET% goto copynuget
+echo Downloading latest version of NuGet.exe...
+IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
+
+:copynuget
+IF EXIST .nuget\nuget.exe goto restore
+echo Copying NuGet
+md .nuget
+copy %CACHED_NUGET% .nuget\nuget.exe > nul
+
+:restore
 IF EXIST packages\KoreBuild goto run
+echo Restoring...
 .nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
+.nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion
 
-IF "%SKIP_DNX_INSTALL%"=="1" goto run
-CALL packages\KoreBuild\build\dnvm upgrade -unstable -runtime CLR -arch x86
-CALL packages\KoreBuild\build\dnvm install default -runtime CoreCLR -arch x86
+IF "%1" == "rebuild-package" goto run
+
+IF "%SKIP_DNX_INSTALL%"=="1" (
+    REM On the CI, don't upgrade since the previous installed DNVM is already there.
+	echo Using default
+    CALL packages\KoreBuild\build\dnvm use default -runtime CLR -arch x86
+) ELSE (
+	echo Upgrading
+    CALL packages\KoreBuild\build\dnvm upgrade -Unstable -runtime CLR -arch x86
+)
 
 :run
 CALL packages\KoreBuild\build\dnvm use default -runtime CLR -arch x86
