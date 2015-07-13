42b9435e8fc4a31aa6692b98405ef68f1be817a6
lbargaoanu <lbargaoanu@lucian.com>
2015-07-13 15:10:29 +0300

    check if we are able to construct the destination object

---
+++ src/UnitTests/Bug/ConstructUsingReturnsNull.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using Should;
+using System;
+
+namespace AutoMapper.UnitTests.Bug
+{
+    public class ConstructUsingReturnsNull : AutoMapperSpecBase
+    {
+        class Source
+        {
+            public int Number { get; set; }
+        }
+        class Destination
+        {
+            public int Number { get; set; }
+        }
+
+        protected override void Establish_context()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Source, Destination>().ConstructUsing((Source source)=>null);
+            });
+        }
+
+        [Fact]
+        public void Should_throw_when_construct_using_returns_null()
+        {
+            new Action(() => Mapper.Map<Source, Destination>(new Source()))
+                .ShouldThrow<AutoMapperMappingException>(ex=>ex.InnerException.ShouldBeType<InvalidOperationException>());
+        }
+    }
+}
