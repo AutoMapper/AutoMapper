86a0359fb3659e57d33e9ad887b016929c853bf7
TylerCarlson1 <tycarlso@hotmail.com>
2015-07-19 01:20:11 -0500

    Example of 2 member conventions work together

--- src/UnitTests/ConvensionTest.cs
+++ src/UnitTests/ConvensionTest.cs
@@ -25,9 +25,6 @@ namespace AutoMapper.UnitTests
         [Fact]
         public void Fact()
         {
-
-            //Mapper.AddConvension().Postfix("Dto");
-
             Mapper.Initialize(cfg =>
             {
                 var profile = cfg.CreateProfile("New Profile");
@@ -66,11 +63,56 @@ namespace AutoMapper.UnitTests
                 AddConditionalObjectMapper().Where((s, d) => s.Name.Contains(d.Name) || d.Name.Contains(s.Name));
             }
         }
+
+        public class ToDTO : Profile
+        {
+            protected override void Configure()
+            {
+                AddMemberConfiguration().AddName<PrePostfixName>(
+                        _ => _.AddStrings(p => p.Postfixes, "Transfer")
+                            .AddStrings(p => p.DestinationPrefixes, "Trans"))
+                            .SetMemberInfo<FieldPropertyMemberInfo>();
+                AddConditionalObjectMapper().Where((s, d) => s.Name == d.Name + "Dto");
+            }
+        }
+        public class FromDTO : Profile
+        {
+            protected override void Configure()
+            {
+                AddMemberConfiguration().AddName<PrePostfixName>(
+                        _ => _.AddStrings(p => p.DestinationPostfixes, "Transfer")
+                            .AddStrings(p => p.Prefixes, "Trans"))
+                            .SetMemberInfo<FieldPropertyMemberInfo>();
+                AddConditionalObjectMapper().Where((s, d) => d.Name == s.Name + "Dto");
+            }
+        }
         public void Fact2()
         {
             Mapper.Initialize(cfg =>
             {
                 cfg.AddProfile<ConventionProfile>();
+            });
+
+            var a2 = Mapper.Map<ClientDto>(new Client() { Value = "Test", Transval = "test" });
+            a2.ValueTransfer.ShouldEqual("Test");
+            a2.val.ShouldEqual("test");
+
+            var a = Mapper.Map<Client>(new ClientDto() { ValueTransfer = "TestTransfer", val = "testTransfer" });
+            a.Value.ShouldEqual("TestTransfer");
+            a.Transval.ShouldEqual("testTransfer");
+
+            var clients = Mapper.Map<Client[]>(new[] { new ClientDto() });
+            Expression<Func<Client, bool>> expr = c => c.ID < 5;
+            var clientExp = Mapper.Map<Expression<Func<ClientDto, bool>>>(expr);
+        }
+
+        [Fact]
+        public void Fact3()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.AddProfile<ToDTO>();
+                cfg.AddProfile<FromDTO>();
             });
 
             var a2 = Mapper.Map<ClientDto>(new Client() { Value = "Test", Transval = "test" });
