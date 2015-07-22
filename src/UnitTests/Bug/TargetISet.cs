4288a0b9bece5f2f7cf17a90d6c8e061812b5b3b
lbargaoanu <lbargaoanu@lucian.com>
2015-07-22 14:21:40 +0300

    passing test

---
+++ src/UnitTests/Bug/TargetISet.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+using Should;
+using System;
+using System.Collections.Generic;
+
+namespace AutoMapper.UnitTests.Bug
+{
+    public class TargetISet : AutoMapperSpecBase
+    {
+        Destination _destination;
+        string[] _items = new[] { "one", "two", "three" };
+
+        public class Source
+        {
+            public IEnumerable<string> Items { get; set; }
+        }
+
+        class Destination
+        {
+            public ISet<string> Items { get; set; }
+        }
+
+        protected override void Establish_context()
+        {
+            Mapper.CreateMap<Source, Destination>();
+        }
+
+        protected override void Because_of()
+        {
+            _destination = Mapper.Map<Destination>(new Source { Items = _items });
+        }
+
+        [Fact]
+        public void Should_map_IEnumerable_to_ISet()
+        {
+            _destination.Items.SetEquals(_items).ShouldBeTrue();
+        }
+    }
+}
