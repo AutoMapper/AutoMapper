6fd59caa9490f692c2c3e6a61363bb01eddaf9f1
Jimmy Bogard <jimmy.bogard@gmail.com>
2015-08-04 20:29:53 -0500

    No clue

---
+++ src/AutoMapper/ICtorParamConfigurationExpression.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoMapper
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public interface ICtorParamConfigurationExpression<TSource>
+    {
+        void MapFrom<TMember>(Expression<Func<TSource, TMember>> sourceMember);
+    }
+}
