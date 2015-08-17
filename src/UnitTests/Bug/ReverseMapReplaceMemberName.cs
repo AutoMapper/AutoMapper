3638f6479c399f432c44e67734c16fe48bda179b
lbargaoanu <lbargaoanu@lucian.com>
2015-08-17 15:11:43 +0300

    a passing test

---
+++ src/UnitTests/Bug/ReverseMapReplaceMemberName.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+using Should;
+using System;
+
+namespace AutoMapper.UnitTests.Bug
+{
+    public class ReverseMapAndReplaceMemberName : AutoMapperSpecBase
+    {
+        const string SomeId = "someId";
+        const string SomeOtherId = "someOtherId";
+        private Source _source;
+        private Destination _destination;
+
+        class Source
+        {
+            public string AccountId { get; set; }
+        }
+        class Destination
+        {
+            public string UserId { get; set; }
+        }
+
+        protected override void Establish_context()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.ReplaceMemberName("Account", "User");
+                cfg.ReplaceMemberName("User", "Account");
+                cfg.CreateMap<Source, Destination>().ReverseMap();
+            });
+        }
+
+        protected override void Because_of()
+        {
+            _source = Mapper.Map<Destination, Source>(new Destination
+            {
+                UserId = SomeId
+            });
+            _destination = Mapper.Map<Source, Destination>(new Source
+            {
+                AccountId = SomeOtherId
+            });
+        }
+
+        [Fact]
+        public void Should_work_together()
+        {
+            _source.AccountId.ShouldEqual(SomeId);
+            _destination.UserId.ShouldEqual(SomeOtherId);
+        }
+    }
+}
