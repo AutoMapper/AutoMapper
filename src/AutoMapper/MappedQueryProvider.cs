0aeec40dc8ddbf98f7866d2c0060d2310c042e74
amarek <alexander.marek@outlook.com>
2015-09-04 19:07:05 +0200

    Added Support for OData

--- src/AutoMapper/MappedQueryProvider.cs
+++ src/AutoMapper/MappedQueryProvider.cs
@@ -13,7 +13,7 @@ namespace AutoMapper
     /// Provides an interceptable IQueryable
     /// Copied from http://blogs.msdn.com/b/alexj/archive/2010/03/01/tip-55-how-to-extend-an-iqueryable-by-wrapping-it.aspx
     /// </summary>
-    public class MappedQueryProvider<TSource> : IQueryProvider
+    public class MappedQueryProvider<TDestination, TSource> : IQueryProvider
     {
         private readonly IQueryable _underlyingQuery;
         private readonly IMappingEngine _engine;
@@ -35,27 +35,24 @@ namespace AutoMapper
 
         public static IQueryable<T> Map<T>(IQueryable<TSource> underlyingQuery, IMappingEngine engine, Action<Exception> onException = null, Action<IEnumerable<T>> onMaterialized = null)
         {
-            return new MappedQueryable<T, TSource>(new MappedQueryProvider<TSource>(underlyingQuery, engine, onException, (x) =>
+            return new MappedQueryable<T, TSource>(new MappedQueryProvider<T, TSource>(underlyingQuery, engine, onException, (x) =>
             {
                 if (onMaterialized != null) onMaterialized((IEnumerable<T>) x);
             }), 
+
             new T[0].AsQueryable().Expression);
         }
 
-        public IEnumerator<TElement> ExecuteQuery<TElement>(Expression expression)
+        public IEnumerator<TDestination> ExecuteQuery(Expression expression)
         {
             try
             {
-                var elementType = typeof (TElement);
-                if (typeof (TElement).IsGenericType())
-                    elementType = typeof (TElement).GetGenericArguments().Single();
-
-                var visitor = new QueryMapperVisitor(elementType, typeof (TSource), _underlyingQuery, _engine);
+                var visitor = new QueryMapperVisitor(typeof(TDestination), typeof (TSource), _underlyingQuery, _engine);
                 var expr = visitor.Visit(expression);
 
                 var newDestQuery = _underlyingQuery.Provider.CreateQuery<TSource>(expr);
 
-                return newDestQuery.ProjectTo<TElement>().GetEnumerator();
+                return newDestQuery.ProjectTo<TDestination>().GetEnumerator();
             }
             catch (Exception x)
             {
@@ -66,26 +63,61 @@ namespace AutoMapper
 
         public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
         {
-            return new MappedQueryable<TElement, TSource>(this, expression);
+            try
+            { 
+                Type et = typeof (TElement);
+                Type dt = typeof(TSource);
+                Type qt = typeof(MappedQueryable<,>).MakeGenericType(et, dt);
+                Type qpt = typeof(MappedQueryProvider<,>).MakeGenericType(et, dt);
+                object[] args = new object[] { this, expression };
+
+                ConstructorInfo ci = qt.GetConstructor(
+                    BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance,
+                    null,
+                    new Type[] {
+                        qpt,
+                        typeof(Expression)
+                    },
+                    null);
+
+                return (IQueryable<TElement>)ci.Invoke(args);
+            }
+            catch (Exception x)
+            {
+                _onException(x);
+                throw;
+            }
         }
 
         public IQueryable CreateQuery(Expression expression)
         {
-            Type et = TypeHelper.FindIEnumerable(expression.Type);
-            Type dt = typeof (TSource);
-            Type qt = typeof(MappedQueryable<,>).MakeGenericType(et, dt);
-            object[] args = new object[] { this, expression };
+            try
+            {
 
-            ConstructorInfo ci = qt.GetConstructor(
-                BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance,
-                null,
-                new Type[] {
-                typeof(MappedQueryProvider<TSource>),
-                typeof(Expression)
-            },
-                null);
+                Type et = TypeHelper.FindIEnumerable(expression.Type).GetGenericArguments().First();
+                Type dt = typeof (TSource);
+                Type qt = typeof (MappedQueryable<,>).MakeGenericType(et, dt);
+                Type qpt = typeof (MappedQueryProvider<,>).MakeGenericType(et, dt);
+                object[] args = new object[] {this, expression};
 
-            return (IQueryable)ci.Invoke(args);
+                ConstructorInfo ci = qt.GetConstructor(
+                    BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance,
+                    null,
+                    new Type[]
+                    {
+                        qpt,
+                        typeof (Expression)
+                    },
+                    null);
+
+                return (IQueryable) ci.Invoke(args);
+
+            }
+            catch (Exception x)
+            {
+                _onException(x);
+                throw;
+            }
         }
         public TResult Execute<TResult>(Expression expression)
         {
@@ -107,16 +139,20 @@ namespace AutoMapper
 
         public object Execute(Expression expression)
         {
-            throw new NotSupportedException();
-            
-            
-            //return this._underlyingProvider.Execute(InterceptExpr(expression));
-            
-            
-            //var visitor = new QueryMapperVisitor(typeof(TSource), typeof(object), _underlyingQuery, _engine);
-            //var expr = visitor.Visit(expression);
-            //var newDestQuery = _underlyingQuery.Provider.CreateQuery<TSource>(expr);
-            //return newDestQuery.SingleOrDefault();
+            try
+            {
+                var visitor = new QueryMapperVisitor(typeof(TDestination), typeof(TSource), _underlyingQuery, _engine);
+                var expr = visitor.Visit(expression);
+
+                var newDestQuery = _underlyingQuery.Provider.CreateQuery<TSource>(expr);
+
+                return newDestQuery.ProjectTo<TSource>().SingleOrDefault();
+            }
+            catch (Exception x)
+            {
+                _onException(x);
+                throw;
+            }
         }
     }
 }
