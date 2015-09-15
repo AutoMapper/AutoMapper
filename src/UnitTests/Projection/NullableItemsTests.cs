5a1cc6dfb97941609e14dfecc1f9a0912aed3173
Jason Holloway <miceeatnicerice@hotmail.com>
2015-09-15 02:49:57 +0100

    Fixed failing enumerable tests to set AllowNullCollections

--- src/UnitTests/Projection/NullableItemsTests.cs
+++ src/UnitTests/Projection/NullableItemsTests.cs
@@ -14,8 +14,40 @@ namespace AutoMapper.UnitTests.Projection
         {
             public NullChildItemTest()
             {
-                Mapper.Initialize(cfg => cfg.CreateMap<Parent, ParentDto>());
+                Mapper.Initialize(cfg => {
+                    cfg.AllowNullDestinationValues = true;
+                    cfg.AllowNullCollections = true;
+
+                    cfg.CreateMap<Parent, ParentDto>();
+                    cfg.CreateMap<SerialParent, SerialParentDto>();
+                    cfg.CreateMap<Child, ChildDto>();
+                });
             }
+
+            [Fact]
+            public void Should_project_null_collection() 
+            {
+                var items = new[] {
+                                new SerialParent()
+                            };
+
+                var projected = items.AsQueryable().ProjectTo<SerialParentDto>().ToList();
+
+                projected[0].Children.ShouldBeNull();
+            }
+
+            class SerialParent
+            {
+                public Child[] Children { get; set; }
+            }
+
+            class SerialParentDto
+            {
+                public ChildDto[] Children { get; set; }
+            }
+
+
+
 
             [Fact]
             public void Should_project_null_value()
@@ -33,7 +65,6 @@ namespace AutoMapper.UnitTests.Projection
                 projected[0].Value.ShouldEqual(5);
                 projected[0].ChildValue.ShouldBeNull();
                 projected[0].ChildGrandChildValue.ShouldBeNull();
-                projected[0].Nephews.ShouldBeNull();
             }
 
 
@@ -43,7 +74,6 @@ namespace AutoMapper.UnitTests.Projection
                 public int? ChildValue { get; set; }
                 public int? ChildGrandChildValue { get; set; }
                 public DateTime? Date { get; set; }
-                public Child[] Nephews { get; set; }
             }
 
 
@@ -51,13 +81,17 @@ namespace AutoMapper.UnitTests.Projection
             {
                 public int Value { get; set; }
                 public Child Child { get; set; }
-                public Child[] Nephews { get; set; }
             }
 
             public class Child
             {
                 public int Value { get; set; }
                 public GrandChild GrandChild { get; set; }
+            }
+
+            public class ChildDto
+            {
+                public int Value { get; set; }
             }
 
             public class GrandChild
