a1481622de4dd249173aacfe49b521c16fa79c48
lbargaoanu <lbargaoanu@lucian.com>
2015-09-15 12:00:59 +0300

    tests

--- src/UnitTests/Mappers/DynamicMapperTests.cs
+++ src/UnitTests/Mappers/DynamicMapperTests.cs
@@ -1,5 +1,8 @@
-﻿using Should;
+﻿using System;
 using System.Dynamic;
+using Microsoft.CSharp.RuntimeBinder;
+using Should;
+using Should.Core.Assertions;
 using Xunit;
 
 namespace AutoMapper.UnitTests.Mappers
@@ -10,7 +13,24 @@ namespace AutoMapper.UnitTests.Mappers
         public string Bar { get; set; }
     }
 
-    public class When_mapping_from_dynamic_with_missing_property : NonValidatingSpecBase
+    public class When_mapping_to_dynamic : NonValidatingSpecBase
+    {
+        dynamic _destination;
+
+        protected override void Because_of()
+        {
+            _destination = Mapper.Map<ExpandoObject>(new Destination { Foo = "Foo", Bar = "Bar" });
+        }
+
+        [Fact]
+        public void Should_map_source_properties()
+        {
+            Assert.Equal("Foo", _destination.Foo);
+            Assert.Equal("Bar", _destination.Bar);
+        }
+    }
+
+    public class When_mapping_from_dynamic : NonValidatingSpecBase
     {
         Destination _destination;
 
@@ -18,13 +38,31 @@ namespace AutoMapper.UnitTests.Mappers
         {
             dynamic source = new ExpandoObject();
             source.Foo = "Foo";
+            source.Bar = "Bar";
             _destination = Mapper.Map<Destination>(source);
         }
 
         [Fact]
-        public void Should_map_the_other_properties()
+        public void Should_map_destination_properties()
         {
             _destination.Foo.ShouldEqual("Foo");
+            _destination.Bar.ShouldEqual("Bar");
+        }
+    }
+
+    public class When_mapping_from_dynamic_with_missing_property : NonValidatingSpecBase
+    {
+        [Fact]
+        public void Should_throw()
+        {
+            dynamic source = new ExpandoObject();
+            source.Foo = "Foo";
+            new Action(()=>Mapper.Map<Destination>(source)).ShouldThrow<AutoMapperMappingException>(ex =>
+            {
+                var innerException = ex.InnerException;
+                innerException.ShouldBeType<RuntimeBinderException>();
+                innerException.Message.EndsWith("'Bar'").ShouldBeTrue();
+            });
         }
     }
 }
