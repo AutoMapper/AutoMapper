599350141be1834d40d6fd72e7a5db83afeb6c8a
Jimmy Bogard <jimmy.bogard@gmail.com>
2015-10-09 07:22:53 -0500

    Trying to fix build

--- default.ps1
+++ default.ps1
@@ -35,7 +35,7 @@ task release {
 task compile -depends clean { 
     exec { dnu restore }
     exec { dnu build $source_dir\AutoMapper --configuration $config}
-    exec { & $source_dir\.nuget\Nuget.exe restore $source_dir\AutoMapper.sln }
+    exec { & $source_dir\.nuget\Nuget.exe restore $source_dir\AutoMapper.NoProjectJson.sln }
     exec { msbuild /t:Clean /t:Build /p:Configuration=$config /v:q /p:NoWarn=1591 /nologo $source_dir\AutoMapper.sln }
 }
 
