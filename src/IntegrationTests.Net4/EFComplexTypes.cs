cec562ca223ef8d5ac00acaa49ea6221b0069a2d
lbargaoanu <lbargaoanu@lucian.com>
2015-10-22 13:59:08 +0300

    the test

---
+++ src/IntegrationTests.Net4/EFComplexTypes.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using Xunit;
+using Should;
+using AutoMapper.UnitTests;
+using AutoMapper.QueryableExtensions;
+
+namespace AutoMapper.IntegrationTests.Net4
+{
+    public class EFComplexTypes : AutoMapperSpecBase
+    {
+        class Initializer : DropCreateDatabaseAlways<ClientContext>
+        {
+        }
+
+        class ClientContext : DbContext
+        {
+            public ClientContext()
+            {
+                Database.SetInitializer(new Initializer());
+            }
+
+            public virtual DbSet<Entity> Entities { get; set; }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+
+                modelBuilder.Entity<Entity>().HasKey(x => x.Id);
+                modelBuilder.ComplexType<ComplexType>();
+            }
+        }
+
+        class Entity
+        {
+            public virtual int Id { get; set; }
+
+            public virtual ComplexType ComplexType { get; set; }
+        }
+
+        class ComplexType : INotNull
+        {
+            public virtual string Field { get; set; }
+        }
+
+        class MappedEntity
+        {
+            public virtual int Id { get; set; }
+
+            public virtual MappedComplexType ComplexType { get; set; }
+        }
+
+        class MappedComplexType
+        {
+            public virtual string Field { get; set; }
+        }
+
+        protected override void Establish_context()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Entity, MappedEntity>();
+                cfg.CreateMap<ComplexType, MappedComplexType>();
+            });
+        }
+
+        [Fact]
+        public void Should_handle_complex_types()
+        {
+            using(var clientContext = new ClientContext())
+            {
+                var entity = clientContext.Set<Entity>().ProjectTo<MappedEntity>().SingleOrDefault(x => x.Id == 1);
+            }
+        }
+    }
+}
