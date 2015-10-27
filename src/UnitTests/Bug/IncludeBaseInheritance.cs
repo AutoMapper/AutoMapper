b2c0232a76d072881e3f9e8661a897c1314f63e1
lbargaoanu <lbargaoanu@lucian.com>
2015-10-27 14:39:55 +0200

    include the base classes too

---
+++ src/UnitTests/Bug/IncludeBaseInheritance.cs
@@ -0,0 +1,77 @@
+﻿using Xunit;
+using Should;
+using System;
+
+namespace AutoMapper.UnitTests.Bug
+{
+    public class IncludeBaseInheritance : AutoMapperSpecBase
+    {
+        DestinationLevel2 _destination;
+
+        public class SourceLevel0
+        {
+            public string SPropertyLevel0 { get; set; }
+        }
+
+        public class SourceLevel1 : SourceLevel0
+        {
+            public string SPropertyLevel1 { get; set; }
+        }
+
+        public class SourceLevel2 : SourceLevel1
+        {
+            public string DPropertyLevel0 { get; set; }
+            public string SPropertyLevel2 { get; set; }
+        }
+
+        public class DestinationLevel0
+        {
+            public string DPropertyLevel0 { get; set; }
+        }
+
+        public class DestinationLevel1 : DestinationLevel0
+        {
+            public string DPropertyLevel1 { get; set; }
+        }
+
+        public class DestinationLevel2 : DestinationLevel1
+        {
+            public string DPropertyLevel2 { get; set; }
+        }
+
+        protected override void Establish_context()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<SourceLevel0, DestinationLevel0>()
+                    .ForMember(dest => dest.DPropertyLevel0, o => o.ResolveUsing(src => src.SPropertyLevel0));
+
+                cfg.CreateMap<SourceLevel1, DestinationLevel1>()
+                    .IncludeBase<SourceLevel0, DestinationLevel0>()
+                    .ForMember(dest => dest.DPropertyLevel1, o => o.ResolveUsing(src => src.SPropertyLevel1));
+
+                cfg.CreateMap<SourceLevel2, DestinationLevel2>()
+                    .IncludeBase<SourceLevel1, DestinationLevel1>()
+                    .ForMember(dest => dest.DPropertyLevel2, o => o.ResolveUsing(src => src.SPropertyLevel2));
+            });
+        }
+
+        protected override void Because_of()
+        {
+            var source = new SourceLevel2
+            {
+                SPropertyLevel0 = "SPropertyLevel0",
+                SPropertyLevel1 = "SPropertyLevel1",
+                SPropertyLevel2 = "SPropertyLevel2",
+                DPropertyLevel0 = "DPropertyLevel0",
+            };
+            _destination = Mapper.Map<DestinationLevel2>(source);
+        }
+
+        [Fact]
+        public void Should_inherit_resolvers()
+        {
+            _destination.DPropertyLevel0.ShouldEqual("SPropertyLevel0");
+        }
+    }
+}
