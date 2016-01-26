0b03e7cbdef34383f969d89798ac6e140b573717
Lucian Bargaoanu <lbargaoanu>
2016-01-26 14:22:40 +0200

    passing test; closes #1046

---
+++ src/UnitTests/MappingInheritance/MapToBaseClass.cs
@@ -0,0 +1,31 @@
+﻿namespace AutoMapper.UnitTests.MappingInheritance
+{
+    using Should;
+    using Xunit;
+    public class MapToBaseClass : SpecBase
+    {
+        A _destination;
+
+        public class Input { }
+        public class A { }
+        public class B : A { }
+
+        protected override void Establish_context()
+        {
+            Mapper.CreateMap<Input, A>().Include<Input, B>();
+            Mapper.CreateMap<Input, B>();
+            Mapper.AssertConfigurationIsValid();
+        }
+
+        protected override void Because_of()
+        {
+            _destination = Mapper.Map<A>(new Input());
+        }
+
+        [Fact]
+        public void ShouldReturnBaseClass()
+        {
+            _destination.ShouldBeType<A>();
+        }
+    }
+}
