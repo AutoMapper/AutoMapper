605e6b51508440f480036cc50339d5c172f9fdd2
Igor Kiselev <igor@kiselev.spb.ru>
2016-01-26 14:26:09 -0800

    Compare first interface implementation type (#1037).

--- src/AutoMapper/MapperConfiguration.cs
+++ src/AutoMapper/MapperConfiguration.cs
@@ -399,6 +399,21 @@ namespace AutoMapper
 
         private IEnumerable<Type> GetAllTypes(Type type)
         {
+            var typeInheritance = GetTypeInheritance(type);
+            foreach (var item in typeInheritance)
+                yield return item;
+
+            var interfaceComparer = new InterfaceComparer(type);
+            var allInterfaces = type.GetTypeInfo().ImplementedInterfaces.OrderByDescending(t => t, interfaceComparer);
+
+            foreach (var interfaceType in allInterfaces)
+            {
+                yield return interfaceType;
+            }
+        }
+
+        private static IEnumerable<Type> GetTypeInheritance(Type type)
+        {
             yield return type;
 
             Type baseType = type.BaseType();
@@ -406,13 +421,6 @@ namespace AutoMapper
             {
                 yield return baseType;
                 baseType = baseType.BaseType();
-            }
-
-            var allInterfaces = type.GetTypeInfo().ImplementedInterfaces.OrderByDescending(t => t, new InterfaceComparer());
-
-            foreach (var interfaceType in allInterfaces)
-            {
-                yield return interfaceType;
             }
         }
 
@@ -536,20 +544,46 @@ namespace AutoMapper
         }
         private class InterfaceComparer : IComparer<Type>
         {
+            private readonly List<TypeInfo> _typeInheritance;
+
+            public InterfaceComparer(Type target)
+            {
+                _typeInheritance = GetTypeInheritance(target).Select(type => type.GetTypeInfo()).Reverse().ToList();
+            }
+
             public int Compare(Type x, Type y)
             {
-                var left = x.IsAssignableFrom(y);
-                var right = y.IsAssignableFrom(x);
-                if (left & !right)
+                var xLessOrEqualY = x.IsAssignableFrom(y);
+                var yLessOrEqualX = y.IsAssignableFrom(x);
+
+                if (xLessOrEqualY & !yLessOrEqualX)
                 {
                     return -1;
                 }
-                if (!left & right)
+                if (!xLessOrEqualY & yLessOrEqualX)
                 {
                     return 1;
                 }
+                if (xLessOrEqualY & yLessOrEqualX)
+                {
+                    return 0;
+                }
+
+                var xFirstIntroduceTypeIndex = _typeInheritance.FindIndex(type => type.ImplementedInterfaces.Contains(x));
+                var yFirstIntroduceTypeIndex = _typeInheritance.FindIndex(type => type.ImplementedInterfaces.Contains(y));
+
+                if (xFirstIntroduceTypeIndex < yFirstIntroduceTypeIndex)
+                {
+                    return -1;
+                }
+                if (yFirstIntroduceTypeIndex > xFirstIntroduceTypeIndex)
+                {
+                    return 1;
+                }
+
                 return 0;
             }
         }
+
     }
 }
