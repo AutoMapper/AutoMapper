f4fb36b98513ea97ab1a1e3276d470066a1b66f6
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-02-05 09:35:19 -0600

    All tests passing

--- src/UnitTests/Bug/ExpressionMapping.cs
+++ src/UnitTests/Bug/ExpressionMapping.cs
@@ -260,14 +260,14 @@ namespace AutoMapper.UnitTests.Bug
             _valid = new Parent { DateTime = DateTime.Now };
         }
 
-        [Fact(Skip = "Failing test")]
+        [Fact]
         public void When_Using_Non_TypeMapped_Class_Property_Against_Constant()
         {
             _predicateExpression = p => p.DateTime.Year.ToString() == "2015";
             _valid = new Parent { DateTime = new DateTime(2015, 1, 1) };
         }
 
-        [Fact(Skip = "Failing test")]
+        [Fact]
         public void When_Using_Non_TypeMapped_Class_Method_Against_Constant()
         {
             _predicateExpression = p => p.DateTime.Year.ToString().Equals("2015");
--- src/UnitTests/Bug/ReportMissingInclude.cs
+++ src/UnitTests/Bug/ReportMissingInclude.cs
@@ -4,17 +4,13 @@ using System;
 
 namespace AutoMapper.UnitTests.Bug
 {
-    public class ReportMissingInclude : NonValidatingSpecBase
+    public class ReportMissingInclude : SpecBase
     {
-        protected override MapperConfiguration Configuration { get; } = new MapperConfiguration(cfg =>
-        {
-            cfg.CreateMap<object, BaseType>().Include<object, ChildType>();
-        });
-
-        [Fact(Skip = "Broken in 5.0")]
+        [Fact]
         public void ShouldDiscoverMissingMappingsInIncludedType()
         {
-            new Action(Configuration.AssertConfigurationIsValid).ShouldThrow<InvalidOperationException>(ex=>ex.Message.ShouldStartWith("Missing map from Object to BaseType."));
+            new Action(() => new MapperConfiguration(cfg => cfg.CreateMap<object, BaseType>().Include<object, ChildType>()))
+                .ShouldThrow<InvalidOperationException>(ex=>ex.Message.ShouldStartWith("Missing map from Object to BaseType."));
         }
 
         public class BaseType { }
