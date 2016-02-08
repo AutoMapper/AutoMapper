e5a1879d963deb2ad7e8183ecb113c54b8e3e64b
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-02-08 10:43:17 -0600

    A static readonly interface. You still must call Mapper.Initialize

--- src/AutoMapper/Mapper.cs
+++ src/AutoMapper/Mapper.cs
@@ -6,14 +6,128 @@ namespace AutoMapper
     {
         #region Static API
 
-        //public static IConfigurationProvider Configuration { get; private set; }
-        //public static IMapper Instance { get; private set; }
+        /// <summary>
+        /// Configuration provider for performing maps
+        /// </summary>
+        public static IConfigurationProvider Configuration { get; private set; }
 
-        //public static void Initialize(Action<IMapperConfiguration> config)
-        //{
-        //    Configuration = new MapperConfiguration(config);
-        //    Instance = new Mapper(Configuration);
-        //}
+        /// <summary>
+        /// Static mapper instance. You can also create a <see cref="Mapper"/> instance directly using the <see cref="Configuration"/> instance.
+        /// </summary>
+        public static IMapper Instance { get; private set; }
+
+        public static void Initialize(Action<IMapperConfiguration> config)
+        {
+            Configuration = new MapperConfiguration(config);
+            Instance = new Mapper(Configuration);
+        }
+
+        /// <summary>
+        /// Execute a mapping from the source object to a new destination object.
+        /// The source type is inferred from the source object.
+        /// </summary>
+        /// <typeparam name="TDestination">Destination type to create</typeparam>
+        /// <param name="source">Source object to map from</param>
+        /// <returns>Mapped destination object</returns>
+        public static TDestination Map<TDestination>(object source) => Instance.Map<TDestination>(source);
+
+        /// <summary>
+        /// Execute a mapping from the source object to a new destination object with supplied mapping options.
+        /// </summary>
+        /// <typeparam name="TDestination">Destination type to create</typeparam>
+        /// <param name="source">Source object to map from</param>
+        /// <param name="opts">Mapping options</param>
+        /// <returns>Mapped destination object</returns>
+        public static TDestination Map<TDestination>(object source, Action<IMappingOperationOptions> opts)
+            => Instance.Map<TDestination>(source, opts);
+
+        /// <summary>
+        /// Execute a mapping from the source object to a new destination object.
+        /// </summary>
+        /// <typeparam name="TSource">Source type to use, regardless of the runtime type</typeparam>
+        /// <typeparam name="TDestination">Destination type to create</typeparam>
+        /// <param name="source">Source object to map from</param>
+        /// <returns>Mapped destination object</returns>
+        public static TDestination Map<TSource, TDestination>(TSource source)
+            => Instance.Map<TSource, TDestination>(source);
+
+        /// <summary>
+        /// Execute a mapping from the source object to a new destination object with supplied mapping options.
+        /// </summary>
+        /// <typeparam name="TSource">Source type to use</typeparam>
+        /// <typeparam name="TDestination">Destination type to create</typeparam>
+        /// <param name="source">Source object to map from</param>
+        /// <param name="opts">Mapping options</param>
+        /// <returns>Mapped destination object</returns>
+        public static TDestination Map<TSource, TDestination>(TSource source, Action<IMappingOperationOptions<TSource, TDestination>> opts)
+            => Instance.Map(source, opts);
+
+        /// <summary>
+        /// Execute a mapping from the source object to the existing destination object.
+        /// </summary>
+        /// <typeparam name="TSource">Source type to use</typeparam>
+        /// <typeparam name="TDestination">Dsetination type</typeparam>
+        /// <param name="source">Source object to map from</param>
+        /// <param name="destination">Destination object to map into</param>
+        /// <returns>The mapped destination object, same instance as the <paramref name="destination"/> object</returns>
+        public static TDestination Map<TSource, TDestination>(TSource source, TDestination destination)
+            => Instance.Map(source, destination);
+
+        /// <summary>
+        /// Execute a mapping from the source object to the existing destination object with supplied mapping options.
+        /// </summary>
+        /// <typeparam name="TSource">Source type to use</typeparam>
+        /// <typeparam name="TDestination">Destination type</typeparam>
+        /// <param name="source">Source object to map from</param>
+        /// <param name="destination">Destination object to map into</param>
+        /// <param name="opts">Mapping options</param>
+        /// <returns>The mapped destination object, same instance as the <paramref name="destination"/> object</returns>
+        public static TDestination Map<TSource, TDestination>(TSource source, TDestination destination, Action<IMappingOperationOptions<TSource, TDestination>> opts)
+            => Instance.Map(source, destination, opts);
+
+        /// <summary>
+        /// Execute a mapping from the source object to a new destination object with explicit <see cref="System.Type"/> objects
+        /// </summary>
+        /// <param name="source">Source object to map from</param>
+        /// <param name="sourceType">Source type to use</param>
+        /// <param name="destinationType">Destination type to create</param>
+        /// <returns>Mapped destination object</returns>
+        public static object Map(object source, Type sourceType, Type destinationType)
+            => Instance.Map(source, sourceType, destinationType);
+
+        /// <summary>
+        /// Execute a mapping from the source object to a new destination object with explicit <see cref="System.Type"/> objects and supplied mapping options.
+        /// </summary>
+        /// <param name="source">Source object to map from</param>
+        /// <param name="sourceType">Source type to use</param>
+        /// <param name="destinationType">Destination type to create</param>
+        /// <param name="opts">Mapping options</param>
+        /// <returns>Mapped destination object</returns>
+        public static object Map(object source, Type sourceType, Type destinationType, Action<IMappingOperationOptions> opts)
+            => Instance.Map(source, sourceType, destinationType, opts);
+
+        /// <summary>
+        /// Execute a mapping from the source object to existing destination object with explicit <see cref="System.Type"/> objects
+        /// </summary>
+        /// <param name="source">Source object to map from</param>
+        /// <param name="destination">Destination object to map into</param>
+        /// <param name="sourceType">Source type to use</param>
+        /// <param name="destinationType">Destination type to use</param>
+        /// <returns>Mapped destination object, same instance as the <paramref name="destination"/> object</returns>
+        public static object Map(object source, object destination, Type sourceType, Type destinationType)
+            => Instance.Map(source, destination, sourceType, destinationType);
+
+        /// <summary>
+        /// Execute a mapping from the source object to existing destination object with supplied mapping options and explicit <see cref="System.Type"/> objects
+        /// </summary>
+        /// <param name="source">Source object to map from</param>
+        /// <param name="destination">Destination object to map into</param>
+        /// <param name="sourceType">Source type to use</param>
+        /// <param name="destinationType">Destination type to use</param>
+        /// <param name="opts">Mapping options</param>
+        /// <returns>Mapped destination object, same instance as the <paramref name="destination"/> object</returns>
+        public static object Map(object source, object destination, Type sourceType, Type destinationType, Action<IMappingOperationOptions> opts)
+            => Instance.Map(source, destination, sourceType, destinationType, opts);
 
         #endregion
 
---
+++ src/UnitTests/StaticMapping.cs
@@ -0,0 +1,33 @@
+﻿namespace AutoMapper.UnitTests
+{
+    using Should;
+    using Xunit;
+
+    public class StaticMapping
+    {
+        public class Source
+        {
+            public int Value { get; set; }
+        }
+
+        public class Dest
+        {
+            public int Value { get; set; }
+        }
+
+        [Fact]
+        public void Can_map_statically()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Source, Dest>();
+            });
+
+            var source = new Source {Value = 5};
+
+            var dest = Mapper.Map<Source, Dest>(source);
+
+            dest.Value.ShouldEqual(source.Value);
+        } 
+    }
+}
