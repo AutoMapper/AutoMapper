9b9074f85e84bf377e0c730ecf0f6730f85462f5
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-04-23 23:02:36 -0500

    Removing some dead code

--- src/AutoMapper/ResolutionContext.cs
+++ src/AutoMapper/ResolutionContext.cs
@@ -8,7 +8,7 @@ namespace AutoMapper
     /// <summary>
     /// Context information regarding resolution of a destination value
     /// </summary>
-    public class ResolutionContext : IEquatable<ResolutionContext>
+    public class ResolutionContext
     {
         private Dictionary<object, object> _instanceCache;
 
@@ -30,12 +30,12 @@ namespace AutoMapper
         /// <summary>
         /// Source value
         /// </summary>
-        public object SourceValue { get; private set; }
+        public object SourceValue { get; }
 
         /// <summary>
         /// Destination value
         /// </summary>
-        public object DestinationValue { get; private set; }
+        public object DestinationValue { get; }
 
         /// <summary>
         /// Parent resolution context
@@ -49,14 +49,11 @@ namespace AutoMapper
         {
             get
             {
-                if(_instanceCache != null)
+                if (_instanceCache != null)
                 {
                     return _instanceCache;
                 }
-                //if(TypeMap?.PreserveReferences ?? false)
-                //{
-                    _instanceCache = new Dictionary<object, object>();
-                //}
+                _instanceCache = new Dictionary<object, object>();
                 return _instanceCache;
             }
         }
@@ -79,13 +76,14 @@ namespace AutoMapper
         /// <summary>
         /// Source and destination type pair
         /// </summary>
-        public TypePair Types { get; private set; }
+        public TypePair Types { get; }
 
         public bool IsSourceValueNull => Equals(null, SourceValue);
 
         public ResolutionContext(object source, object destination, TypePair types, ResolutionContext parent)
-            : this(source, destination)
         {
+            SourceValue = source;
+            DestinationValue = destination;
             Parent = parent;
             Options = parent.Options;
             Mapper = parent.Mapper;
@@ -95,62 +93,16 @@ namespace AutoMapper
             Types = types;
         }
 
-        internal ResolutionContext(ResolutionContext parent)
-        {
-            Parent = parent;
-            Options = parent.Options;
-            Mapper = parent.Mapper;
-            _instanceCache = parent.InstanceCache;
-        }
-
-        internal void Fill(object source, object destination, TypePair types, TypeMap typeMap)
-        {
-           Types = types;
-           SourceValue = source;
-           DestinationValue = destination;
-        }
-
         public ResolutionContext(object source, object destination, TypePair types, MappingOperationOptions options, IRuntimeMapper mapper)
-            : this(source, destination)
         {
+            SourceValue = source;
+            DestinationValue = destination;
             Options = options;
             Mapper = mapper;
             Types = types;
         }
 
-        private ResolutionContext(object source, object destination)
-        {
-            SourceValue = source;
-            DestinationValue = destination;
-        }
-
         public override string ToString() => $"Trying to map {SourceType.Name} to {DestinationType.Name}.";
-
-        public bool Equals(ResolutionContext other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return Equals(other.Types, Types) &&
-                   Equals(other.SourceValue, SourceValue);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != typeof (ResolutionContext)) return false;
-            return Equals((ResolutionContext) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int result = Types.GetHashCode();
-                result = (result*397) ^ (SourceValue?.GetHashCode() ?? 0);
-                return result;
-            }
-        }
 
         public TypeMap GetContextTypeMap()
         {
@@ -172,7 +124,7 @@ namespace AutoMapper
         protected IEnumerable<ResolutionContext> GetContextsCore()
         {
             var context = this;
-            while(context.Parent != null)
+            while (context.Parent != null)
             {
                 yield return context;
                 context = context.Parent;
@@ -182,7 +134,7 @@ namespace AutoMapper
 
         public void BeforeMap(object destination)
         {
-            if(Parent == null)
+            if (Parent == null)
             {
                 Options.BeforeMapAction(SourceValue, destination);
             }
@@ -190,7 +142,7 @@ namespace AutoMapper
 
         public void AfterMap(object destination)
         {
-            if(Parent == null)
+            if (Parent == null)
             {
                 Options.AfterMapAction(SourceValue, destination);
             }
