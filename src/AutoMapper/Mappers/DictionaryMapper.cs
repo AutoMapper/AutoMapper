ae4c28c142cb780902f828ad5b6a437618145c2a
Tyler Carlson <tycarlso@hotmail.com>
2016-04-29 12:21:10 -0500

    please build

--- src/AutoMapper/Mappers/DictionaryMapper.cs
+++ src/AutoMapper/Mappers/DictionaryMapper.cs
@@ -3,7 +3,6 @@ using System.Linq.Expressions;
 namespace AutoMapper.Mappers
 {
     using System;
-    using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
