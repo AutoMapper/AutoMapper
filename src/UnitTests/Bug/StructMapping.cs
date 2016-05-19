1e377955a35f74a4b17161f1c13d53e6cd8f0e21
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2016-05-19 16:31:03 +0300

    I believe this is useful only for classes

---
+++ src/UnitTests/Bug/StructMapping.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+using Should;
+using System;
+
+namespace AutoMapper.UnitTests.Bug
+{
+    public class StructMapping : AutoMapperSpecBase
+    {
+        private Destination _destination;
+
+        struct Source
+        {
+            public int Number { get; set; }
+        }
+        class Destination
+        {
+            public int Number { get; set; }
+        }
+
+        protected override MapperConfiguration Configuration { get; } = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<Source, Destination>();
+        });
+
+        protected override void Because_of()
+        {
+            var source = new Source
+            {
+                Number = 23
+            };
+            _destination = Mapper.Map<Source, Destination>(source);
+        }
+
+        [Fact]
+        public void Should_work()
+        {
+            _destination.Number.ShouldEqual(23);
+        }
+    }
+}
