b151a33786f1349628d06850d0a4cab5d67739b3
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-05-25 08:54:43 -0500

    Test for #1307

--- src/UnitTests/ValueTypes.cs
+++ src/UnitTests/ValueTypes.cs
@@ -46,6 +46,42 @@ namespace AutoMapper.UnitTests
 			}
 		}
 
+	    public class When_source_struct_config_has_custom_mappings : AutoMapperSpecBase
+	    {
+            public struct matrixDigiInStruct1
+            {
+                public ushort CNCinfo;
+                public ushort Reg1;
+                public ushort Reg2;
+            }
+            public class DigiIn1
+            {
+                public ushort CncInfo { get; set; }
+                public ushort Reg1 { get; set; }
+                public ushort Reg2 { get; set; }
+            }
+
+	        protected override MapperConfiguration Configuration { get; } = new MapperConfiguration(
+	            cfg => cfg.CreateMap<matrixDigiInStruct1, DigiIn1>()
+	                .ForMember(d => d.CncInfo, x => x.MapFrom(s => s.CNCinfo)));
+
+	        [Fact]
+	        public void Should_map_correctly()
+	        {
+	            var source = new matrixDigiInStruct1
+	            {
+	                CNCinfo = 5,
+                    Reg1 = 6,
+                    Reg2 = 7
+	            };
+	            var dest = Mapper.Map<matrixDigiInStruct1, DigiIn1>(source);
+
+                dest.CncInfo.ShouldEqual(source.CNCinfo);
+                dest.Reg1.ShouldEqual(source.Reg1);
+                dest.Reg2.ShouldEqual(source.Reg2);
+	        }
+	    }
+
 
         public class When_destination_type_is_a_nullable_value_type : AutoMapperSpecBase
         {
