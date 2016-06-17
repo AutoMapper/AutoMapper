64c97b0dbca5c0c14ee1284c553aa0a7733967ec
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-06-17 10:07:10 -0500

    Removing enumerable mapper map

--- src/AutoMapper/Mappers/EnumerableMapper.cs
+++ src/AutoMapper/Mappers/EnumerableMapper.cs
@@ -1,23 +1,13 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-
 namespace AutoMapper.Mappers
 {
     using System;
     using System.Collections;
-    using System.Reflection;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
     using Configuration;
 
     public class EnumerableMapper : IObjectMapper
     {
-
-        public object Map(ResolutionContext context)
-        {
-            var listType = typeof(List<>).MakeGenericType(TypeHelper.GetElementType(context.DestinationType));
-            return context.MapCollection(IfEditableList(Expression.Constant(listType)), typeof(List<>), CollectionMapperExtensions.MapItemMethodInfo, listType);
-        }
-
         public bool IsMatch(TypePair context)
         {
             // destination type must be IEnumerable interface or a class implementing at least IList 
@@ -26,16 +16,21 @@ namespace AutoMapper.Mappers
                    && context.SourceType.IsEnumerableType();
         }
 
-        public Expression MapExpression(TypeMapRegistry typeMapRegistry, IConfigurationProvider configurationProvider, PropertyMap propertyMap, Expression sourceExpression, Expression destExpression, Expression contextExpression)
+        public Expression MapExpression(TypeMapRegistry typeMapRegistry, IConfigurationProvider configurationProvider,
+            PropertyMap propertyMap, Expression sourceExpression, Expression destExpression,
+            Expression contextExpression)
         {
             var listType = typeof(List<>).MakeGenericType(TypeHelper.GetElementType(destExpression.Type));
 
-            return typeMapRegistry.MapCollectionExpression(configurationProvider, propertyMap, sourceExpression, Expression.Default(listType), contextExpression, IfEditableList, typeof(List<>), CollectionMapperExtensions.MapItemExpr);
+            return typeMapRegistry.MapCollectionExpression(configurationProvider, propertyMap, sourceExpression,
+                Expression.Default(listType), contextExpression, IfEditableList, typeof(List<>),
+                CollectionMapperExtensions.MapItemExpr);
         }
 
         private static Expression IfEditableList(Expression dest)
         {
-            return Expression.And(Expression.TypeIs(dest, typeof(IList)), Expression.Not(Expression.TypeIs(dest, typeof(Array))));
+            return Expression.And(Expression.TypeIs(dest, typeof(IList)),
+                Expression.Not(Expression.TypeIs(dest, typeof(Array))));
         }
     }
 }
--- src/AutoMapper/Mappers/EnumerableToDictionaryMapper.cs
+++ src/AutoMapper/Mappers/EnumerableToDictionaryMapper.cs
@@ -1,27 +1,24 @@
-using System.Linq.Expressions;
-
 namespace AutoMapper.Mappers
 {
-    using System;
-    using System.Collections;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Reflection;
+    using System.Linq.Expressions;
     using Configuration;
 
     public class EnumerableToDictionaryMapper : IObjectMapper
     {
         public bool IsMatch(TypePair context)
         {
-            return (context.DestinationType.IsDictionaryType())
-                   && (context.SourceType.IsEnumerableType())
-                   && (!context.SourceType.IsDictionaryType());
+            return context.DestinationType.IsDictionaryType()
+                   && context.SourceType.IsEnumerableType()
+                   && !context.SourceType.IsDictionaryType();
         }
 
-        public object Map(ResolutionContext context)
-            => context.MapCollection(CollectionMapperExtensions.IfNotNull(Expression.Constant(context.DestinationValue)), typeof(Dictionary<,>), CollectionMapperExtensions.MapItemMethodInfo);
-
-        public Expression MapExpression(TypeMapRegistry typeMapRegistry, IConfigurationProvider configurationProvider, PropertyMap propertyMap, Expression sourceExpression, Expression destExpression, Expression contextExpression)
-            => typeMapRegistry.MapCollectionExpression(configurationProvider, propertyMap, sourceExpression, destExpression, contextExpression, CollectionMapperExtensions.IfNotNull, typeof(Dictionary<,>), CollectionMapperExtensions.MapItemExpr);
+        public Expression MapExpression(TypeMapRegistry typeMapRegistry, IConfigurationProvider configurationProvider,
+                PropertyMap propertyMap, Expression sourceExpression, Expression destExpression,
+                Expression contextExpression)
+            =>
+            typeMapRegistry.MapCollectionExpression(configurationProvider, propertyMap, sourceExpression, destExpression,
+                contextExpression, CollectionMapperExtensions.IfNotNull, typeof(Dictionary<,>),
+                CollectionMapperExtensions.MapItemExpr);
     }
 }
