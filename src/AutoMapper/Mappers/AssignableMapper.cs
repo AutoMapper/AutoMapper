733471a3c6fe4f4921901210a0021672b98f8023
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-06-17 08:46:03 -0500

    Removing assignable mapper map

--- src/AutoMapper/Mappers/AssignableMapper.cs
+++ src/AutoMapper/Mappers/AssignableMapper.cs
@@ -4,16 +4,6 @@ namespace AutoMapper.Mappers
 {
     public class AssignableMapper : IObjectMapper
     {
-        public object Map(ResolutionContext context)
-        {
-            if (context.SourceValue == null && !context.Mapper.ShouldMapSourceValueAsNull(context))
-            {
-                return context.Mapper.CreateObject(context);
-            }
-
-            return context.SourceValue;
-        }
-
         public bool IsMatch(TypePair context)
         {
             return context.DestinationType.IsAssignableFrom(context.SourceType);
