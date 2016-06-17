aa971e0d1808b37f02f28da083513f7c18104560
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-06-17 08:45:21 -0500

    Removing array mapper map

--- src/AutoMapper/Mappers/ArrayMapper.cs
+++ src/AutoMapper/Mappers/ArrayMapper.cs
@@ -12,28 +12,7 @@ namespace AutoMapper.Mappers
 
     public class ArrayMapper : IObjectMapper
     {
-        public static TDestinationElement[] Map<TSource, TSourceElement, TDestinationElement>(TSource source, ResolutionContext context)
-            where TSource : IEnumerable
-        {
-            if (source == null)
-                return context.Mapper.ShouldMapSourceCollectionAsNull(context) ? null : new TDestinationElement[0];
-            
-            if (context.DestinationType.IsAssignableFrom(context.SourceType))
-            {
-                var elementTypeMap = context.ConfigurationProvider.ResolveTypeMap(typeof(TSourceElement), typeof(TDestinationElement));
-                if (elementTypeMap == null)
-                    return source as TDestinationElement[];
-            }
-
-            var itemContext = new ResolutionContext(context);
-
-            return source.Cast<TSourceElement>()
-                .Select(item => itemContext.Map(item, default(TDestinationElement)))
-                .ToArray();
-        }
-
         private static readonly MethodInfo MapMethodInfo = typeof(ArrayMapper).GetAllMethods().First(_ => _.IsStatic);
-        private static readonly MethodInfo Map2MethodInfo = typeof(ArrayMapper).GetAllMethods().Where(_ => _.IsStatic).ElementAt(1);
         
         public static TDestination[] Map<TSource, TDestination>(IEnumerable<TSource> source, ResolutionContext context, Func<TSource, ResolutionContext, TDestination> newItemFunc)
         {
@@ -41,11 +20,6 @@ namespace AutoMapper.Mappers
 
             return source.Select(item => newItemFunc(item, itemContext))
                 .ToArray();
-        }
-
-        public object Map(ResolutionContext context)
-        {
-            return MapMethodInfo.MakeGenericMethod(context.SourceType, TypeHelper.GetElementType(context.SourceType, (IEnumerable)context.SourceValue), TypeHelper.GetElementType(context.DestinationType, (IEnumerable)context.DestinationValue)).Invoke(null, new [] { context.SourceValue, context });
         }
 
         public bool IsMatch(TypePair context)
@@ -76,7 +50,7 @@ namespace AutoMapper.Mappers
                                  : NewArrayBounds(destElementType, Constant(0));
             var itemExpr = typeMapRegistry.MapItemExpr(configurationProvider, propertyMap, sourceExpression.Type, destExpression.Type);
 
-            var mapExpr = Call(null, Map2MethodInfo.MakeGenericMethod(sourceElementType, destElementType), sourceExpression, contextExpression, itemExpr);
+            var mapExpr = Call(null, MapMethodInfo.MakeGenericMethod(sourceElementType, destElementType), sourceExpression, contextExpression, itemExpr);
 
             // return (source == null) ? ifNullExpr : Map<TSourceElement, TDestElement>(source, context);
             return Condition(Equal(sourceExpression, Constant(null)), ifNullExpr, mapExpr);
