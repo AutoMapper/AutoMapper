06b2d91614a70a33f8ab68b4c0fcc0a6f2975897
Tyler Carlson <tycarlso@hotmail.com>
2016-06-24 13:52:34 -0500

    Remove partialy trusted callers

--- src/AutoMapper/AssemblyInfo.cs
+++ src/AutoMapper/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Security;
 
 [assembly:CLSCompliant(true)]
 #if NET45
-[assembly: AllowPartiallyTrustedCallers]
+//[assembly: AllowPartiallyTrustedCallers]
 //[assembly: SecurityTransparent]
 //[assembly: SecurityRules(SecurityRuleSet.Level2, SkipVerificationInFullTrust = true)]
 #endif
