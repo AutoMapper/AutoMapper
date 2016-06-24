8d02f10953969d1536ad66a73378df9ed3c04d46
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2016-06-24 10:49:13 +0300

    the other 2 attributes don't seem to make a difference performance wise

--- src/Benchmark/Program.cs
+++ src/Benchmark/Program.cs
@@ -4,8 +4,8 @@ using System.Security;
 using Benchmark.Flattening;
 
 [assembly: AllowPartiallyTrustedCallers]
-[assembly: SecurityTransparent]
-[assembly: SecurityRules(SecurityRuleSet.Level2, SkipVerificationInFullTrust = true)]
+//[assembly: SecurityTransparent]
+//[assembly: SecurityRules(SecurityRuleSet.Level2, SkipVerificationInFullTrust = true)]
 
 namespace Benchmark
 {
@@ -15,19 +15,18 @@ namespace Benchmark
 		{
             var mappers = new Dictionary<string, IObjectToObjectMapper[]>
                 {
-                    { "Flattening", new IObjectToObjectMapper[] { new FlatteningMapper(), new ManualMapper(), } },
+                    { "Flattening", new IObjectToObjectMapper[] { new FlatteningMapper() , new ManualMapper(), } },
                     { "Ctors", new IObjectToObjectMapper[] { new CtorMapper(), new ManualCtorMapper(),  } },
                     { "Complex", new IObjectToObjectMapper[] { new ComplexTypeMapper(), new ManualComplexTypeMapper() } },
                     { "Deep", new IObjectToObjectMapper[] { new DeepTypeMapper(), new ManualDeepTypeMapper() } }
-                };		
-
-			foreach (var pair in mappers)
-			{
-				foreach (var mapper in pair.Value)
-				{
-					new BenchEngine(mapper, pair.Key).Start();
-				}
-			}
+                };
+            foreach(var pair in mappers)
+            {
+                foreach(var mapper in pair.Value)
+                {
+                    new BenchEngine(mapper, pair.Key).Start();
+                }
+            }
 		}
 	}
 }
