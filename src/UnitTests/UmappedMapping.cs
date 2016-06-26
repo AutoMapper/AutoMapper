1b3f7c8f528178c4479ba79ad3415c853cec6d56
Patric Forsgard <patric@tasteful.se>
2016-06-26 21:08:11 +0200

    Test that throws with "Expression of type 'System.Object' cannot be used for constructor parameter of type 'System.Exception'"

---
+++ src/UnitTests/UmappedMapping.cs
@@ -0,0 +1,34 @@
+﻿using Should.Core.Assertions;
+
+namespace AutoMapper.UnitTests
+{
+    using System.Linq;
+    using Should;
+    using Xunit;
+    using QueryableExtensions;
+
+    public class UmappedMapping
+    {
+        public class Source
+        {
+            public int Value { get; set; }
+        }
+
+        public class Dest
+        {
+            public int Value { get; set; }
+        }
+
+        [Fact]
+        public void Can_not_map_unmapped_type()
+        {
+            Mapper.Initialize(cfg =>
+            {
+            });
+
+            var source = new Source {Value = 5};
+
+            Mapper.Map<Source, Dest>(source);
+        } 
+    }
+}
