ba402b9a198257e1a3d0cc32f2734cfb078cb5f6
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-06-28 17:24:01 -0500

    Preferring type map even if a mapper matches for property maps

---
+++ src/UnitTests/Bug/DeepCloningBug.cs
@@ -0,0 +1,46 @@
+﻿namespace AutoMapper.UnitTests.Bug
+{
+    using System;
+    using Should;
+    using Xunit;
+
+    public class DeepCloningBug : AutoMapperSpecBase
+    {
+        private Outer _source;
+        private Outer _dest;
+
+        public class Outer
+        {
+            public Inner Foo { get; set; }
+        }
+
+        public class Inner
+        {
+
+        }
+
+        protected override MapperConfiguration Configuration { get; } = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<Outer, Outer>();
+            cfg.CreateMap<Inner, Inner>();
+        });
+
+        protected override void Because_of()
+        {
+            _source = new Outer { Foo = new Inner() };
+            _dest = Mapper.Map<Outer>(_source);
+        }
+
+        [Fact]
+        public void Should_map_new_top_object()
+        {
+            _dest.ShouldNotBeSameAs(_source);
+        }
+
+        [Fact]
+        public void Should_map_new_second_level_object()
+        {
+            _dest.Foo.ShouldNotBeSameAs(_source.Foo);
+        }
+    }
+}
