57ebb2d68b7a869f917f214360bf327bd8c7ead9
Blake Niemyjski <bniemyjski@gmail.com>
2016-07-13 11:12:20 -0500

    Added breaking unit test for #1518

---
+++ src/UnitTests/CustomCollectionTester.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AutoMapper.UnitTests {
+    public class CustomCollectionTester {
+        [Fact]
+        public void Should_be_able_to_handle_custom_dictionary_with_custom_methods() {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<BaseClassWithDictionary, DerivedClassWithDictionary>());
+            var mapper = config.CreateMapper();
+        }
+        
+        public class BaseClassWithDictionary {
+            public DataDictionary Data { get; set; }
+        }
+
+        public class DerivedClassWithDictionary : BaseClassWithDictionary { }
+
+        public class DataDictionary : Dictionary<string, object> {
+            public string GetString(string name, string @default) {
+                return null;
+            }
+        }
+    }
+}
