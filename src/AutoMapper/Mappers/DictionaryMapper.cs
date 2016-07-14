4e2077905d2c8b13cf3c180304a8bcfce651d87a
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2016-07-14 11:32:41 +0300

    ExpandoObject is a dictionary and should be mapped as such

--- src/AutoMapper/Mappers/DictionaryMapper.cs
+++ src/AutoMapper/Mappers/DictionaryMapper.cs
@@ -8,8 +8,7 @@ namespace AutoMapper.Mappers
     
     public class DictionaryMapper : IObjectMapper
     {
-        public bool IsMatch(TypePair context)
-            => (context.SourceType.IsDictionaryType() && context.DestinationType.IsDictionaryType() && !context.SourceType.IsDynamic() && !context.DestinationType.IsDynamic());
+        public bool IsMatch(TypePair context) => context.SourceType.IsDictionaryType() && context.DestinationType.IsDictionaryType();
 
         public Expression MapExpression(TypeMapRegistry typeMapRegistry, IConfigurationProvider configurationProvider, PropertyMap propertyMap, Expression sourceExpression, Expression destExpression, Expression contextExpression)
             => typeMapRegistry.MapCollectionExpression(configurationProvider, propertyMap, sourceExpression, destExpression, contextExpression, CollectionMapperExtensions.IfNotNull, typeof(Dictionary<,>), CollectionMapperExtensions.MapKeyPairValueExpr);
