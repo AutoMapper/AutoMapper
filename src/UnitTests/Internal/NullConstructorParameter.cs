6cbad449cad89525b0260c73c73472b8d5306cfe
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2016-08-08 17:49:04 +0300

    the test

---
+++ src/UnitTests/Internal/NullConstructorParameter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace AutoMapper.UnitTests
+{
+    public class NullConstructorParameter : AutoMapperSpecBase
+    {
+        class Source
+        {
+        }
+        class Destination
+        {
+        }
+
+        class Parameter : ParameterInfo
+        {
+        }
+
+        protected override MapperConfiguration Configuration => new MapperConfiguration(cfg => cfg.CreateMap<Source, Destination>());
+
+        [Fact]
+        public void Should_work()
+        {
+            var typeMap = Configuration.GetAllTypeMaps().Single();
+            var constructorMap = new ConstructorMap(typeof(Source).GetConstructor(Type.EmptyTypes), typeMap);
+            constructorMap.AddParameter(new Parameter(), new MemberInfo[0], true);
+            typeMap.ConstructorMap = constructorMap;
+            typeMap.ConstructorParameterMatches("");
+        }
+    }
+}
