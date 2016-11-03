07921fe5e355d5e61100e0118c32d45bc27babf6
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2016-11-03 17:09:19 +0200

    better data structures

--- src/AutoMapper/Configuration/Conventions/SourceToDestinationNameMapperAttributesMember.cs
+++ src/AutoMapper/Configuration/Conventions/SourceToDestinationNameMapperAttributesMember.cs
@@ -1,20 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
 namespace AutoMapper.Configuration.Conventions
 {
-    using System;
-    using System.Collections.Concurrent;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Reflection;
-
     public class SourceToDestinationNameMapperAttributesMember : ISourceToDestinationNameMapper
     {
-        private readonly ConcurrentDictionary<TypeDetails, Dictionary<MemberInfo, IEnumerable<SourceToDestinationMapperAttribute>>> Cache = new ConcurrentDictionary<TypeDetails, Dictionary<MemberInfo, IEnumerable<SourceToDestinationMapperAttribute>>>();
+        private readonly Dictionary<TypeDetails, SourceMember[]> allSourceMembers = new Dictionary<TypeDetails, SourceMember[]>();
 
         public MemberInfo GetMatchingMemberInfo(IGetTypeInfoMembers getTypeInfoMembers, TypeDetails typeInfo, Type destType, Type destMemberType, string nameToSearch)
         {
-            Cache.GetOrAdd(typeInfo, ti => getTypeInfoMembers.GetMemberInfos(ti).ToDictionary(mi => mi, mi => mi.GetCustomAttributes(typeof(SourceToDestinationMapperAttribute), true).OfType<SourceToDestinationMapperAttribute>()));
+            SourceMember[] sourceMembers;
+            if(!allSourceMembers.TryGetValue(typeInfo, out sourceMembers))
+            {
+                sourceMembers = getTypeInfoMembers.GetMemberInfos(typeInfo).Select(sourceMember => new SourceMember(sourceMember)).ToArray();
+                allSourceMembers[typeInfo] = sourceMembers;
+            }
+            return sourceMembers.FirstOrDefault(d => d.Attribute != null && d.Attribute.IsMatch(typeInfo, d.Member, destType, destMemberType, nameToSearch)).Member;
+        }
 
-            return Cache[typeInfo].FirstOrDefault(kp => kp.Value.Any(_ => _.IsMatch(typeInfo, kp.Key, destType, destMemberType, nameToSearch))).Key;
+        struct SourceMember
+        {
+            public SourceMember(MemberInfo sourceMember)
+            {
+                Member = sourceMember;
+                Attribute = sourceMember.GetCustomAttribute<SourceToDestinationMapperAttribute>(inherit:true);
+            }
+
+            public MemberInfo Member { get; }
+            public SourceToDestinationMapperAttribute Attribute { get; }
         }
     }
 }
