8c4851fad2a771cbce7c726f12ff4d456b251eb6
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2016-12-12 12:23:17 +0200

    added a test

---
+++ src/UnitTests/Bug/ConvertMapperThreading.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xunit;
+using AutoMapper;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.ExceptionServices;
+
+namespace AutoMapper.UnitTests
+{
+    public class ConvertMapperThreading
+    {
+        class Source
+        {
+            public string Number { get; set; }
+        }
+
+        class Destination
+        {
+            public int Number { get; set; }
+        }
+
+        [Fact]
+        public void Should_work()
+        {
+            var tasks = Enumerable.Range(0, 5).Select(i => Task.Factory.StartNew(() =>
+            {
+                new MapperConfiguration(c => c.CreateMap<Source, Destination>());
+            })).ToArray();
+            try
+            {
+                Task.WaitAll(tasks);
+            }
+            catch(AggregateException ex)
+            {
+                ex.Handle(e =>
+                {
+                    if(e is InvalidOperationException)
+                    {
+                        ExceptionDispatchInfo.Capture(e).Throw();
+                    }
+                    return false;
+                });
+            }
+        }
+    }
+}
