219c57bf840afbe9eee70726f39497fef593c6f8
Huberto Kusters <kusteh2@medtronic.com>
2016-12-13 17:09:27 +0100

    Mapping dynamic objects with lists of dynamic objects
    Added the ability to map dynamic objects with lists of dynamic objects
    to class instances  and back again.

--- src/UnitTests/Mappers/DynamicMapperTests.cs
+++ src/UnitTests/Mappers/DynamicMapperTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 using Should;
@@ -31,6 +32,45 @@ namespace AutoMapper.UnitTests.Mappers.Dynamic
         }
 
         public int Count => dictionary.Count;
+    }
+
+    public class EntityA
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ModifyDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public byte[] RowVersion { get; set; }
+    }
+
+    public class DomainA : DynamicDictionary
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class EntityB
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ModifyDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public byte[] RowVersion { get; set; }
+        public List<EntityA> Items { get; set; }
+    }
+
+    public class DomainB : DynamicDictionary
+    {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<DomainA> Items { get; set; }
     }
 
     public class When_mapping_to_dynamic_from_getter_only_property
@@ -147,6 +187,152 @@ namespace AutoMapper.UnitTests.Mappers.Dynamic
             _destination = config.CreateMapper().Map<DynamicDictionary>(source);
             Assert.Equal("Foo", _destination.Foo);
             Assert.Equal("Bar", _destination.Bar);
+        }
+    }
+
+    public class When_mapping_from_partial_dynamic_to_class
+    {
+        [Fact]
+        public void Should_map()
+        {
+            // Arrange
+            var entity = new EntityA
+            {
+                Id = 100,
+                Name = "ENTITY1",
+                Description = "Entity 1",
+                CreatedBy = "unittest",
+                CreationDate = DateTime.UtcNow,
+                ModifiedBy = "unittest",
+                ModifyDate = DateTime.UtcNow,
+                RowVersion = new byte[] {0x00, 0x00, 0x00, 0x00, 0xfa, 0x23, 0x45, 0xf3},
+            };
+            var config = new MapperConfiguration(cfg => { });
+            var mapper = config.CreateMapper();
+
+            // Act
+            var domain = mapper.Map<DomainA>(entity);
+            var dynamicDomain = (dynamic) domain;
+            var newEntity = mapper.Map<EntityA>(domain);
+
+            //Assert
+            Assert.NotNull(domain);
+            Assert.Equal(entity.Name, domain.Name);
+            Assert.Equal(entity.Description, domain.Description);
+            Assert.Equal(entity.Id, dynamicDomain.Id);
+            Assert.Equal(entity.CreatedBy, dynamicDomain.CreatedBy);
+            Assert.Equal(entity.CreationDate, dynamicDomain.CreationDate);
+            Assert.Equal(entity.ModifiedBy, dynamicDomain.ModifiedBy);
+            Assert.Equal(entity.ModifyDate, dynamicDomain.ModifyDate);
+            Assert.Equal(entity.RowVersion, dynamicDomain.RowVersion);
+
+            Assert.Equal(entity.Name, newEntity.Name);
+            Assert.Equal(entity.Description, newEntity.Description);
+            Assert.Equal(entity.Id, newEntity.Id);
+            Assert.Equal(entity.CreatedBy, newEntity.CreatedBy);
+            Assert.Equal(entity.CreationDate, newEntity.CreationDate);
+            Assert.Equal(entity.ModifiedBy, newEntity.ModifiedBy);
+            Assert.Equal(entity.ModifyDate, newEntity.ModifyDate);
+            Assert.Equal(entity.RowVersion, newEntity.RowVersion);
+        }
+    }
+
+    public class When_mapping_from_partial_dynamic_with_list_of_partial_dynamic_to_class
+    {
+        [Fact]
+        public void Should_map()
+        {
+            // Arrange
+            var entityA1 = new EntityA
+            {
+                Id = 100,
+                Name = "ENTITYA1",
+                Description = "Entity A1",
+                CreatedBy = "unittest",
+                CreationDate = DateTime.UtcNow,
+                ModifiedBy = "unittest",
+                ModifyDate = DateTime.UtcNow,
+                RowVersion = new byte[] { 0x00, 0x00, 0x00, 0x00, 0xfc, 0x23, 0x45, 0xf3 },
+            };
+
+            var entityA2 = new EntityA
+            {
+                Id = 100,
+                Name = "ENTITYA2",
+                Description = "Entity A2",
+                CreatedBy = "unittest",
+                CreationDate = DateTime.UtcNow,
+                ModifiedBy = "unittest",
+                ModifyDate = DateTime.UtcNow,
+                RowVersion = new byte[] { 0x00, 0x00, 0x00, 0x00, 0xfd, 0x23, 0x45, 0xf3 },
+
+            };
+
+            var entityB1 = new EntityB
+            {
+                Id = 100,
+                Name = "ENTITYB1",
+                Description = "Entity B1",
+                CreatedBy = "unittest",
+                CreationDate = DateTime.UtcNow,
+                ModifiedBy = "unittest",
+                ModifyDate = DateTime.UtcNow,
+                RowVersion = new byte[] {0x00, 0x00, 0x00, 0x00, 0xfa, 0x23, 0x45, 0xf3},
+                Items = new List<EntityA> {entityA1, entityA2}
+            };
+            var mapper = new MapperConfiguration(cfg => { }).CreateMapper();
+
+            // Act
+            var domainB1 = mapper.Map<DomainB>(entityB1);
+            var dynamicDomainB1 = (dynamic)domainB1;
+            var newEntityB1 = mapper.Map<EntityB>(domainB1);
+
+            //Assert
+            Assert.NotNull(domainB1);
+            Assert.Equal(entityB1.Name, domainB1.Name);
+            Assert.Equal(entityB1.Description, domainB1.Description);
+            Assert.Equal(entityB1.Id, dynamicDomainB1.Id);
+            Assert.Equal(entityB1.CreatedBy, dynamicDomainB1.CreatedBy);
+            Assert.Equal(entityB1.CreationDate, dynamicDomainB1.CreationDate);
+            Assert.Equal(entityB1.ModifiedBy, dynamicDomainB1.ModifiedBy);
+            Assert.Equal(entityB1.ModifyDate, dynamicDomainB1.ModifyDate);
+            Assert.Equal(entityB1.RowVersion, dynamicDomainB1.RowVersion);
+
+            Assert.Equal(domainB1.Items.Count, 2);
+            Assert.Equal(domainB1.Items[0].Count, 6);
+            Assert.Equal(domainB1.Items[0].Name, entityA1.Name);
+            Assert.Equal(domainB1.Items[0].Description, entityA1.Description);
+            Assert.Equal(domainB1.Items[1].Count, 6);
+            Assert.Equal(domainB1.Items[1].Name, entityA2.Name);
+            Assert.Equal(domainB1.Items[1].Description, entityA2.Description);
+
+            Assert.Equal(entityB1.Name, newEntityB1.Name);
+            Assert.Equal(entityB1.Description, newEntityB1.Description);
+            Assert.Equal(entityB1.Id, newEntityB1.Id);
+            Assert.Equal(entityB1.CreatedBy, newEntityB1.CreatedBy);
+            Assert.Equal(entityB1.CreationDate, newEntityB1.CreationDate);
+            Assert.Equal(entityB1.ModifiedBy, newEntityB1.ModifiedBy);
+            Assert.Equal(entityB1.ModifyDate, newEntityB1.ModifyDate);
+            Assert.Equal(entityB1.RowVersion, newEntityB1.RowVersion);
+            Assert.Equal(entityB1.Items.Count, newEntityB1.Items.Count);
+
+            Assert.Equal(entityB1.Items[0].Id, newEntityB1.Items[0].Id);
+            Assert.Equal(entityB1.Items[0].Name, newEntityB1.Items[0].Name);
+            Assert.Equal(entityB1.Items[0].Description, newEntityB1.Items[0].Description);
+            Assert.Equal(entityB1.Items[0].CreatedBy, newEntityB1.Items[0].CreatedBy);
+            Assert.Equal(entityB1.Items[0].CreationDate, newEntityB1.Items[0].CreationDate);
+            Assert.Equal(entityB1.Items[0].ModifiedBy, newEntityB1.Items[0].ModifiedBy);
+            Assert.Equal(entityB1.Items[0].ModifyDate, newEntityB1.Items[0].ModifyDate);
+            Assert.Equal(entityB1.Items[0].RowVersion, newEntityB1.Items[0].RowVersion);
+
+            Assert.Equal(entityB1.Items[1].Id, newEntityB1.Items[1].Id);
+            Assert.Equal(entityB1.Items[1].Name, newEntityB1.Items[1].Name);
+            Assert.Equal(entityB1.Items[1].Description, newEntityB1.Items[1].Description);
+            Assert.Equal(entityB1.Items[1].CreatedBy, newEntityB1.Items[1].CreatedBy);
+            Assert.Equal(entityB1.Items[1].CreationDate, newEntityB1.Items[1].CreationDate);
+            Assert.Equal(entityB1.Items[1].ModifiedBy, newEntityB1.Items[1].ModifiedBy);
+            Assert.Equal(entityB1.Items[1].ModifyDate, newEntityB1.Items[1].ModifyDate);
+            Assert.Equal(entityB1.Items[1].RowVersion, newEntityB1.Items[1].RowVersion);
         }
     }
 }
