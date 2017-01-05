8800db5306fc7ffd81099afb60e18f8ad5ec6f4e
Rob Lugt <rob.lugt@cheynecapital.com>
2017-01-05 16:06:33 +0000

    Correct order of ExpressionBinders bringing CustomProjectionExpressionBinder to the top.  fixes #1879

---
+++ src/UnitTests/Bug/CannotProjectStringToNullableEnum.cs
@@ -0,0 +1,45 @@
+﻿using Should;
+using Xunit;
+using System.Linq;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+
+namespace AutoMapper.UnitTests.Bug
+{
+    public class CannotProjectStringToNullableEnum
+    {
+        public enum DummyTypes : int
+        {
+            Foo = 1,
+            Bar = 2
+        }
+
+        public class DummySource
+        {
+            public string Dummy { get; set; }
+        }
+
+        public class DummyDestination
+        {
+            public DummyTypes? Dummy { get; set; }
+        }
+
+        [Fact]
+        public void Should_project_string_to_nullable_enum()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<string, DummyTypes?>().ProjectUsing(s => (DummyTypes)System.Enum.Parse(typeof(DummyTypes),s));
+                cfg.CreateMap<DummySource, DummyDestination>();
+            });
+
+            config.AssertConfigurationIsValid();
+
+            var src = new DummySource[] { new DummySource { Dummy = "Foo" } };
+
+            var destination = src.AsQueryable().ProjectTo<DummyDestination>(config).First();
+
+            destination.Dummy.ShouldEqual(DummyTypes.Foo);
+        }
+    }
+}
