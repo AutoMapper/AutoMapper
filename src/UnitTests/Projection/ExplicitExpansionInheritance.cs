2988c9bd6993ed450cde8fcdad9c53bad8065ab9
moudrick <moudrick@gmail.com>
2017-02-15 15:11:00 +0200

    Unit Test class code cleanup

--- src/UnitTests/Projection/ExplicitExpansionInheritance.cs
+++ src/UnitTests/Projection/ExplicitExpansionInheritance.cs
@@ -1,67 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using AutoMapper;
+
 using AutoMapper.QueryableExtensions;
-using AutoMapper.UnitTests;
-using AutoMapper.UnitTests.Projection;
+
 using Should;
-using Should.Core.Assertions;
 using Xunit;
 
 namespace AutoMapper.UnitTests.Projection 
 {
-    using NameSourceType = Nullable<int> /* String */; using NameDtoType = Nullable<int> /* String */;
+    using NameSourceType = Nullable<int>; using NameDtoType = Nullable<int>;
     using DescSourceType = String; using DescDtoType = String;
-
-    public class Source
-    {   
-        public NameSourceType Name { get; set; }
-        public DescSourceType Desc { get; set; }
-    }
-
-    public class DtoBase              { public NameDtoType Name { get; set; } }
-    public class DtoDerived : DtoBase { public DescDtoType Desc { get; set; } }
-
-//    static class MyMappers
-//    {
-//        public static IMappingExpression<TSource, TDestination> Configure<TSource, TDestination>(this IMappingExpression<TSource, TDestination> target)
-//            where TSource : Source
-//            where TDestination : DtoBase
-//        {
-//            return target.ForMember(dto => dto.Name, conf =>
-//                {
-//                    conf.MapFrom(src => src.Name);
-//                    conf.ExplicitExpansion();
-//                });
-//        }
-//    }
 
     public class ExplicitExpansionInheritance  : AutoMapperSpecBase
     {
+        private class Source
+        {   
+            public NameSourceType Name { get; set; }
+            public DescSourceType Desc { get; set; }
+        }
+        public class DtoBase              { public NameDtoType Name { get; set; } }
+        public class DtoDerived : DtoBase { public DescDtoType Desc { get; set; } }
+
         protected override MapperConfiguration Configuration { get; } = new MapperConfiguration(cfg =>
         {
             cfg.CreateMap<Source, DtoBase>()
-                // .Configure()
-                .ForMember(dto => dto.Name, conf =>
-                {
-                    conf.MapFrom(src => src.Name);
-                    conf.ExplicitExpansion();
-                })
-                .Include<Source, DtoDerived>();
+                .ForMember(dto => dto.Name, conf => { conf.MapFrom(src => src.Name); conf.ExplicitExpansion(); }).Include<Source, DtoDerived>();
             cfg.CreateMap<Source, DtoDerived>()
-                // .Configure()
-                .ForMember(dto => dto.Desc, conf => {
-                    conf.MapFrom(src => src.Desc);
-                    conf.ExplicitExpansion();
-                })
-            ;
+                .ForMember(dto => dto.Desc, conf => { conf.MapFrom(src => src.Desc); conf.ExplicitExpansion(); });
         });
 
         private static readonly IQueryable<Source> _iq = new List<Source> {
             new Source { Name = -25, Desc = "Descr", },
         } .AsQueryable();
-
         private static readonly Source _iqf = _iq.First();
 
         [Fact] public void ProjectAll() 
