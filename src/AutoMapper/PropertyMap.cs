b133bdc801b2db3b4b37430dc731b23c8e263034
moudrick <moudrick@gmail.com>
2017-02-15 14:47:35 +0200

    fixed ExplicitExpansion inheritance itself

--- src/AutoMapper/PropertyMap.cs
+++ src/AutoMapper/PropertyMap.cs
@@ -41,7 +41,7 @@ namespace AutoMapper
         public LambdaExpression CustomExpression { get; private set; }
         public MemberInfo CustomSourceMember { get; set; }
         public bool UseDestinationValue { get; set; }
-        public bool ExplicitExpansion { get; set; }
+        public bool ExplicitExpansion { get { return _explicitExpansion.GetValueOrDefault(); } set { _explicitExpansion = value; } } private bool? _explicitExpansion; 
         public object NullSubstitute { get; set; }
         public ValueResolverConfiguration ValueResolverConfig { get; set; }
 
@@ -106,6 +106,7 @@ namespace AutoMapper
             MappingOrder = MappingOrder ?? inheritedMappedProperty.MappingOrder;
             CustomSourceMember = CustomSourceMember ?? inheritedMappedProperty.CustomSourceMember;
             ValueResolverConfig = ValueResolverConfig ?? inheritedMappedProperty.ValueResolverConfig;
+            ExplicitExpansion = _explicitExpansion ?? inheritedMappedProperty.ExplicitExpansion;
         }
 
         public bool IsMapped()
