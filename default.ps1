8520a6e9aa7e7fe554eaefab72688931d196c49c
Jimmy Bogard <jimmy.bogard@gmail.com>
2017-03-11 22:31:57 -0600

    Latest nuget

--- default.ps1
+++ default.ps1
@@ -31,7 +31,7 @@ task compile -depends clean {
 	$version = if ($env:APPVEYOR_BUILD_NUMBER -ne $NULL) { $env:APPVEYOR_BUILD_NUMBER } else { '0' }
 	$version = "{0:D5}" -f [convert]::ToInt32($version, 10)
 	
-    exec { dotnet restore $source_dir\AutoMapper.sln }
+	exec { dotnet restore $source_dir\AutoMapper.sln }
 
     exec { dotnet build $source_dir\AutoMapper.sln -c $config -v q /nologo  }
 
