99b801543abc25a9607107cadcc603fad7e3c542
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2017-03-29 15:51:34 +0300

    consider abstract types

--- src/UnitTests/Mappers/StringDictionaryMapperTests.cs
+++ src/UnitTests/Mappers/StringDictionaryMapperTests.cs
@@ -1,4 +1,5 @@
-﻿using Should;
+﻿using System;
+using Should;
 using Xunit;
 using StringDictionary = System.Collections.Generic.Dictionary<string, object>;
 
@@ -112,6 +113,86 @@ namespace AutoMapper.UnitTests.Mappers
         {
             _destination["Foo"].ShouldEqual("Foo");
             _destination["Bar"].ShouldEqual("Bar");
+        }
+    }
+
+    public class When_mapping_from_StringDictionary_to_existing_destination : AutoMapperSpecBase
+    {
+        public abstract class SomeBase
+        {
+            protected int _x = 100;
+            public abstract int X { get; }
+            protected int _y = 200;
+            public abstract int Y { get; }
+        }
+
+        public class SomeBody : SomeBase
+        {
+            public override int X { get { return _x + 10; } }
+
+            public override int Y { get { return _y + 20; } }
+            private int _z = 300;
+            public int Z { get { return _z + 30; } }
+        }
+
+        public class SomeOne : SomeBase
+        {
+            public override int X { get { return _x - 10; } }
+
+            public override int Y { get { return _y - 20; } }
+            private int _a = 300;
+            public int A { get { return _a - 30; } }
+        }
+
+        protected override MapperConfiguration Configuration => new MapperConfiguration(c => c.CreateMap<SomeBase, SomeBase>());
+
+        [Fact]
+        public void Should_map_ok()
+        {
+            SomeBase someBase = new SomeBody();
+            var someOne = new StringDictionary();
+
+            Mapper.Map(someOne, someBase);
+        }
+    }
+
+    public class When_mapping_from_StringDictionary_to_abstract_type : AutoMapperSpecBase
+    {
+        public abstract class SomeBase
+        {
+            protected int _x = 100;
+            public abstract int X { get; }
+            protected int _y = 200;
+            public abstract int Y { get; }
+        }
+
+        public class SomeBody : SomeBase
+        {
+            public override int X { get { return _x + 10; } }
+
+            public override int Y { get { return _y + 20; } }
+            private int _z = 300;
+            public int Z { get { return _z + 30; } }
+        }
+
+        public class SomeOne : SomeBase
+        {
+            public override int X { get { return _x - 10; } }
+
+            public override int Y { get { return _y - 20; } }
+            private int _a = 300;
+            public int A { get { return _a - 30; } }
+        }
+
+        protected override MapperConfiguration Configuration => new MapperConfiguration(c => c.CreateMap<SomeBase, SomeBase>());
+
+        [Fact]
+        public void Should_throw()
+        {
+            new Action(()=>Mapper.Map<SomeBase>(new StringDictionary()))
+                .ShouldThrow<AutoMapperMappingException>(ex=>
+                    ex.InnerException.Message.ShouldStartWith($"Cannot create an instance of abstract type {typeof(SomeBase)}."));
+
         }
     }
 }
