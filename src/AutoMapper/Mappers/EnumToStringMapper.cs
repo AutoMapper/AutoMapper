c318c731babfa1315c58c0a17d415f9e1cccf68e
Tornhoof <Tornhoof@gmail.com>
2017-04-11 22:53:56 +0200

    Removed Blockexpression and simplified the expressions

--- src/AutoMapper/Mappers/EnumToStringMapper.cs
+++ src/AutoMapper/Mappers/EnumToStringMapper.cs
@@ -10,49 +10,32 @@ using AutoMapper.Mappers.Internal;
 namespace AutoMapper.Mappers
 {
     public class EnumToStringMapper : IObjectMapper
-    {    
-        public bool IsMatch(TypePair context) => context.DestinationType == typeof(string) && ElementTypeHelper.GetEnumerationType(context.SourceType) != null;
+    {
+        public bool IsMatch(TypePair context) => context.DestinationType == typeof(string) &&
+                                                 ElementTypeHelper.GetEnumerationType(context.SourceType) != null;
 
-        public Expression MapExpression(IConfigurationProvider configurationProvider, ProfileMap profileMap, PropertyMap propertyMap, Expression sourceExpression, Expression destExpression, Expression contextExpression)
+        public Expression MapExpression(IConfigurationProvider configurationProvider, ProfileMap profileMap,
+            PropertyMap propertyMap, Expression sourceExpression, Expression destExpression,
+            Expression contextExpression)
         {
             var sourceType = sourceExpression.Type;
             var sourceTypeEnum = ElementTypeHelper.GetEnumerationType(sourceType);
             var toStringCall = Expression.Call(sourceExpression, typeof(object).GetDeclaredMethod("ToString"));
-            return BuildEnumMemberSwitchTable(sourceTypeEnum, sourceExpression, toStringCall);
-        }
-
-        private static Expression BuildEnumMemberSwitchTable(Type destinationEnumType, Expression sourceExpression, Expression toStringCall)
-        {
             var switchCases = new List<SwitchCase>();
-            var enumNames = destinationEnumType.GetDeclaredMembers();
-            var resultVariable = Expression.Variable(typeof(string));
-            Expression resultExpression;
+            var enumNames = sourceTypeEnum.GetDeclaredMembers();
             foreach (var memberInfo in enumNames.Where(x => x.IsStatic()))
             {
                 var attribute = memberInfo.GetCustomAttribute(typeof(EnumMemberAttribute)) as EnumMemberAttribute;
                 if (attribute?.Value != null)
                 {
-                    var switchCase = Expression.SwitchCase(
-                        Expression.Assign(resultVariable, Expression.Constant(attribute.Value)),
-                        Expression.Constant(Enum.ToObject(destinationEnumType, memberInfo.GetMemberValue(null))));
+                    var switchCase = Expression.SwitchCase(Expression.Constant(attribute.Value),
+                        Expression.Constant(Enum.ToObject(sourceTypeEnum, memberInfo.GetMemberValue(null))));
                     switchCases.Add(switchCase);
                 }
             }
-            if (switchCases.Count > 0)
-            {
-                var returnTarget = Expression.Label(typeof(string));
-                var blockExpressions = new List<Expression>
-                {
-                    Expression.Switch(sourceExpression, Expression.Assign(resultVariable, toStringCall), switchCases.ToArray()),
-                    Expression.Label(returnTarget, resultVariable)
-                };
-                resultExpression = Expression.Block(new[] { resultVariable }, blockExpressions);
-            }
-            else
-            {
-                resultExpression = toStringCall;
-            }
-            return resultExpression;
+            return switchCases.Count > 0
+                ? (Expression) Expression.Switch(sourceExpression, toStringCall, switchCases.ToArray())
+                : toStringCall;
         }
     }
 }
--- src/AutoMapper/Mappers/StringToEnumMapper.cs
+++ src/AutoMapper/Mappers/StringToEnumMapper.cs
@@ -13,55 +13,36 @@ namespace AutoMapper.Mappers
 
     public class StringToEnumMapper : IObjectMapper
     {
-        public bool IsMatch(TypePair context)
-        {
-            var destEnumType = ElementTypeHelper.GetEnumerationType(context.DestinationType);
-            return destEnumType != null && context.SourceType == typeof(string);
-        }
+        public bool IsMatch(TypePair context) => context.SourceType == typeof(string) &&
+                                                 ElementTypeHelper.GetEnumerationType(context.DestinationType) != null;
 
-        public Expression MapExpression(IConfigurationProvider configurationProvider, ProfileMap profileMap, PropertyMap propertyMap, Expression sourceExpression, Expression destExpression, Expression contextExpression)
+        public Expression MapExpression(IConfigurationProvider configurationProvider, ProfileMap profileMap,
+            PropertyMap propertyMap, Expression sourceExpression, Expression destExpression,
+            Expression contextExpression)
         {
             var destinationType = destExpression.Type;
             var destinationEnumType = ElementTypeHelper.GetEnumerationType(destinationType);
-            var enumParse = Expression.Call(typeof(Enum), "Parse", null, Expression.Constant(destinationEnumType), sourceExpression, Expression.Constant(true));
-            var switchTable = BuildEnumMemberSwitchTable(destinationEnumType, destinationType, sourceExpression, enumParse);
-            var isNullOrEmpty = Expression.Call(typeof(string), "IsNullOrEmpty", null, sourceExpression);
-            return Expression.Condition(isNullOrEmpty, Expression.Default(destinationType), switchTable);
-        }
-
-        private static Expression BuildEnumMemberSwitchTable(Type destinationEnumType, Type destinationType, Expression sourceExpression, Expression enumParse)
-        {
+            var enumParse = Expression.Call(typeof(Enum), "Parse", null, Expression.Constant(destinationEnumType),
+                sourceExpression, Expression.Constant(true));
             var switchCases = new List<SwitchCase>();
             var enumNames = destinationEnumType.GetDeclaredMembers();
-            var resultVariable = Expression.Variable(destinationEnumType);
-            sourceExpression = Expression.Call(sourceExpression, "ToUpperInvariant", null, null);
-            Expression resultExpression;
+            var upperCaseSourceExpression = Expression.Call(sourceExpression, "ToUpperInvariant", null, null);
             foreach (var memberInfo in enumNames.Where(x => x.IsStatic()))
             {
                 var attribute = memberInfo.GetCustomAttribute(typeof(EnumMemberAttribute)) as EnumMemberAttribute;
                 if (attribute?.Value != null)
                 {
                     var switchCase = Expression.SwitchCase(
-                        Expression.Assign(resultVariable, Expression.Constant(Enum.ToObject(destinationEnumType, memberInfo.GetMemberValue(null)))),
-                        Expression.Constant(attribute.Value.ToUpperInvariant()));
+                        ToType(Expression.Constant(Enum.ToObject(destinationEnumType, memberInfo.GetMemberValue(null))),
+                            destinationType), Expression.Constant(attribute.Value.ToUpperInvariant()));
                     switchCases.Add(switchCase);
                 }
             }
-            if (switchCases.Count > 0)
-            {
-                var returnTarget = Expression.Label(destinationType);
-                var blockExpressions = new List<Expression>
-                {
-                    Expression.Switch(sourceExpression, Expression.Assign(resultVariable, ToType(enumParse, destinationEnumType)), switchCases.ToArray()),
-                    Expression.Label(returnTarget, ToType(resultVariable, destinationType))
-                };
-                resultExpression = Expression.Block(new[] {resultVariable}, blockExpressions);
-            }
-            else
-            {
-                resultExpression = ToType(enumParse, destinationType);
-            }
-            return resultExpression;
+            var switchTable = switchCases.Count > 0
+                ? Expression.Switch(upperCaseSourceExpression, ToType(enumParse, destinationType), switchCases.ToArray())
+                : ToType(enumParse, destinationType);
+            var isNullOrEmpty = Expression.Call(typeof(string), "IsNullOrEmpty", null, sourceExpression);
+            return Expression.Condition(isNullOrEmpty, Expression.Default(destinationType), switchTable);
         }
     }
 }
