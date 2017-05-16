ecf7c745d5ec2b0e042101f741532ae6b445ba0b
Daniel Severin <daniel.severin@endava.com>
2017-05-16 14:52:42 +0300

    Revert "introduced "ScanForInternalProfiles" property for AddProfiles()"
    This reverts commit e67073f767be8d11d9a22c32947b638d4aac10b3.

--- src/AutoMapper/Configuration/MapperConfigurationExpression.cs
+++ src/AutoMapper/Configuration/MapperConfigurationExpression.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -9,7 +8,7 @@ namespace AutoMapper.Configuration
 {
     public class MapperConfigurationExpression : Profile, IMapperConfigurationExpression, IConfiguration
     {
-        private IEnumerable<Profile> _profiles = Enumerable.Empty<Profile>();
+        private readonly IList<Profile> _profiles = new List<Profile>();
 
         public MapperConfigurationExpression() : base("")
         {
@@ -21,7 +20,7 @@ namespace AutoMapper.Configuration
         public IEnumerable<IProfileConfiguration> Profiles => _profiles;
         public Func<Type, object> ServiceCtor { get; private set; } = Activator.CreateInstance;
 
-        public void CreateProfile(string profileName, Action<IProfileExpression> config)
+        public void CreateProfile(string profileName, Action<IProfileExpression> config) 
             => AddProfile(new NamedProfile(profileName, config));
 
         public IList<IObjectMapper> Mappers { get; }
@@ -36,13 +35,13 @@ namespace AutoMapper.Configuration
         }
 
         public void AddProfile(Profile profile)
-            => _profiles = _profiles.Concat(new[] { profile });
+        {
+            _profiles.Add(profile);
+        }
 
-        public void AddProfile<TProfile>() where TProfile : Profile, new()
-            => _profiles = _profiles.Concat(new[] {new TProfile()});
+        public void AddProfile<TProfile>() where TProfile : Profile, new() => AddProfile(new TProfile());
 
-        public void AddProfile(Type profileType)
-            => _profiles = _profiles.Concat(new[] {(Profile) Activator.CreateInstance(profileType)});
+        public void AddProfile(Type profileType) => AddProfile((Profile)Activator.CreateInstance(profileType));
 
         public void AddProfiles(IEnumerable<Assembly> assembliesToScan)
             => AddProfilesCore(assembliesToScan);
@@ -63,51 +62,22 @@ namespace AutoMapper.Configuration
             => AddProfilesCore(typesFromAssembliesContainingProfiles.Select(t => t.GetTypeInfo().Assembly));
 
         private void AddProfilesCore(IEnumerable<Assembly> assembliesToScan)
-            => _profiles = _profiles.Concat(new ProfileScannerIterator(assembliesToScan, Advanced));
+        {
+            var allTypes = assembliesToScan.Where(a => !a.IsDynamic).SelectMany(a => a.ExportedTypes).ToArray();
+
+            var profiles =
+                allTypes
+                    .Where(t => typeof(Profile).GetTypeInfo().IsAssignableFrom(t.GetTypeInfo()))
+                    .Where(t => !t.GetTypeInfo().IsAbstract);
+
+            foreach (var profile in profiles)
+            {
+                AddProfile(profile);
+            }
+
+        }
+
 
         public void ConstructServicesUsing(Func<Type, object> constructor) => ServiceCtor = constructor;
-
-        private class ProfileScannerIterator : IEnumerable<Profile>
-        {
-            private readonly IEnumerable<Assembly> _assembliesToScan;
-            private readonly AdvancedConfiguration _config;
-
-            public ProfileScannerIterator(IEnumerable<Assembly> assembliesToScan, AdvancedConfiguration config)
-            {
-                _assembliesToScan = assembliesToScan;
-                _config = config;
-            }
-
-            private IEnumerable<Profile> GetEnumerableProfilesIncludingInternals()
-            {
-                return from assembly in _assembliesToScan
-                       where !assembly.IsDynamic
-                       from typeInfo in assembly.DefinedTypes
-                       where typeof(Profile).GetTypeInfo().IsAssignableFrom(typeInfo) && !typeInfo.IsAbstract
-                       select (Profile) Activator.CreateInstance(typeInfo.AsType());
-            }
-            private IEnumerable<Profile> GetEnumerableProfiles()
-            {
-                return from assembly in _assembliesToScan
-                       where !assembly.IsDynamic
-                       from type in assembly.ExportedTypes.Select(t => t.GetTypeInfo())
-                       where typeof(Profile).GetTypeInfo().IsAssignableFrom(type) && !type.IsAbstract
-                       select(Profile) Activator.CreateInstance(type.AsType());
-            }
-
-            public IEnumerator<Profile> GetEnumerator()
-            {
-                var profiles = _config.ScanForInternalProfiles
-                    ? GetEnumerableProfilesIncludingInternals()
-                    : GetEnumerableProfiles();
-
-                return profiles.GetEnumerator();
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return GetEnumerator();
-            }
-        }
     }
 }
