8ae16c2b85ea19bc4901357407ed4212c0a82af6
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2017-06-06 15:56:34 +0300

    failing test

---
+++ src/IntegrationTests.Net4/MapObjectPropertyFromSubQuery.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Should;
+using System.Linq;
+using System.Collections.Generic;
+using AutoMapper.UnitTests;
+using System.Data.Entity;
+using Xunit;
+
+namespace AutoMapper.IntegrationTests
+{
+    using System.Linq.Expressions;
+    using QueryableExtensions;
+
+    public class MapObjectPropertyFromSubQuery : AutoMapperSpecBase
+    {
+        protected override MapperConfiguration Configuration => new MapperConfiguration(cfg=>
+        {
+            cfg.CreateMap<Product, ProductModel>()
+                .ForMember(d => d.Price, o => o.MapFrom(source => source.Articles.Where(x => x.IsDefault && x.NationId == 1 && source.ECommercePublished).FirstOrDefault()));
+            cfg.CreateMap<Article, PriceModel>()
+                .ForMember(d => d.RegionId, o => o.MapFrom(s => s.NationId));
+        });
+
+        [Fact]
+        public void Should_cache_the_subquery()
+        {
+            using(var context = new ClientContext())
+            {
+                var projection = context.Products.ProjectTo<ProductModel>(Configuration);
+                var counter = new FirstOrDefaultCounter();
+                counter.Visit(projection.Expression);
+                counter.Count.ShouldEqual(1);
+                var productModel = projection.First();
+                productModel.Price.RegionId.ShouldEqual((short)1);
+                productModel.Price.IsDefault.ShouldBeTrue();
+                productModel.Price.Id.ShouldEqual(1);
+                productModel.Id.ShouldEqual(1);
+            }
+        }
+
+        class FirstOrDefaultCounter : ExpressionVisitor
+        {
+            public int Count;
+
+            protected override Expression VisitMethodCall(MethodCallExpression node)
+            {
+                if(node.Method.Name == "FirstOrDefault")
+                {
+                    Count++;
+                }
+                return base.VisitMethodCall(node);
+            }
+        }
+
+        public partial class Article
+        {
+            public int Id { get; set; }
+            public int ProductId { get; set; }
+            public bool IsDefault { get; set; }
+            public short NationId { get; set; }
+            public virtual Product Product { get; set; }
+        }
+
+        public partial class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public bool ECommercePublished { get; set; }
+            public virtual ICollection<Article> Articles { get; set; }
+        }
+
+        public class PriceModel
+        {
+            public int Id { get; set; }
+            public short RegionId { get; set; }
+            public bool IsDefault { get; set; }
+        }
+
+        public class ProductModel
+        {
+            public int Id { get; set; }
+            public PriceModel Price { get; set; }
+        }
+
+        class Initializer : DropCreateDatabaseAlways<ClientContext>
+        {
+            protected override void Seed(ClientContext context)
+            {
+                context.Products.Add(new Product { ECommercePublished = true, Articles = new[] { new Article { IsDefault = true, NationId = 1, ProductId = 1 } } });
+            }
+        }
+
+        class ClientContext : DbContext
+        {
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                Database.SetInitializer(new Initializer());
+            }
+
+            public DbSet<Product> Products { get; set; }
+        }
+    }
+}
