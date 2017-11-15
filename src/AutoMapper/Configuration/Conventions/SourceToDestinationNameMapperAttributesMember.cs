9b2bf581925916f0255a240c5953f83aa1998be7
Jimmy Bogard <jimmy.bogard@gmail.com>
2017-11-15 07:11:33 -0600

    Getting rid of static cache

--- src/AutoMapper/Configuration/Conventions/SourceToDestinationNameMapperAttributesMember.cs
+++ src/AutoMapper/Configuration/Conventions/SourceToDestinationNameMapperAttributesMember.cs
@@ -6,18 +6,12 @@ namespace AutoMapper.Configuration.Conventions
 {
     public class SourceToDestinationNameMapperAttributesMember : ISourceToDestinationNameMapper
     {
-        private static readonly SourceMember[] Empty = new SourceMember[0];
-        private LockingConcurrentDictionary<TypeDetails, SourceMember[]> _allSourceMembers
-            = new LockingConcurrentDictionary<TypeDetails, SourceMember[]>(_ => Empty);
-
         public MemberInfo GetMatchingMemberInfo(IGetTypeInfoMembers getTypeInfoMembers, TypeDetails typeInfo, Type destType, Type destMemberType, string nameToSearch)
         {
-            SourceMember[] ValueFactory(TypeDetails td) => getTypeInfoMembers.GetMemberInfos(td)
+            var sourceMembers = getTypeInfoMembers.GetMemberInfos(typeInfo)
                 .Select(sourceMember => new SourceMember(sourceMember))
                 .Where(s => s.Attribute != null)
                 .ToArray();
-
-            var sourceMembers = _allSourceMembers.GetOrAdd(typeInfo, td => new Lazy<SourceMember[]>(() => ValueFactory(td)));
 
             return sourceMembers.FirstOrDefault(d => d.Attribute.IsMatch(typeInfo, d.Member, destType, destMemberType, nameToSearch)).Member;
         }
