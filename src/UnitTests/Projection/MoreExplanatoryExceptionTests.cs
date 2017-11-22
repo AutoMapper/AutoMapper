df421587c4ebc037af9e1cd51b3023a1d95f7472
Thomas Caudal <thomas.caudal@enablon.com>
2017-11-22 21:41:58 +0100

    Improve error message when the creation of instantiation expression fails

---
+++ src/UnitTests/Projection/MoreExplanatoryExceptionTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using AutoMapper.QueryableExtensions;
+
+namespace AutoMapper.UnitTests.Projection
+{
+    public class MoreExplanatoryExceptionTests
+    {
+        [Fact]
+        public void ConstructorWithUnknownParameterTypeThrowsExplicitException()
+        {
+            // Arrange
+            var config = new MapperConfiguration(cfg =>
+                cfg.CreateMap<EntitySource, EntityDestination>());
+
+            // Act
+            var exception = Assert.Throws<AutoMapperMappingException>(() =>
+                new EntitySource[0].AsQueryable().ProjectTo<EntityDestination>(config));
+
+            // Assert
+            Assert.Contains("object notSupported", exception.Message, StringComparison.OrdinalIgnoreCase);
+        }
+
+        class EntitySource
+        {
+        }
+        class EntityDestination
+        {
+            public EntityDestination(object notSupported = null) { }
+        }
+    }
+}
