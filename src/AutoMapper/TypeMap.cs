1ac3e91f6bd64a7b879a46ca6b0e168285135f72
Jimmy Bogard <jimmy.bogard@gmail.com>
2018-01-31 15:48:48 -0600

    Marking MemberPath as a readonly struct; making all of its members readonly; updating method parameters to use in

--- src/AutoMapper/TypeMap.cs
+++ src/AutoMapper/TypeMap.cs
@@ -39,14 +39,21 @@ namespace AutoMapper
             Profile = profile;
         }
 
-        public PathMap FindOrCreatePathMapFor(LambdaExpression destinationExpression, MemberPath path, TypeMap typeMap)
+        public PathMap FindOrCreatePathMapFor(LambdaExpression destinationExpression, in MemberPath path, TypeMap typeMap)
         {
-            var pathMap = _pathMaps.SingleOrDefault(p => p.MemberPath == path);
-            if(pathMap == null)
+            PathMap pathMap = null;
+            foreach (var map in _pathMaps)
             {
-                pathMap = new PathMap(destinationExpression, path, typeMap);
-                _pathMaps.Add(pathMap);
+                if (map.MemberPath == path)
+                    pathMap = map;
             }
+
+            if (pathMap != null)
+                return pathMap;
+
+            pathMap = new PathMap(destinationExpression, path, typeMap);
+            _pathMaps.Add(pathMap);
+
             return pathMap;
         }
 
