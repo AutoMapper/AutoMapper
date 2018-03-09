494a9629843c65490a884517411c80cb9fadb4a9
Christopher Black <black@madeinoffice.com>
2018-03-09 11:48:12 +0100

    define ComVisible Attribute If not defined it is always true

--- src/AutoMapper/AssemblyInfo.cs
+++ src/AutoMapper/AssemblyInfo.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Resources;
-
 #if NET45 || NET40
+using System.Runtime.InteropServices;
 using System.Security;
 #endif
 
-[assembly:CLSCompliant(true)]
+[assembly: CLSCompliant(true)]
 #if NET45 || NET40
 [assembly: AllowPartiallyTrustedCallers]
+[assembly: ComVisible(false)]
 #endif
 
 [assembly: NeutralResourcesLanguage("en")]
