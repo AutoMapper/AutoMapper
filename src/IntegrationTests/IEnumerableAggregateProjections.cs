d216ad466a224a3902a0bc65485aa07de77f582f
Kawsar Saiyeed <kawsar.saiyeed@projiris.com>
2018-04-11 16:20:38 +0100

    Fix projection to destinations using extension methods #2593
    Allow resolved extension methods to be used as getters for sources.
    
    All resolved getters were converted to a MemberExpression which
    threw an ArgumentException whenever a getter was actually for a
    method. Check if the getter is a method and convert that to a
    MethodCallExpression instead.

---
+++ src/IntegrationTests/IEnumerableAggregateProjections.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using Shouldly;
+using Xunit;
+
+namespace AutoMapper.IntegrationTests
+{
+    using UnitTests;
+    using QueryableExtensions;
+    using System.Collections.Generic;
+
+    public class IEnumerableAggregateProjections : AutoMapperSpecBase
+    {
+        public class Customer
+        {
+            [Key]
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public ICollection<Item> Items { get; set; }
+        }
+
+        public class Item
+        {
+            public int Id { get; set; }
+            public int Code { get; set; }
+        }
+
+        public class CustomerViewModel
+        {
+            public int ItemCodesCount { get; set; }
+            public int ItemCodesMin { get; set; }
+            public int ItemCodesMax { get; set; }
+            public int ItemCodesSum { get; set; }
+        }
+
+        public class Context : DbContext
+        {
+            public Context()
+            {
+                Database.SetInitializer<Context>(new DatabaseInitializer());
+            }
+
+            public DbSet<Customer> Customers { get; set; }
+        }
+
+        public class DatabaseInitializer : DropCreateDatabaseAlways<Context>
+        {
+            protected override void Seed(Context context)
+            {
+                context.Customers.Add(new Customer
+                {
+                    Id = 1,
+                    FirstName = "Bob",
+                    LastName = "Smith",
+                    Items = new[] { new Item { Code = 1 }, new Item { Code = 3 }, new Item { Code = 5 } }
+                });
+
+                base.Seed(context);
+            }
+        }
+
+        protected override MapperConfiguration Configuration => new MapperConfiguration(_ => { });
+
+        [Fact]
+        public void Can_map_with_projection()
+        {
+            using (var context = new Context())
+            {
+                var result = context.Customers.Select(customer => new
+                {
+                    ItemCodes = customer.Items.Select(item => item.Code)
+                }).ProjectTo<CustomerViewModel>(Configuration).Single();
+
+                result.ItemCodesCount.ShouldBe(3);
+                result.ItemCodesMin.ShouldBe(1);
+                result.ItemCodesMax.ShouldBe(5);
+                result.ItemCodesSum.ShouldBe(9);
+            }
+        }
+    }
+}
---
+++ src/UnitTests/Bug/CannotProjectIEnumerableToAggregateDestinations.cs
@@ -0,0 +1,43 @@
+﻿using Shouldly;
+using Xunit;
+using System.Linq;
+using AutoMapper.QueryableExtensions;
+using System.Collections.Generic;
+
+namespace AutoMapper.UnitTests.Bug
+{
+    public class CannotProjectIEnumerableToAggregateDestinations
+    {
+        class DummySource
+        {
+            public IEnumerable<int> DummyEnumerable { get; set; }
+        }
+
+        class DummyDestination
+        {
+            public int DummyEnumerableCount { get; set; }
+            public int DummyEnumerableSum { get; set; }
+            public int DummyEnumerableMin { get; set; }
+            public int DummyEnumerableMax { get; set; }
+        }
+
+        [Fact]
+        public void Should_project_ienumerable_to_aggregate_destinations()
+        {
+            // arrange
+            var config = new MapperConfiguration(_ => { });
+            var source = new DummySource() { DummyEnumerable = new[] { 1, 4, 5 } };
+
+            // act
+            var destination = new[] { source }.AsQueryable()
+                .ProjectTo<DummyDestination>(config)
+                .Single();
+
+            // assert
+            destination.DummyEnumerableCount.ShouldBe(3);
+            destination.DummyEnumerableSum.ShouldBe(10);
+            destination.DummyEnumerableMin.ShouldBe(1);
+            destination.DummyEnumerableMax.ShouldBe(5);
+        }
+    }
+}
