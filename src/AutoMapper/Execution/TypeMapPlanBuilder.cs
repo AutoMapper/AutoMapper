219175622926280a6c5f6aac2d4afbe794ffbd08
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2018-09-27 18:28:58 +0300

    switch to ValueTuple

--- src/AutoMapper/Execution/TypeMapPlanBuilder.cs
+++ src/AutoMapper/Execution/TypeMapPlanBuilder.cs
@@ -94,8 +94,8 @@ namespace AutoMapper.Execution
             var defaultPropertyMap = new PropertyMap(default(MemberInfo), null);
             var properties = 
                 _typeMap.GetPropertyMaps().Concat(_typeMap.PathMaps.Select(pm => new PropertyMap(pm))).Where(pm=>pm.CanResolveValue())
-                .Select(pm=>new { PropertyTypeMap = ResolvePropertyTypeMap(pm), PropertyMap = pm })
-                .Concat(_typeMap.ConstructorMappingTypes.Select(tp => new { PropertyTypeMap = ResolveTypeMap(tp), PropertyMap = defaultPropertyMap }))
+                .Select(pm=>(PropertyTypeMap: ResolvePropertyTypeMap(pm), PropertyMap: pm))
+                .Concat(_typeMap.ConstructorMappingTypes.Select(tp => (PropertyTypeMap: ResolveTypeMap(tp), PropertyMap: defaultPropertyMap)))
                 .Where(p => p.PropertyTypeMap != null && !p.PropertyTypeMap.PreserveReferences && p.PropertyTypeMap.MapExpression == null);
             foreach(var property in properties)
             {
