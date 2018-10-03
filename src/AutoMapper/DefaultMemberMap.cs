d416b42f0091e7053246ca68b1d9f96ab8f27e4d
Jimmy Bogard <jimmy.bogard@gmail.com>
2018-10-03 10:36:03 -0500

    Making the default member map public

---
+++ src/AutoMapper/DefaultMemberMap.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace AutoMapper
+{
+    /// <summary>
+    /// Member maps with default values. Used in dynamic/dictionary scenarios when source/destination members do not exist.
+    /// </summary>
+    public class DefaultMemberMap : IMemberMap
+    {
+        private DefaultMemberMap() { }
+
+        public static readonly IMemberMap Instance = new DefaultMemberMap();
+
+        public TypeMap TypeMap => default;
+        public Type SourceType => default;
+        public IEnumerable<MemberInfo> SourceMembers { get; } 
+            = Enumerable.Empty<MemberInfo>();
+        public MemberInfo DestinationMember => default;
+        public Type DestinationMemberType => default;
+        public TypePair Types => default;
+        public bool CanResolveValue() => default;
+
+        public bool Ignored => default;
+        public bool Inline { get; set; } = true;
+        public bool UseDestinationValue => default;
+        public object NullSubstitute => default;
+        public LambdaExpression PreCondition => default;
+        public LambdaExpression Condition => default;
+        public LambdaExpression CustomMapExpression => default;
+        public LambdaExpression CustomMapFunction => default;
+        public ValueResolverConfiguration ValueResolverConfig => default;
+        public ValueConverterConfiguration ValueConverterConfig => default;
+
+        public IEnumerable<ValueTransformerConfiguration> ValueTransformers { get; } 
+            = Enumerable.Empty<ValueTransformerConfiguration>();
+    }
+}
--- src/AutoMapper/IMemberMap.cs
+++ src/AutoMapper/IMemberMap.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -26,35 +25,5 @@ namespace AutoMapper
         ValueResolverConfiguration ValueResolverConfig { get; }
         ValueConverterConfiguration ValueConverterConfig { get; }
         IEnumerable<ValueTransformerConfiguration> ValueTransformers { get; }
-    }
-
-    internal class DefaultMemberMap : IMemberMap
-    {
-        private DefaultMemberMap() { }
-
-        public static readonly IMemberMap Instance = new DefaultMemberMap();
-
-        public TypeMap TypeMap => default;
-        public Type SourceType => default;
-        public IEnumerable<MemberInfo> SourceMembers { get; } 
-            = Enumerable.Empty<MemberInfo>();
-        public MemberInfo DestinationMember => default;
-        public Type DestinationMemberType => default;
-        public TypePair Types => default;
-        public bool CanResolveValue() => default;
-
-        public bool Ignored => default;
-        public bool Inline { get; set; } = true;
-        public bool UseDestinationValue => default;
-        public object NullSubstitute => default;
-        public LambdaExpression PreCondition => default;
-        public LambdaExpression Condition => default;
-        public LambdaExpression CustomMapExpression => default;
-        public LambdaExpression CustomMapFunction => default;
-        public ValueResolverConfiguration ValueResolverConfig => default;
-        public ValueConverterConfiguration ValueConverterConfig => default;
-
-        public IEnumerable<ValueTransformerConfiguration> ValueTransformers { get; } 
-            = Enumerable.Empty<ValueTransformerConfiguration>();
     }
 }
