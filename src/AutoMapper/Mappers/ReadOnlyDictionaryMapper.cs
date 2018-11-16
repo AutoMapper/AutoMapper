e7443d81c6a416db3f383b18e7615f48a676b21f
Jimmy Bogard <jimmy.bogard@gmail.com>
2018-11-16 14:16:21 +0200

    First test passing

---
+++ src/AutoMapper/Mappers/ReadOnlyDictionaryMapper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Linq.Expressions;
+using AutoMapper.Configuration;
+using AutoMapper.Mappers.Internal;
+
+namespace AutoMapper.Mappers
+{
+    using static Expression;
+    using static CollectionMapperExpressionFactory;
+
+    public class ReadOnlyDictionaryMapper : IObjectMapper
+    {
+        public bool IsMatch(TypePair context)
+        {
+            if (!(context.SourceType.IsEnumerableType() && context.DestinationType.IsGenericType()))
+                return false;
+
+            var genericType = context.DestinationType.GetGenericTypeDefinition();
+
+            return genericType == typeof(ReadOnlyDictionary<,>);
+        }
+
+        public Expression MapExpression(IConfigurationProvider configurationProvider, ProfileMap profileMap,
+            IMemberMap memberMap, Expression sourceExpression, Expression destExpression, Expression contextExpression)
+        {
+            var dictType = typeof(Dictionary<,>).MakeGenericType(ElementTypeHelper.GetElementTypes(destExpression.Type, ElementTypeFlags.BreakKeyValuePair));
+            var dict = MapCollectionExpression(configurationProvider, profileMap, memberMap, sourceExpression, Default(dictType), contextExpression, typeof(Dictionary<,>), MapKeyPairValueExpr);
+            var dest = Variable(dictType, "dest");
+
+            return Block(new[] { dest }, Assign(dest, dict), Condition(NotEqual(dest, Default(dictType)), New(destExpression.Type.GetDeclaredConstructors().First(), dest), Default(destExpression.Type)));
+        }
+    }
+}
---
+++ src/UnitTests/Mappers/ReadOnlyDictionaryMapperTests.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Shouldly;
+using Xunit;
+
+namespace AutoMapper.UnitTests.Mappers.ReadOnlyDictionaryMapper
+{
+    public class When_mapping_to_interface_readonly_dictionary : AutoMapperSpecBase
+    {
+        public class Source
+        {
+            public IReadOnlyDictionary<int, int> Values { get; set; }
+        }
+
+        public class Destination
+        {
+            public IReadOnlyDictionary<int, int> Values { get; set; }
+        }
+
+        protected override MapperConfiguration Configuration { get; } = new MapperConfiguration(config =>
+        {
+            config.CreateMap<Source, Destination>();
+        });
+
+        [Fact]
+        public void Should_map_readonly_values()
+        {
+            var source = new Source
+            {
+                Values = new Dictionary<int, int>
+                {
+                    {1, 1},
+                    {2, 2},
+                    {3, 3},
+                    {4, 4},
+                }
+            };
+
+            var dest = Mapper.Map<Destination>(source);
+
+            dest.Values.Count.ShouldBe(4);
+        }
+    }
+    public class When_mapping_to_concrete_readonly_dictionary : AutoMapperSpecBase
+    {
+        public class Source
+        {
+            public ReadOnlyDictionary<int, int> Values { get; set; }
+        }
+
+        public class Destination
+        {
+            public ReadOnlyDictionary<int, int> Values { get; set; }
+        }
+
+        protected override MapperConfiguration Configuration { get; } = new MapperConfiguration(config =>
+        {
+            config.CreateMap<Source, Destination>();
+        });
+
+        [Fact]
+        public void Should_map_readonly_values()
+        {
+            var source = new Source
+            {
+                Values = new ReadOnlyDictionary<int, int>(new Dictionary<int, int>
+                {
+                    {1, 1},
+                    {2, 2},
+                    {3, 3},
+                    {4, 4},
+                })
+            };
+
+            var dest = Mapper.Map<Destination>(source);
+
+            dest.Values.Count.ShouldBe(4);
+        }
+    }
+}
