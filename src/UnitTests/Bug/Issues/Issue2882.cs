f3be48a76ef159747c94eb601aa9fbec385bdae4
jkuehner <jochen.kuehner@kardex.com>
2018-11-19 13:40:26 +0100

    reproduce issue without castle.core

--- src/UnitTests/Bug/Issues/Issue2882.cs
+++ src/UnitTests/Bug/Issues/Issue2882.cs
@@ -1,39 +1,55 @@
 ﻿using System;
-using System.ComponentModel;
-using Castle.DynamicProxy;
+using System.Reflection;
+using System.Reflection.Emit;
 using Xunit;
 
 namespace AutoMapper.UnitTests.Bug.Issues
 {
+#if NET461
     public class Issue2882
     {
         [Fact]
         public void TestMethod1()
         {
-            var proxyGenerator = new ProxyGenerator();
-            var proxy = proxyGenerator.CreateClassProxy(typeof(ResourcePointDTO), new[]
-            {
-                typeof(INotifyPropertyChanged)
-            }, new NotifyPropertyChangedInterceptor());
+            var proxy = CreateDynamicObject();
             var config = new MapperConfiguration(cfg =>
             {
-                cfg.CreateMap(typeof(ResourcePointDTO), proxy.GetType());
                 cfg.AllowNullCollections = true;
                 cfg.AllowNullDestinationValues = true;
+                cfg.CreateMap(typeof(ResourcePointDTO), proxy.GetType());
             });
             var mapper = config.CreateMapper();
         }
 
-        public class NotifyPropertyChangedInterceptor : IInterceptor
-        {
-            public NotifyPropertyChangedInterceptor()
-            { }
-
-            public void Intercept(IInvocation invocation)
-            { }
-        }
-
         public class ResourcePointDTO
         { }
+
+        public interface A { }
+
+        object CreateDynamicObject()
+        {
+            var assemblyName = new AssemblyName("TestClass");
+            AssemblyBuilder assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("MainModule");
+            TypeBuilder typeBuilder = moduleBuilder.DefineType(assemblyName.FullName
+                , TypeAttributes.Public |
+                  TypeAttributes.Class |
+                  TypeAttributes.AutoClass |
+                  TypeAttributes.AnsiClass |
+                  TypeAttributes.BeforeFieldInit |
+                  TypeAttributes.AutoLayout
+                , null);
+            var fieldBuilder = typeBuilder.DefineField("__interceptors", typeof(A), FieldAttributes.Private);
+            typeBuilder.DefineDefaultConstructor(MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName);
+            var cBuilder = typeBuilder.DefineConstructor(MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName, CallingConventions.Standard, new []{ typeof(A) });
+            ILGenerator myConstructorIL = cBuilder.GetILGenerator();
+            myConstructorIL.Emit(OpCodes.Ldarg_0);
+            myConstructorIL.Emit(OpCodes.Ldarg_1);
+            myConstructorIL.Emit(OpCodes.Stfld, fieldBuilder);
+            myConstructorIL.Emit(OpCodes.Ret);
+            var type = typeBuilder.CreateType();
+            return Activator.CreateInstance(type);
+        }
     }
+#endif
 }
