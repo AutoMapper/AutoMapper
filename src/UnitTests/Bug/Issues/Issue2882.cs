0e722a460affb3b982a10b5755408cb7b8d2cfc5
jkuehner <jochen.kuehner@kardex.com>
2018-11-19 10:59:24 +0100

    Test for issue 2882

---
+++ src/UnitTests/Bug/Issues/Issue2882.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using Castle.DynamicProxy;
+using Xunit;
+
+namespace AutoMapper.UnitTests.Bug.Issues
+{
+    public class Issue2882
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            var proxyGenerator = new ProxyGenerator();
+            var proxy = proxyGenerator.CreateClassProxy(typeof(ResourcePointDTO), new[]
+            {
+                typeof(INotifyPropertyChanged)
+            }, new NotifyPropertyChangedInterceptor());
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap(typeof(ResourcePointDTO), proxy.GetType());
+                cfg.AllowNullCollections = true;
+                cfg.AllowNullDestinationValues = true;
+            });
+            var mapper = config.CreateMapper();
+        }
+
+        public class NotifyPropertyChangedInterceptor : IInterceptor
+        {
+            public NotifyPropertyChangedInterceptor()
+            { }
+
+            public void Intercept(IInvocation invocation)
+            { }
+        }
+
+        public class ResourcePointDTO
+        { }
+    }
+}
