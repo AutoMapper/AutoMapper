ccc4427d91b6c0e96d8f9552a769d1f1f63d1f8a
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2018-12-19 09:25:46 +0200

    use https

--- src/AutoMapper/IConfigurationProvider.cs
+++ src/AutoMapper/IConfigurationProvider.cs
@@ -159,7 +159,7 @@ namespace AutoMapper
         /// <summary>
         /// Builds the execution plan used to map the source to destination.
         /// Useful to understand what exactly is happening during mapping.
-        /// See <a href="http://automapper.readthedocs.io/en/latest/Understanding-your-mapping.html">the wiki</a> for details.
+        /// See <a href="https://automapper.readthedocs.io/en/latest/Understanding-your-mapping.html">the wiki</a> for details.
         /// </summary>
         /// <param name="sourceType">the runtime type of the source object</param>
         /// <param name="destinationType">the runtime type of the destination object</param>
@@ -169,7 +169,7 @@ namespace AutoMapper
         /// <summary>
         /// Builds the execution plan used to map the source to destination.
         /// Useful to understand what exactly is happening during mapping.
-        /// See <a href="http://automapper.readthedocs.io/en/latest/Understanding-your-mapping.html">the wiki</a> for details.
+        /// See <a href="https://automapper.readthedocs.io/en/latest/Understanding-your-mapping.html">the wiki</a> for details.
         /// </summary>
         /// <param name="mapRequest">The source/destination map request</param>
         /// <returns>the execution plan</returns>
