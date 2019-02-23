d65fc8a7f7aa66a6c413183393ab36eb87a29a5f
Patric Forsgard <patric@tasteful.se>
2019-02-23 09:48:15 +0100

    Removed Lazy from the FeatureCollectionBase

--- src/AutoMapper/Internal/FeatureCollectionBase.cs
+++ src/AutoMapper/Internal/FeatureCollectionBase.cs
@@ -1,40 +1,32 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace AutoMapper.Internal
 {
     public class FeatureCollectionBase<TValue> : IEnumerable<KeyValuePair<Type, TValue>>
     {
-        private readonly Lazy<ConcurrentDictionary<Type, TValue>> _features = new Lazy<ConcurrentDictionary<Type, TValue>>(() => new ConcurrentDictionary<Type, TValue>());
-        private bool _readOnly;
+        private IDictionary<Type, TValue> _features = new Dictionary<Type, TValue>();
 
         public TValue this[Type key]
         {
             get
             {
-                return _features.IsValueCreated && _features.Value.TryGetValue(key, out var feature)
+                return _features.TryGetValue(key, out var feature)
                     ? feature
                     : default(TValue);
             }
             set
             {
-                if (_readOnly)
-                {
-                    throw new NotSupportedException("Features collection is read only.");
-                }
-
                 if (value == null)
                 {
-                    if (!_features.IsValueCreated)
-                    {
-                        return;
-                    }
-                    _features.Value.TryRemove(key, out var _);
+                    _features.Remove(key);
                 }
-
-                _features.Value.AddOrUpdate(key, value, (_, __) => value);
+                else
+                {
+                    _features[key] = value;
+                }
             }
         }
 
@@ -62,12 +54,9 @@ namespace AutoMapper.Internal
 
         public IEnumerator<KeyValuePair<Type, TValue>> GetEnumerator()
         {
-            if (_features.IsValueCreated)
+            foreach (var feature in _features)
             {
-                foreach (var feature in _features.Value)
-                {
-                    yield return feature;
-                }
+                yield return feature;
             }
         }
 
@@ -78,7 +67,7 @@ namespace AutoMapper.Internal
 
         protected void MakeReadOnly()
         {
-            _readOnly = true;
+            _features = new ReadOnlyDictionary<Type, TValue>(_features);
         }
     }
 }
