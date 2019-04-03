beffcbb6724f48a2203b49375a73cb9e2d946ac6
Patrick <patrickvanek@gmail.com>
2019-04-03 17:47:02 +0200

    fixed enum values caseinsensitivity bug

---
+++ src/UnitTests/Bug/EnumCaseSensitivityBug.cs
@@ -0,0 +1,42 @@
+﻿using Shouldly;
+
+namespace AutoMapper.UnitTests.Bug
+{
+    using Xunit;
+
+    public class EnumCaseSensitivityBug : AutoMapperSpecBase
+    {
+        private SecondEnum _resultSecondEnum;
+        private FirstEnum _resultFirstEnum;
+
+        public enum FirstEnum
+        {
+            Dog,
+            Cat
+        }
+
+        public enum SecondEnum
+        {
+            cat,
+            dog
+        }
+
+        protected override MapperConfiguration Configuration { get; } = new MapperConfiguration(cfg =>
+        {
+            // not creating a map on purpose to trigger use of EnumToEnumMapper
+        });
+
+        protected override void Because_of()
+        {
+            _resultSecondEnum = Mapper.Map<SecondEnum>(FirstEnum.Cat);
+            _resultFirstEnum = Mapper.Map<FirstEnum>(SecondEnum.dog);
+        }
+
+        [Fact]
+        public void Should_match_on_the_name_even_if_values_match()
+        {
+            _resultSecondEnum.ShouldBe(SecondEnum.cat);
+            _resultFirstEnum.ShouldBe(FirstEnum.Dog);
+        }
+    }
+}
