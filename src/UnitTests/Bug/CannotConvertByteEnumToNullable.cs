8643542129be3619cad0ba87b737237b3bba6799
Papp, Tamás <tpapp@itron.com>
2019-04-18 00:33:59 +0200

    add: CannotConvertByteEnumToNullable test

---
+++ src/UnitTests/Bug/CannotConvertByteEnumToNullable.cs
@@ -0,0 +1,36 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace AutoMapper.UnitTests.Bug
+{
+    public class CannotConvertByteEnumToNullable
+    {
+        public enum DummyTypes : byte
+        {
+            Foo = 1,
+            Bar = 2
+        }
+
+        public class DummySource
+        {
+            public DummyTypes Dummy { get; set; }
+        }
+
+        public class DummyDestination
+        {
+            public int? Dummy { get; set; }
+        }
+
+        [Fact]
+        public void Should_map_byte_enum_to_nullable_int()
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<DummySource, DummyDestination>());
+            config.AssertConfigurationIsValid();
+            DummySource src = new DummySource() { Dummy = DummyTypes.Bar };
+
+            var destination = config.CreateMapper().Map<DummySource, DummyDestination>(src);
+
+            destination.Dummy.ShouldBe((int)DummyTypes.Bar);
+        }
+    }
+}
