4606b76430fa7c972f7e15edf03e2de1d578273c
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2019-06-25 16:55:53 +0300

    automate LastMajorVersionBinary

--- src/AutoMapper/PreBuild.bat
+++
@@ -1,4 +0,0 @@
-rd /s /q ..\LastMajorVersionBinary
-mkdir ..\LastMajorVersionBinary
-..\..\nuget install AutoMapper -Version %1% -OutputDirectory ..\LastMajorVersionBinary
-copy ..\LastMajorVersionBinary\AutoMapper.%1%\lib\netstandard2.0\AutoMapper.dll ..\LastMajorVersionBinary
