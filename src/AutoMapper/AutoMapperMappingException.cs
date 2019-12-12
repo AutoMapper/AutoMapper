8e76eb33e840ab89f08e629924b6b7384ab62e0e
Bouke Haarsma <bouke@haarsma.eu>
2019-12-12 22:46:36 +0100

    Removed dead links

--- src/AutoMapper/AutoMapperMappingException.cs
+++ src/AutoMapper/AutoMapperMappingException.cs
@@ -9,13 +9,6 @@ namespace AutoMapper
     {
         private readonly string _message;
 
-        //
-        // For guidelines regarding the creation of new exception types, see
-        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
-        // and
-        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
-        //
-
         public AutoMapperMappingException()
         {
         }
