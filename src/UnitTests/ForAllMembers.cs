c45dd451a2d04a9941bf3db6502ce3ee3dcbab8d
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2020-05-22 17:41:27 +0300

    passing test for ForAllPropertyMaps inside a profile

--- src/UnitTests/ForAllMembers.cs
+++ src/UnitTests/ForAllMembers.cs
@@ -1,54 +1,85 @@
-﻿namespace AutoMapper.UnitTests
+﻿using System;
+using Shouldly;
+using Xunit;
+
+namespace AutoMapper.UnitTests.ForAllMembers
 {
-    namespace ForAllMembers
+    public class When_conditionally_applying_a_resolver_globally : AutoMapperSpecBase
     {
-        using System;
-        using Shouldly;
-        using Xunit;
-
-        public class When_conditionally_applying_a_resolver_globally : AutoMapperSpecBase
+        public class Source
         {
-            public class Source
+            public DateTime SomeDate { get; set; }
+            public DateTime OtherDate { get; set; }
+        }
+
+        public class Dest
+        {
+            public DateTime SomeDate { get; set; }
+            public DateTime OtherDate { get; set; }
+        }
+
+        protected override MapperConfiguration Configuration { get; } = new MapperConfiguration(cfg =>
+        {
+            cfg.ForAllPropertyMaps(pm => pm.DestinationName.StartsWith("Other"),
+                (pm, opt) => opt.MapFrom(typeof(ConditionalValueResolver), pm.SourceMember.Name));
+
+            cfg.CreateMap<Source, Dest>();
+        });
+
+        public class ConditionalValueResolver : IMemberValueResolver<object, object, DateTime, DateTime>
+        {
+            public DateTime Resolve(object s, object d, DateTime source, DateTime destination, ResolutionContext context)
             {
-                public DateTime SomeDate { get; set; }
-                public DateTime OtherDate { get; set; }
+                return source.AddDays(1);
             }
+        }
 
-            public class Dest
+        [Fact]
+        public void Should_use_resolver()
+        {
+            var source = new Source
             {
-                public DateTime SomeDate { get; set; }
-                public DateTime OtherDate { get; set; }
+                SomeDate = new DateTime(2000, 1, 1),
+                OtherDate = new DateTime(2000, 1, 1),
+            };
+            var dest = Mapper.Map<Source, Dest>(source);
+
+            dest.SomeDate.ShouldBe(source.SomeDate);
+            dest.OtherDate.ShouldBe(source.OtherDate.AddDays(1));
+        }
+    }
+    public class When_conditionally_applying_a_resolver_per_profile : AutoMapperSpecBase
+    {
+        public class Source
+        {
+            public DateTime SomeDate { get; set; }
+            public DateTime OtherDate { get; set; }
+        }
+        public class Dest
+        {
+            public DateTime SomeDate { get; set; }
+            public DateTime OtherDate { get; set; }
+        }
+        class MyProfile : Profile
+        {
+            public MyProfile()
+            {
+                CreateMap<Source, Dest>();
+                ForAllPropertyMaps(pm => pm.DestinationName.StartsWith("Other"), (pm, opt) => opt.MapFrom(typeof(ConditionalValueResolver), pm.SourceMember.Name));
             }
-
-            protected override MapperConfiguration Configuration { get; } = new MapperConfiguration(cfg =>
-            {
-                cfg.ForAllPropertyMaps(pm => pm.DestinationName.StartsWith("Other"), 
-                    (pm, opt) => opt.MapFrom(typeof(ConditionalValueResolver), pm.SourceMember.Name));
-
-                cfg.CreateMap<Source, Dest>();
-            });
-
-            public class ConditionalValueResolver : IMemberValueResolver<object, object, DateTime, DateTime>
-            {
-                public DateTime Resolve(object s, object d, DateTime source, DateTime destination, ResolutionContext context)
-                {
-                    return source.AddDays(1);
-                }
-            }
-
-            [Fact]
-            public void Should_use_resolver()
-            {
-                var source = new Source
-                {
-                    SomeDate = new DateTime(2000, 1, 1),
-                    OtherDate = new DateTime(2000, 1, 1),
-                };
-                var dest = Mapper.Map<Source, Dest>(source);
-
-                dest.SomeDate.ShouldBe(source.SomeDate);
-                dest.OtherDate.ShouldBe(source.OtherDate.AddDays(1));
-            }
+        }
+        protected override MapperConfiguration Configuration { get; } = new MapperConfiguration(cfg => cfg.AddProfile<MyProfile>());
+        public class ConditionalValueResolver : IMemberValueResolver<object, object, DateTime, DateTime>
+        {
+            public DateTime Resolve(object s, object d, DateTime source, DateTime destination, ResolutionContext context) => source.AddDays(1);
+        }
+        [Fact]
+        public void Should_use_resolver()
+        {
+            var source = new Source { SomeDate = new DateTime(2000, 1, 1), OtherDate = new DateTime(2000, 1, 1) };
+            var dest = Mapper.Map<Source, Dest>(source);
+            dest.SomeDate.ShouldBe(source.SomeDate);
+            dest.OtherDate.ShouldBe(source.OtherDate.AddDays(1));
         }
     }
 }
