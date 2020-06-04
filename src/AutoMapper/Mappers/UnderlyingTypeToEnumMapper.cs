df352dc8aae5074685fd6dc58e5568dc2d149e6e
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2020-06-04 11:05:40 +0300

    refactoring

--- src/AutoMapper/Mappers/UnderlyingTypeToEnumMapper.cs
+++ src/AutoMapper/Mappers/UnderlyingTypeToEnumMapper.cs
@@ -1,32 +1,17 @@
 using System;
 using System.Linq.Expressions;
-using System.Reflection;
-using AutoMapper.Internal;
 using AutoMapper.Mappers.Internal;
 
 namespace AutoMapper.Mappers
 {
-    using static Expression;
-    using static ExpressionFactory;
-
     public class UnderlyingTypeToEnumMapper : IObjectMapper
     {
-        private static readonly MethodInfo EnumToObject = Method(() => Enum.ToObject(typeof(object), null));
-
         public bool IsMatch(TypePair context)
         {
             var destEnumType = ElementTypeHelper.GetEnumerationType(context.DestinationType);
-
             return destEnumType != null && context.SourceType.IsAssignableFrom(Enum.GetUnderlyingType(destEnumType));
         }
-
-        public Expression MapExpression(IConfigurationProvider configurationProvider, ProfileMap profileMap,
-            IMemberMap memberMap, Expression sourceExpression, Expression destExpression,
-            Expression contextExpression) =>
-                ToType(
-                    Call(EnumToObject, Constant(destExpression.Type),
-                        ToObject(sourceExpression)),
-                    destExpression.Type
-                );
+        public Expression MapExpression(IConfigurationProvider configurationProvider, ProfileMap profileMap, IMemberMap memberMap, Expression sourceExpression,
+            Expression destExpression, Expression contextExpression) => sourceExpression;
     }
 }
