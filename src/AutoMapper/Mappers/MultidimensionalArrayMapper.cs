2befee5892116e86f07c0f14296618b6d0842e28
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2020-07-05 11:10:48 +0300

    it only really works for multidimensional arrays

--- src/AutoMapper/Mappers/MultidimensionalArrayMapper.cs
+++ src/AutoMapper/Mappers/MultidimensionalArrayMapper.cs
@@ -14,23 +14,19 @@ namespace AutoMapper.Mappers
         private static Array Map<TDestination, TSource, TSourceElement>(TSource source, ResolutionContext context, IConfigurationProvider configurationProvider)
             where TSource : IEnumerable
         {
+            var sourceArray = source as Array;
             var destElementType = ElementTypeHelper.GetElementType(typeof(TDestination));
-
             if (typeof(TDestination).IsAssignableFrom(typeof(TSource)))
             {
                 var elementTypeMap = configurationProvider.ResolveTypeMap(typeof(TSourceElement), destElementType);
                 if (elementTypeMap == null)
-                    return source as Array;
+                {
+                    return sourceArray;
+                }
             }
-
-            var sourceList = (IEnumerable)source;
-            var sourceArray = source as Array;
-            var destinationArray = sourceArray == null 
-                ? Array.CreateInstance(destElementType, sourceList.Cast<object>().Count()) 
-                : Array.CreateInstance(destElementType, Enumerable.Range(0, sourceArray.Rank).Select(sourceArray.GetLength).ToArray());
-
+            var destinationArray = Array.CreateInstance(destElementType, Enumerable.Range(0, sourceArray.Rank).Select(sourceArray.GetLength).ToArray());
             var filler = new MultidimensionalArrayFiller(destinationArray);
-            foreach (var item in sourceList)
+            foreach (var item in source)
             {
                 filler.NewValue(context.Map(item, null, typeof(TSourceElement), destElementType, null));
             }
@@ -39,10 +35,15 @@ namespace AutoMapper.Mappers
 
         private static readonly MethodInfo MapMethodInfo = typeof(MultidimensionalArrayMapper).GetDeclaredMethod(nameof(Map));
 
-        public bool IsMatch(TypePair context) => 
-            context.DestinationType.IsArray 
-            && context.DestinationType.GetArrayRank() > 1 
-            && context.SourceType.IsEnumerableType();
+        public bool IsMatch(TypePair context)
+        {
+            if (!context.DestinationType.IsArray || !context.SourceType.IsArray)
+            {
+                return false;
+            }
+            var destinationRank = context.DestinationType.GetArrayRank();
+            return destinationRank > 1 && destinationRank == context.SourceType.GetArrayRank();
+        }
 
         public Expression MapExpression(IConfigurationProvider configurationProvider, ProfileMap profileMap,
             IMemberMap memberMap, Expression sourceExpression, Expression destExpression,
@@ -92,5 +93,4 @@ namespace AutoMapper.Mappers
             }
         }
     }
-
 }
