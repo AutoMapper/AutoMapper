50b67c7c6931278c73ce559c0b1d43dcaff31fd6
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2020-12-16 11:48:56 +0200

    not needed anymore

--- src/AutoMapper/Internal/LockingConcurrentDictionary.cs
+++ src/AutoMapper/Internal/LockingConcurrentDictionary.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 
 namespace AutoMapper.Internal
 {
@@ -10,14 +9,6 @@ namespace AutoMapper.Internal
         public LockingConcurrentDictionary(Func<TKey, TValue> valueFactory, int capacity = 31) =>
             _dictionary = new ConcurrentDictionaryWrapper<TKey, Lazy<TValue>>(key => new Lazy<TValue>(() => valueFactory(key)), capacity);
         public TValue GetOrAdd(in TKey key) => _dictionary.GetOrAdd(key).Value;
-        public TValue this[in TKey key]
-        {
-            get => _dictionary[key].Value;
-            set => _dictionary[key] = new Lazy<TValue>(() => value);
-        }
-        public bool ContainsKey(in TKey key) => _dictionary.ContainsKey(key);
-        public ICollection<TKey> Keys => _dictionary.Keys;
-        public TValue GetOrDefault(in TKey key) => _dictionary.TryGetValue(key, out Lazy<TValue> lazy) ? lazy.Value : default;
     }
     public readonly struct ConcurrentDictionaryWrapper<TKey, TValue>
     {
@@ -28,19 +19,6 @@ namespace AutoMapper.Internal
             _dictionary = new ConcurrentDictionary<TKey, TValue>(Environment.ProcessorCount, capacity);
             _valueFactory = key => valueFactory(key);
         }
-        public TValue this[in TKey key]
-        {
-            get => _dictionary[key];
-            set => _dictionary[key] = value;
-        }
-        public TValue GetOrDefault(in TKey key)
-        {
-            _dictionary.TryGetValue(key, out var value);
-            return value;
-        }
-        public ICollection<TKey> Keys => _dictionary.Keys;
-        public bool ContainsKey(in TKey key) => _dictionary.ContainsKey(key);
         public TValue GetOrAdd(in TKey key) => _dictionary.GetOrAdd(key, _valueFactory);
-        public bool TryGetValue(in TKey key, out TValue value) => _dictionary.TryGetValue(key, out value);
     }
 }
