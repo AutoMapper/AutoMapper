bceae8c3ca4b12ac1b98aa914d3713ead1079028
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2021-01-07 09:08:34 +0200

    eliminate boxing

--- src/AutoMapper/Mappers/EnumToEnumMapper.cs
+++ src/AutoMapper/Mappers/EnumToEnumMapper.cs
@@ -7,23 +7,17 @@ namespace AutoMapper.Internal.Mappers
     using static Execution.ExpressionBuilder;
     public class EnumToEnumMapper : IObjectMapper
     {
-        private static readonly MethodInfo ToObjectMethod = typeof(Enum).GetMethod("ToObject", new[] { typeof(Type), typeof(object) });
-        private static readonly MethodInfo IsDefinedMethod = typeof(Enum).GetMethod("IsDefined", new[] { typeof(Type), typeof(object) });
         private static readonly MethodInfo TryParseMethod = typeof(Enum).StaticGenericMethod("TryParse", parametersCount: 3);
         public bool IsMatch(in TypePair context) => context.IsEnumToEnum();
         public Expression MapExpression(IGlobalConfiguration configurationProvider, ProfileMap profileMap,
             MemberMap memberMap, Expression sourceExpression, Expression destExpression)
         {
             var destinationType = destExpression.Type;
-            var sourceToObject = sourceExpression.ToObject();
-            var toObject = Call(ToObjectMethod, Constant(destinationType), sourceToObject);
-            var castToEnum = Convert(toObject, destinationType);
-            var isDefined = Call(IsDefinedMethod, Constant(sourceExpression.Type), sourceToObject);
             var sourceToString = Call(sourceExpression, ObjectToString);
             var result = Variable(destinationType, "destinationEnumValue");
             var ignoreCase = True;
             var tryParse = Call(TryParseMethod.MakeGenericMethod(destinationType), sourceToString, ignoreCase, result);
-            return Block(new[] { result }, Condition(isDefined, Condition(tryParse, result, castToEnum), castToEnum));
+            return Block(new[] { result }, Condition(tryParse, result, Convert(sourceExpression, destinationType)));
         }
     }
 }
