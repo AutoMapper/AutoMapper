3ada6674f77e668fd7f1f072aa6eb645e0c0c837
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2021-10-21 13:49:12 +0200

    Add EFCore test to illustrate projection issue

--- src/IntegrationsTests.EFCore/ProjectionConversionTests.cs
+++ src/IntegrationsTests.EFCore/ProjectionConversionTests.cs
@@ -1,11 +1,13 @@
-﻿using AutoMapper.UnitTests;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Shouldly;
-using System.Data.Entity;
+using System;
 using System.Linq;
 using Xunit;
-namespace AutoMapper.IntegrationTests
+
+namespace IntegrationsTests.EFCore
 {
-    public class ProjectionConversionTests : AutoMapperSpecBase
+    public class ProjectionConversionTests
     {
         class Source
         {
@@ -48,24 +50,27 @@ namespace AutoMapper.IntegrationTests
 
         private class ClientContext : DbContext
         {
-            static ClientContext()
-            {
-                Database.SetInitializer(new DropCreateDatabaseAlways<ClientContext>());
-            }
-
             public DbSet<Source> Source1 { get; set; }
             public DbSet<NullableSource> Source2 { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+            }
         }
 
-        protected override MapperConfiguration Configuration => new MapperConfiguration(config =>
+        private readonly IMapper _mapper = new MapperConfiguration(config =>
         {
             config.CreateMap<Source, Destination>();
             config.CreateMap<NullableSource, NullableDestination>();
-
-            // THESE LINES ARE NEEDED TO MAKE THE TEST PASS
             //config.CreateMap<SourceValue, DestinationValue>().ConvertUsing(src => src);
             //config.CreateMap<SourceValue?, DestinationValue?>().ConvertUsing(src => src);
-        });
+        }).CreateMapper();
 
         [Fact]
         public void Should_use_implicit_operator()
@@ -74,7 +79,7 @@ namespace AutoMapper.IntegrationTests
             context.Source1.Add(new Source { Value = SourceValue.Id });
             context.SaveChanges();
 
-            ProjectTo<Destination>(context.Source1).Single().Value.Value.ShouldBe("Id");
+            _mapper.ProjectTo<Destination>(context.Source1).Single().Value.Value.ShouldBe("Id");
         }
 
         [Fact]
@@ -84,7 +89,7 @@ namespace AutoMapper.IntegrationTests
             context.Source2.Add(new NullableSource { Value = null });
             context.SaveChanges();
 
-            ProjectTo<NullableDestination>(context.Source2).Single().Value.ShouldBe(null);
+            _mapper.ProjectTo<NullableDestination>(context.Source2).Single().Value.ShouldBe(null);
         }
 
         [Fact]
@@ -94,7 +99,7 @@ namespace AutoMapper.IntegrationTests
             context.Source2.Add(new NullableSource { Value = SourceValue.Id });
             context.SaveChanges();
 
-            ProjectTo<NullableDestination>(context.Source2).Single().Value.Value.Value.ShouldBe("Id");
+            _mapper.ProjectTo<NullableDestination>(context.Source2).Single().Value.Value.Value.ShouldBe("Id");
         }
     }
 }
