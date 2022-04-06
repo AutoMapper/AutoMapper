4222500e0fdf4e4c15afb218bb7c06e724dc43e5
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2022-04-06 10:08:09 +0300

    increase the timeout

--- src/IntegrationTests/LocalDbContext.cs
+++ src/IntegrationTests/LocalDbContext.cs
@@ -5,5 +5,5 @@ namespace AutoMapper.IntegrationTests;
 public abstract class LocalDbContext : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(
-        @$"Data Source=(localdb)\mssqllocaldb;Integrated Security=True;MultipleActiveResultSets=True;Database={GetType()};Connection Timeout=160");
+        @$"Data Source=(localdb)\mssqllocaldb;Integrated Security=True;MultipleActiveResultSets=True;Database={GetType()};Connection Timeout=300");
 }
