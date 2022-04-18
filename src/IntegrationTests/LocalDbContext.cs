62b4a38a187b0a117668b01b8be390898faa78e1
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2022-04-18 09:45:31 +0300

    EnableRetryOnFailure

--- src/IntegrationTests/LocalDbContext.cs
+++ src/IntegrationTests/LocalDbContext.cs
@@ -5,5 +5,6 @@ namespace AutoMapper.IntegrationTests;
 public abstract class LocalDbContext : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(
-        @$"Data Source=(localdb)\mssqllocaldb;Integrated Security=True;MultipleActiveResultSets=True;Database={GetType()};Connection Timeout=300");
+        @$"Data Source=(localdb)\mssqllocaldb;Integrated Security=True;MultipleActiveResultSets=True;Database={GetType()};Connection Timeout=300", 
+        o=>o.EnableRetryOnFailure());
 }
