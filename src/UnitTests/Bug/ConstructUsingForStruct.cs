422fcaab4b7a1ffaef1c5df4cd4cc8ed126abc43
Maksim Golev <mgolev@htc-cs.ru>
2023-04-04 09:59:22 +0400

    feat: Adding test for reproduce bug with ConstructUsing for struct.

---
+++ src/UnitTests/Bug/ConstructUsingForStruct.cs
@@ -0,0 +1,37 @@
+﻿namespace AutoMapper.UnitTests.Bug;
+public class ConstructUsingForStruct : AutoMapperSpecBase
+{
+    private Dest[] _dest;
+
+    public class Source
+    {
+        public DateTime Value { get; set; }
+    }
+
+    public class Dest
+    {
+        public DateOnly Value { get; set; }
+    }
+
+    protected override MapperConfiguration CreateConfiguration() => new(cfg =>
+    {
+        cfg.CreateMap<DateTime, DateOnly>().ConstructUsing(src => new DateOnly(src.Year + 10, src.Month, src.Day));
+        cfg.CreateMap<Source, Dest>();
+    });
+
+    protected override void Because_of()
+    {
+        var values = new[]
+        {
+            new Source()
+            {
+                Value = new DateTime(2023, 01, 02)
+            }
+        }.AsQueryable();
+
+        _dest = values.ProjectTo<Dest>(Configuration).ToArray();
+    }
+
+    [Fact(Skip = "Bug has not been fixed yet")]
+    public void Should_return_expected_result() => _dest[0].Value.ShouldBe(new DateOnly(2033, 01, 02));
+}
