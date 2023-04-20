7794318c0f9d5655d3b4d5fa1b7c34c7ac66a087
Lucian Bargaoanu <lbargaoanu@hotmail.com>
2023-04-20 18:38:55 +0300

    cosmetic

--- src/AutoMapper/Mapper.cs
+++ src/AutoMapper/Mapper.cs
@@ -15,7 +15,7 @@ public interface IMapperBase
     /// <summary>
     /// Execute a mapping from the source object to a new destination object.
     /// </summary>
-    /// <typeparam name="TSource">Source type to use, regardless of the runtime type</typeparam>
+    /// <typeparam name="TSource">Source type to use</typeparam>
     /// <typeparam name="TDestination">Destination type to create</typeparam>
     /// <param name="source">Source object to map from</param>
     /// <returns>Mapped destination object</returns>
